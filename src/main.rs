bdea4dd15f828211ce6e0afe74016b885776eae8
Łukasz Hanuszczak <hanuszczak@google.com>
2020-03-31 15:04:54 +0200

    Create a basic scaffolding for the main loop.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,21 @@
 // Use of this source code is governed by an MIT-style license that can be found
 // in the LICENSE file or at https://opensource.org/licenses/MIT.
 
-fn main() {
-    println!("Hello, world!");
+use std::io::Result;
+use std::time::Duration;
+
+fn main() -> Result<()> {
+    fleetspeak::startup(env!("CARGO_PKG_VERSION"))?;
+
+    loop {
+        let packet = fleetspeak::collect(Duration::from_secs(1))?;
+        handle(packet.data);
+    }
+}
+
+fn handle(message: rrg_proto::GrrMessage) {
+    match message.name {
+        Some(name) => println!("requested to execute the '{}' action", name),
+        None => eprintln!("missing action name to execute"),
+    }
 }
