4f8cb02b752b6645b6a8502e91321827dc2913c6
Łukasz Hanuszczak <hanuszczak@google.com>
2020-04-05 22:16:17 +0200

    Setup logging.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 // Use of this source code is governed by an MIT-style license that can be found
 // in the LICENSE file or at https://opensource.org/licenses/MIT.
 
+use std::fs::File;
 use std::io::Result;
 use std::time::Duration;
 use std::path::PathBuf;
@@ -82,15 +83,32 @@ fn main() -> Result<()> {
 }
 
 fn init(opts: &Opts) {
-    let _verbosity = opts.log_verbosity; // TODO.
+    let Verbosity(level) = opts.log_verbosity;
+    let mut loggers = Vec::<Box<dyn simplelog::SharedLogger>>::new();
 
-    if let Some(_std) = &opts.log_std {
-        // TODO.
+    if let Some(std) = &opts.log_std {
+        let config = Default::default();
+
+        use simplelog::TerminalMode::*;
+        let logger = simplelog::TermLogger::new(level, config, match std {
+            Std::Out => Stdout,
+            Std::Err => Stderr,
+            Std::Mix => Mixed,
+        }).expect("failed to create a terminal logger");
+
+        loggers.push(logger);
     }
 
-    if let Some(_path) = &opts.log_file {
-        // TODO.
+    if let Some(path) = &opts.log_file {
+        let config = Default::default();
+
+        let file = File::create(path).expect("failed to create the log file");
+        let logger = simplelog::WriteLogger::new(level, config, file);
+
+        loggers.push(logger);
     }
+
+    simplelog::CombinedLogger::init(loggers).expect("failed to init logging");
 }
 
 fn handle(message: rrg_proto::GrrMessage) {
