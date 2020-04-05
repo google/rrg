e372e475a3fae84872d3018f3158674eb175b2c8
Łukasz Hanuszczak <hanuszczak@google.com>
2020-04-05 21:41:52 +0200

    Define command-line options for logging.

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,91 @@
 
 use std::io::Result;
 use std::time::Duration;
+use std::path::PathBuf;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Verbosity(log::LevelFilter);
+
+impl std::str::FromStr for Verbosity {
+
+    type Err = String; // TODO.
+
+    fn from_str(string: &str) -> std::result::Result<Verbosity, String> {
+        use log::LevelFilter::*;
+
+        match string {
+            "quiet" => Ok(Verbosity(Off)),
+            "error" => Ok(Verbosity(Error)),
+            "warn" => Ok(Verbosity(Warn)),
+            "info" => Ok(Verbosity(Info)),
+            "debug" => Ok(Verbosity(Debug)),
+            "trace" => Ok(Verbosity(Trace)),
+            _ => Err(format!("invalid verbosity choice '{}'", string)),
+        }
+    }
+}
+
+// TODO: This should just be a wrapper around `simplelog::TerminalMode`, but
+// it does not implement standard traits. So, for now, we just re-implement it
+// like that.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Std {
+    Out,
+    Err,
+    Mix,
+}
+
+impl std::str::FromStr for Std {
+
+    type Err = String; // TODO.
+
+    fn from_str(string: &str) -> std::result::Result<Std, String> {
+        match string {
+            "out" => Ok(Std::Out),
+            "err" => Ok(Std::Err),
+            "mix" => Ok(Std::Mix),
+            _ => Err(format!("invalid std choice '{}'", string)),
+        }
+    }
+}
 
 #[derive(structopt_derive::StructOpt)]
 #[structopt(name = "RRG", about = "A GRR agent rewritten in Rust.")]
 struct Opts {
+    #[structopt(long="log-verbosity", name="LEVEL", default_value="info",
+                help="A log verbosity level")]
+    log_verbosity: Verbosity,
+
+    #[structopt(long="log-std", name="STD",
+                help="A standard stream to log to")]
+    log_std: Option<Std>,
+
+    #[structopt(long="log-file", name="FILE",
+                help="A file to log to")]
+    log_file: Option<PathBuf>,
 }
 
 fn main() -> Result<()> {
-    <Opts as structopt::StructOpt>::from_args();
+    let opts = <Opts as structopt::StructOpt>::from_args();
+    init(&opts);
 
     fleetspeak::startup(env!("CARGO_PKG_VERSION"))?;
 
     loop {
         let packet = fleetspeak::collect(Duration::from_secs(1))?;
         handle(packet.data);
+    }
+}
+
+fn init(opts: &Opts) {
+    let _verbosity = opts.log_verbosity; // TODO.
+
+    if let Some(_std) = &opts.log_std {
+        // TODO.
+    }
+
+    if let Some(_path) = &opts.log_file {
+        // TODO.
     }
 }
 
