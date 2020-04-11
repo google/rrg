3ed2c520a62d3ddb8843bf036ffe74b9d61d69b2
Łukasz Hanuszczak <hanuszczak@google.com>
2020-04-11 16:52:11 +0200

    Write docs for command-line options.

--- src/opts.rs
+++ src/opts.rs
@@ -3,6 +3,18 @@
 // Use of this source code is governed by an MIT-style license that can be found
 // in the LICENSE file or at https://opensource.org/licenses/MIT.
 
+//! Structured specification of command-line arguments.
+//!
+//! This module specifies all command-line arguments that RRG offers and exposes
+//! functions for parsing them into a high-level structure.
+//!
+//! Ideally, only one instance of this high-level structure should ever be
+//! created (using the [`from_args`] function). Then this instance should be
+//! shared through the entire lifetime of a program and explicitly passed to
+//! functions that care about it.
+//!
+//! [`from_args`]: fn.from_args.html
+
 use std::time::Duration;
 use std::path::PathBuf;
 
@@ -11,28 +23,40 @@ use structopt::StructOpt;
 #[derive(StructOpt)]
 #[structopt(name = "RRG", about = "A GRR agent rewritten in Rust.")]
 pub struct Opts {
+    /// A level of log verbosity.
     #[structopt(long="log-verbosity", name="LEVEL", default_value="info",
                 help="Specifies the level of log verbosity")]
     pub log_verbosity: Verbosity,
 
+    /// A standard stream to log into.
     #[structopt(long="log-std", name="STD",
                 help="Enables logging to the specified standard stream")]
     pub log_std: Option<Std>,
 
+    /// A path to the file to log into.
     #[structopt(long="log-file", name="FILE",
                 help="Enables logging to the specified file")]
     pub log_file: Option<PathBuf>,
 
+    /// A frequence of heartbeat messages to send to the Fleetspeak client.
     #[structopt(long="heartbeat-rate", name="DURATION", default_value="5s",
                 parse(try_from_str = humantime::parse_duration),
                 help="Specifies the frequency of heartbeat messages")]
     pub heartbeat_rate: Duration,
 }
 
+/// Parses command-line arguments.
+///
+/// This is a just a convenience function intended to be used as a shortcut for
+/// creating instances of [`Opts`]. Ideally, it should be called only once in
+/// the entire lifetime of the agent.
+///
+/// [`Opts`]: struct.Opts.html
 pub fn from_args() -> Opts {
     Opts::from_args()
 }
 
+/// A type representing level of log verbosity.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Verbosity {
     level: log::LevelFilter,
@@ -40,6 +64,7 @@ pub struct Verbosity {
 
 impl Verbosity {
 
+    /// Yields a corresponding log filter.
     pub fn level(&self) -> log::LevelFilter {
         self.level
     }
@@ -71,15 +96,20 @@ impl std::str::FromStr for Verbosity {
 // TODO: This should just be a wrapper around `simplelog::TerminalMode`, but
 // it does not implement standard traits. So, for now, we just re-implement it
 // like that.
+/// A type listing different options for logging to standard streams.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Std {
+    /// Log to the standard output.
     Out,
+    /// Log to the standard error.
     Err,
+    /// Log errors to the standard error, not-errors to the standard output.
     Mix,
 }
 
 impl Std {
 
+    /// Yields a corresponding terminal mode.
     pub fn mode(&self) -> simplelog::TerminalMode {
         use simplelog::TerminalMode::*;
 
