45f0d9710df8f01e5dbfc9bdfc541fe0c7aa6ff4
Łukasz Hanuszczak <hanuszczak@google.com>
2020-04-17 21:25:47 +0200

    Create a more idiomatic wrapper for GRR messages.

--- src/session.rs
+++ src/session.rs
@@ -20,24 +20,44 @@ impl Session {
 
     // TODO: Handle errors properly.
     pub fn reply<R: Response>(&mut self, response: R) -> Result<()> {
-        let mut data = Vec::new();
-        prost::Message::encode(&response.into_proto(), &mut data)?;
+        Message {
+            session_id: self.id.clone(),
+            response_id: self.next_response_id,
+            data: response,
+        }.send()?;
 
-        let message = rrg_proto::GrrMessage {
-            session_id: Some(self.id.clone()),
-            response_id: Some(self.next_response_id),
+        self.next_response_id += 1;
+
+        Ok(())
+    }
+}
+
+struct Message<R: Response> {
+    session_id: String,
+    response_id: u64,
+    data: R,
+}
+
+impl<R: Response> Message<R> {
+
+    // TODO: Handle errors properly.
+    fn send(self) -> Result<()> {
+        let mut data = Vec::new();
+        prost::Message::encode(&self.data.into_proto(), &mut data)?;
+
+        let proto = rrg_proto::GrrMessage {
+            session_id: Some(self.session_id),
+            response_id: Some(self.response_id),
             r#type: Some(rrg_proto::grr_message::Type::Message.into()),
             args_rdf_name: R::RDF_NAME.map(String::from),
             args: Some(data),
             ..Default::default()
         };
 
-        self.next_response_id += 1;
-
         fleetspeak::send(Packet {
             service: String::from("GRR"),
             kind: Some(String::from("GrrMessage")),
-            data: message,
+            data: proto,
         })?;
 
         Ok(())
