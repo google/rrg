c7deb6e88d12e1479aae5f6e2513078d558b4caf
Łukasz Hanuszczak <hanuszczak@google.com>
2020-04-24 23:51:21 +0200

    Write basic documentation for the metadata action.

--- src/action/metadata.rs
+++ src/action/metadata.rs
@@ -3,13 +3,21 @@
 // Use of this source code is governed by an MIT-style license that can be found
 // in the LICENSE file or at https://opensource.org/licenses/MIT.
 
+//! A handler and associated types for the metadata action.
+//!
+//! The metadata action collects basic information about the client (e.g. its
+//! version number and its name).
+
 use crate::metadata::{Metadata};
 use crate::session::{self, Session};
 
+/// A response type for the metadata action.
 pub struct Response {
+    /// Metadata about the RRG agent.
     metadata: Metadata,
 }
 
+/// Handles requests for the metadata action.
 pub fn handle<S: Session>(session: &mut S, _: ()) -> session::Result<()> {
     session.reply(Response {
         metadata: Metadata::from_cargo(),
