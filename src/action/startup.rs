87e0008de64e2b2753544e34e3cffe7b31a58852
Łukasz Hanuszczak <hanuszczak@google.com>
2020-05-04 21:00:37 +0200

    Write tests for the startup action.

--- src/action/startup.rs
+++ src/action/startup.rs
@@ -101,3 +101,29 @@ impl super::Response for Response {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_boot_time() {
+        let mut session = session::test::Fake::new();
+        assert!(handle(&mut session, ()).is_ok());
+
+        let response = session.get_sink::<Response>(session::Sink::STARTUP, 0);
+        assert!(response.boot_time > std::time::UNIX_EPOCH);
+        assert!(response.boot_time < std::time::SystemTime::now());
+    }
+
+    #[test]
+    fn test_metadata() {
+        let mut session = session::test::Fake::new();
+        assert!(handle(&mut session, ()).is_ok());
+
+        let response = session.get_sink::<Response>(session::Sink::STARTUP, 0);
+        assert!(response.metadata.version.as_numeric() > 0);
+        assert_eq!(response.metadata.name, "rrg");
+    }
+}
