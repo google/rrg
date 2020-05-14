c3d9cfadb4adb778020c177c1e09394d57ce0e71
nikkita1267 <nikkita1267@gmail.com>
2020-05-14 10:23:09 +0300

    Add tests and some comments

--- src/action/platform_info.rs
+++ src/action/platform_info.rs
@@ -4,8 +4,9 @@
 // in the LICENSE file or at https://opensource.org/licenses/MIT.
 
 extern crate sys_info;
+
+/// Using for `uname` syscall.
 extern crate libc;
-// extern crate sysinfo;
 
 use sys_info::{linux_os_release, os_type, hostname};
 use libc::{uname, utsname, c_char};
@@ -15,6 +16,7 @@ use crate::session::{self, Session};
 
 use rrg_proto::Uname;
 
+/// Structure that contains information about client platform.
 #[derive(Default)]
 pub struct PlatformInfo {
     system: Option<String>,
@@ -27,10 +29,13 @@ pub struct PlatformInfo {
     node: Option<String>
 }
 
+/// A Response type for `GetPlatformInfo` action
 pub struct Response {
+    /// Client platform information
     platform_information: PlatformInfo,
 }
 
+/// Funcion that converts raw C-strings into `String` type
 #[inline(always)]
 fn convert_raw_string(c_string: &[c_char]) -> String {
     unsafe { 
@@ -38,6 +43,8 @@ fn convert_raw_string(c_string: &[c_char]) -> String {
     } 
 }
 
+/// Handles requests for `GetPlatformInfo` action.
+/// Currently works fine only for Linux OS.
 pub fn handle<S: Session>(session:&mut S, _: ()) -> session::Result<()> {
     let os_type = os_type().expect("Can't get os type");
     match os_type.as_str() {
@@ -82,9 +89,10 @@ pub fn handle<S: Session>(session:&mut S, _: ()) -> session::Result<()> {
 impl super::Response for Response {
     const RDF_NAME: Option<&'static str> = Some("Uname");
 
-    type Proto = Uname;
+    type Proto = rrg_proto::Uname;
 
-    fn into_proto(self) -> Uname {
+    /// Convert PlatformInformation struct to protobuf message Uname
+    fn into_proto(self) -> rrg_proto::Uname {
         Uname {
             system: self.platform_information.system.clone(),
             release: self.platform_information.release_name.clone(),
@@ -101,6 +109,124 @@ impl super::Response for Response {
                     self.platform_information.architecture.unwrap_or(String::from(""))
             )),
             ..Default::default()
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_system() {
+        let mut session = session::test::Fake::new();
+        assert!(handle(&mut session, ()).is_ok());
+
+        assert_eq!(session.reply_count(), 1);
+        let platform_info = &session.reply::<Response>(0).platform_information;
+
+        assert_eq!(platform_info.system.as_ref().unwrap(), sys_info::os_type().as_ref().unwrap());
+    }
+
+    #[test]
+    fn test_linux_release() {
+        let mut session = session::test::Fake::new();
+        assert!(handle(&mut session, ()).is_ok());
+
+        assert_eq!(session.reply_count(), 1);
+        let platform_info = &session.reply::<Response>(0).platform_information;
+
+        if sys_info::os_type().unwrap() == "Linux" {
+            assert_eq!(platform_info.release_name.as_ref().unwrap(), sys_info::linux_os_release().unwrap().name.as_ref().unwrap());
+        }
+    }
+
+    #[test]
+    fn test_linux_version() {
+        let mut session = session::test::Fake::new();
+        assert!(handle(&mut session, ()).is_ok());
+
+        assert_eq!(session.reply_count(), 1);
+        let platform_info = &session.reply::<Response>(0).platform_information;
+
+        if sys_info::os_type().unwrap() == "Linux" {
+            assert_eq!(platform_info.version_id.as_ref().unwrap(), sys_info::linux_os_release().unwrap().version_id.as_ref().unwrap());
+        }
+    }
+
+    #[test]
+    fn test_linux_machine() {
+        let mut session = session::test::Fake::new();
+        assert!(handle(&mut session, ()).is_ok());
+
+        assert_eq!(session.reply_count(), 1);
+        let platform_info = &session.reply::<Response>(0).platform_information;
+
+        if sys_info::os_type().unwrap() == "Linux" {
+            let mut system_info: utsname;
+
+            unsafe {
+                system_info = std::mem::zeroed();
+                uname(&mut system_info);
+            }
+            assert_eq!(platform_info.machine.as_ref().unwrap(), &convert_raw_string(&system_info.machine));
+        }
+    }
+
+    #[test]
+    fn test_linux_kernel_release() {
+        let mut session = session::test::Fake::new();
+        assert!(handle(&mut session, ()).is_ok());
+
+        assert_eq!(session.reply_count(), 1);
+        let platform_info = &session.reply::<Response>(0).platform_information;
+
+        if sys_info::os_type().unwrap() == "Linux" {
+            let mut system_info: utsname;
+
+            unsafe {
+                system_info = std::mem::zeroed();
+                uname(&mut system_info);
+            }
+            assert_eq!(platform_info.kernel_release.as_ref().unwrap(), &convert_raw_string(&system_info.release));
+        }
+    }
+
+    #[test]
+    fn test_linux_architecture() {
+        let mut session = session::test::Fake::new();
+        assert!(handle(&mut session, ()).is_ok());
+
+        assert_eq!(session.reply_count(), 1);
+        let platform_info = &session.reply::<Response>(0).platform_information;
+
+        if sys_info::os_type().unwrap() == "Linux" {
+            let mut system_info: utsname;
+
+            unsafe {
+                system_info = std::mem::zeroed();
+                uname(&mut system_info);
+            }
+            assert_eq!(platform_info.architecture.as_ref().unwrap(), &convert_raw_string(&system_info.machine));
+        }
+    }
+
+    #[test]
+    fn test_linux_node() {
+        let mut session = session::test::Fake::new();
+        assert!(handle(&mut session, ()).is_ok());
+
+        assert_eq!(session.reply_count(), 1);
+        let platform_info = &session.reply::<Response>(0).platform_information;
+
+        if sys_info::os_type().unwrap() == "Linux" {
+            let mut system_info: utsname;
+
+            unsafe {
+                system_info = std::mem::zeroed();
+                uname(&mut system_info);
+            }
+            assert_eq!(platform_info.node.as_ref().unwrap(), &convert_raw_string(&system_info.nodename));
         }
     }
 }
