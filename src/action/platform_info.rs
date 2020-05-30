a30c6baaa5c385afa2e8787220d1e4d8fca99fea
nikkita1267 <nikkita1267@gmail.com>
2020-05-30 15:36:49 +0300

    Some fixes

--- src/action/platform_info.rs
+++ src/action/platform_info.rs
@@ -39,7 +39,6 @@ impl Display for Error {
             CannotGetOSType(ref error) => {
                 write!(fmt, "can't get OS type error: {}", error)
             },
-
             CannotGetLinuxRelease(ref error) => {
                 write!(fmt, "can't get Linux release info error: {}", error)
             }
@@ -54,9 +53,10 @@ impl From<Error> for session::Error {
     }
 }
 
-/// Structure that contains information about client platform.
+/// A Response type for `GetPlatformInfo` action
 #[derive(Default)]
-pub struct PlatformInfo {
+pub struct Response {
+    /// Client platform information
     system: Option<String>,
     release_name: Option<String>,
     version_id: Option<String>,
@@ -65,12 +65,6 @@ pub struct PlatformInfo {
     fqdn: Option<String>,
     architecture: Option<String>,
     node: Option<String>
-}
-
-/// A Response type for `GetPlatformInfo` action
-pub struct Response {
-    /// Client platform information
-    platform_information: PlatformInfo,
 }
 
 /// Funcion that converts raw C-strings into `String` type
@@ -99,7 +93,6 @@ pub fn handle<S: Session>(session:&mut S, _: ()) -> session::Result<()> {
             }
 
             session.reply(Response {
-                platform_information: PlatformInfo {
                     system: Some(os_type.to_string()),
                     release_name: linux_release_info.name,
                     version_id: linux_release_info.version_id,
@@ -108,18 +101,14 @@ pub fn handle<S: Session>(session:&mut S, _: ()) -> session::Result<()> {
                     fqdn: hostname().ok(),
                     architecture: Some(convert_raw_string(&system_info.machine)),
                     node: Some(convert_raw_string(&system_info.nodename))
-                },
-            })?;
+                })?;
         },
-
         _ => {
             session.reply(Response {
-                platform_information: PlatformInfo {
                     system: Some(os_type.to_string()),
                     fqdn: hostname().ok(),
                     ..Default::default() // TODO: Add other systems
-                }
-            })?;
+                })?;
         }
     }
 
@@ -127,6 +116,7 @@ pub fn handle<S: Session>(session:&mut S, _: ()) -> session::Result<()> {
 }
 
 impl super::Response for Response {
+
     const RDF_NAME: Option<&'static str> = Some("Uname");
 
     type Proto = rrg_proto::Uname;
@@ -134,19 +124,19 @@ impl super::Response for Response {
     /// Convert PlatformInformation struct to protobuf message Uname
     fn into_proto(self) -> rrg_proto::Uname {
         Uname {
-            system: self.platform_information.system.clone(),
-            release: self.platform_information.release_name.clone(),
-            version: self.platform_information.version_id,
-            machine: self.platform_information.machine,
-            kernel: self.platform_information.kernel_release,
-            fqdn: self.platform_information.fqdn,
-            architecture: self.platform_information.architecture.clone(),
-            node: self.platform_information.node,
+            system: self.system.clone(),
+            release: self.release_name.clone(),
+            version: self.version_id,
+            machine: self.machine,
+            kernel: self.kernel_release,
+            fqdn: self.fqdn,
+            architecture: self.architecture.clone(),
+            node: self.node,
             pep425tag: Some(
                 format!("{}_{}_{}", 
-                    self.platform_information.system.unwrap_or(String::from("")), 
-                    self.platform_information.release_name.unwrap_or(String::from("")), 
-                    self.platform_information.architecture.unwrap_or(String::from(""))
+                    self.system.unwrap_or(String::from("")), 
+                    self.release_name.unwrap_or(String::from("")), 
+                    self.architecture.unwrap_or(String::from(""))
             )),
             ..Default::default()
         }
@@ -155,6 +145,7 @@ impl super::Response for Response {
 
 #[cfg(test)]
 mod test {
+
     use super::*;
 
     #[test]
@@ -163,7 +154,7 @@ mod test {
         assert!(handle(&mut session, ()).is_ok());
 
         assert_eq!(session.reply_count(), 1);
-        let platform_info = &session.reply::<Response>(0).platform_information;
+        let platform_info = &session.reply::<Response>(0);
 
         assert_eq!(platform_info.system.as_ref().unwrap(), sys_info::os_type().as_ref().unwrap());
     }
@@ -174,7 +165,7 @@ mod test {
         assert!(handle(&mut session, ()).is_ok());
 
         assert_eq!(session.reply_count(), 1);
-        let platform_info = &session.reply::<Response>(0).platform_information;
+        let platform_info = &session.reply::<Response>(0);
 
         if sys_info::os_type().unwrap() == "Linux" {
             assert_eq!(platform_info.release_name.as_ref().unwrap(), sys_info::linux_os_release().unwrap().name.as_ref().unwrap());
@@ -187,7 +178,7 @@ mod test {
         assert!(handle(&mut session, ()).is_ok());
 
         assert_eq!(session.reply_count(), 1);
-        let platform_info = &session.reply::<Response>(0).platform_information;
+        let platform_info = &session.reply::<Response>(0);
 
         if sys_info::os_type().unwrap() == "Linux" {
             assert_eq!(platform_info.version_id.as_ref().unwrap(), sys_info::linux_os_release().unwrap().version_id.as_ref().unwrap());
@@ -200,7 +191,7 @@ mod test {
         assert!(handle(&mut session, ()).is_ok());
 
         assert_eq!(session.reply_count(), 1);
-        let platform_info = &session.reply::<Response>(0).platform_information;
+        let platform_info = &session.reply::<Response>(0);
 
         if sys_info::os_type().unwrap() == "Linux" {
             let mut system_info: utsname;
@@ -219,7 +210,7 @@ mod test {
         assert!(handle(&mut session, ()).is_ok());
 
         assert_eq!(session.reply_count(), 1);
-        let platform_info = &session.reply::<Response>(0).platform_information;
+        let platform_info = &session.reply::<Response>(0);
 
         if sys_info::os_type().unwrap() == "Linux" {
             let mut system_info: utsname;
@@ -238,7 +229,7 @@ mod test {
         assert!(handle(&mut session, ()).is_ok());
 
         assert_eq!(session.reply_count(), 1);
-        let platform_info = &session.reply::<Response>(0).platform_information;
+        let platform_info = &session.reply::<Response>(0);
 
         if sys_info::os_type().unwrap() == "Linux" {
             let mut system_info: utsname;
@@ -257,7 +248,7 @@ mod test {
         assert!(handle(&mut session, ()).is_ok());
 
         assert_eq!(session.reply_count(), 1);
-        let platform_info = &session.reply::<Response>(0).platform_information;
+        let platform_info = &session.reply::<Response>(0);
 
         if sys_info::os_type().unwrap() == "Linux" {
             let mut system_info: utsname;
