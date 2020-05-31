7066640cee036ac5fa7423dd2750db759c801676
Alexander Kernozhitsky <sh200105@mail.ru>
2020-05-31 19:58:15 +0300

    Apply style fixes and add documentation

--- src/action/install_date.rs
+++ src/action/install_date.rs
@@ -1,3 +1,13 @@
+// Copyright 2020 Google LLC
+//
+// Use of this source code is governed by an MIT-style license that can be found
+// in the LICENSE file or at https://opensource.org/licenses/MIT.
+
+//! A handler and associated types for the install time action.
+//!
+//! The install time action uses various heuristics to detect the operating
+//! system install time.
+
 use std::time::{SystemTime, Duration};
 
 use log::error;
@@ -9,13 +19,16 @@ use std::{fs, path::Path};
 #[cfg(target_os = "windows")]
 use winreg::RegKey;
 
+/// A response type for the install time action.
 struct Response {
+    /// Install time of the operating system, or `None` if the attemps to
+    /// obtain install time failed.
     time: Option<SystemTime>,
 }
 
 impl super::Response for Response {
 
-    // TODO : change to RDFDatetime when the client will be capable to send
+    // TODO: change to RDFDatetime when the client will be capable to send
     // "raw" strings without wrapping them into protobuf
     const RDF_NAME: Option<&'static str> = Some("DataBlob");
 
@@ -33,7 +46,7 @@ impl super::Response for Response {
             Ok(duration) => duration,
             Err(err) => {
                 error!(
-                    "install date is {} seconds earlier than Unix epoch",
+                    "install time is {} seconds earlier than Unix epoch",
                     err.duration().as_secs()
                 );
                 Duration::from_secs(0)
@@ -43,6 +56,11 @@ impl super::Response for Response {
     }
 }
 
+/// Iterates over files given it `iter`, checking their modification times.
+///
+/// If the function cannot get modification time from all the files (e. g.
+/// they all don't exist), the return value is `None`. Otherwise, the
+/// modification time of the first file is returned.
 #[cfg(target_family = "unix")]
 fn get_modified_time<P, It>(iter: It) -> Option<SystemTime>
 where
@@ -59,6 +77,8 @@ where
     None
 }
 
+/// Contains utilities to work with ext2/3/4 filesystems. Currently, it is used
+/// to run `dumpe2fs`, parse its output and get filesystem creation time.
 #[cfg(target_os = "linux")]
 mod e2fs_utils {
     use std::io::{Cursor, Read, BufRead, BufReader};
@@ -69,12 +89,19 @@ mod e2fs_utils {
     use chrono::prelude::*;
     use proc_mounts::MountList;
 
+    /// Returns the file name of the device that is mounted as root file
+    /// system.
+    ///
+    /// This function returns `None` in case of errors.
     fn get_root_device() -> Option<PathBuf> {
         let mount_list = MountList::new().ok()?;
         mount_list.get_mount_by_dest("/").map(|info| info.source.clone())
     }
 
-    fn parse_creation_date_from_dumpe2fs<R>(output: R) -> Option<SystemTime>
+    /// Parses `dumpe2fs` output to get the filesystem creation time.
+    ///
+    /// This function returns `None` in case of errors.
+    fn parse_creation_time_from_dumpe2fs<R>(output: R) -> Option<SystemTime>
     where
         R: Read,
     {
@@ -95,7 +122,11 @@ mod e2fs_utils {
         None
     }
 
-    pub fn creation_date_from_dumpe2fs() -> Option<SystemTime> {
+    /// Launches `dumpe2fs`, captures its output and obtains filesystem
+    /// creation time.
+    ///
+    /// This function returns `None` in case of errors.
+    pub fn creation_time_from_dumpe2fs() -> Option<SystemTime> {
         let output = Command::new("dumpe2fs")
             .arg(get_root_device()?)
             .stdout(Stdio::piped())
@@ -104,7 +135,7 @@ mod e2fs_utils {
         if !output.status.success() {
             return None;
         }
-        parse_creation_date_from_dumpe2fs(Cursor::new(output.stdout))
+        parse_creation_time_from_dumpe2fs(Cursor::new(output.stdout))
     }
 
     #[cfg(test)]
@@ -113,10 +144,10 @@ mod e2fs_utils {
         use std::io::Cursor;
 
         #[test]
-        fn test_parse_creation_date() {
-            let required = DateTime::parse_from_rfc3339("2020-05-31T19:02:03Z")
+        fn test_parse_creation_time() {
+            let correct = DateTime::parse_from_rfc3339("2020-05-31T19:02:03Z")
                 .unwrap().with_timezone(&Utc);
-            let creation_time = required.with_timezone(&Local)
+            let creation_time = correct.with_timezone(&Local)
                 .format("%c").to_string();
             let input = String::from(
                 "Line 1\n\
@@ -126,11 +157,14 @@ mod e2fs_utils {
             let cursor = Cursor::new(input.as_bytes());
             let parsed = parse_creation_date_from_dumpe2fs(cursor).unwrap();
             let parsed = DateTime::<Utc>::from(parsed);
-            assert_eq!(parsed, required);
+            assert_eq!(parsed, correct);
         }
     }
 }
 
+/// Obtains system installation time using various heuristics.
+///
+/// This function returns `None` in case of errors.
 #[cfg(target_os = "linux")]
 fn get_install_time() -> Option<SystemTime> {
     // First, check the creation time of the root. This method works on
@@ -140,9 +174,9 @@ fn get_install_time() -> Option<SystemTime> {
         return Some(time);
     }
 
-    // Then, try to detect filesystem creation date using dumpe2fs. This
+    // Then, try to detect filesystem creation time using dumpe2fs. This
     // works only on ext2/3/4 filesystems.
-    let time = e2fs_utils::creation_date_from_dumpe2fs();
+    let time = e2fs_utils::creation_time_from_dumpe2fs();
     if let Some(time) = time {
         return Some(time);
     }
@@ -172,6 +206,9 @@ fn get_install_time() -> Option<SystemTime> {
     None
 }
 
+/// Obtains system installation time using various heuristics.
+///
+/// This function returns `None` in case of errors.
 #[cfg(target_os = "macos")]
 fn get_install_time() -> Option<SystemTime> {
     // Here, we use the same way as Python version of GRR client does. We just
@@ -184,22 +221,24 @@ fn get_install_time() -> Option<SystemTime> {
     get_modified_time(CANDIDATES.iter())
 }
 
+/// Obtains system installation time using various heuristics.
+///
+/// This function returns `None` in case of errors.
 #[cfg(target_os = "windows")]
 fn get_install_time() -> Option<SystemTime> {
     // Don't use winreg::enums::KEY_WOW64_64KEY since it breaks on Windows 2000
     let hklm = RegKey::predef(winreg::enums::HKEY_LOCAL_MACHINE);
-    let install_date = hklm
+    let install_time = hklm
         .open_subkey("Software\\Microsoft\\Windows NT\\CurrentVersion").ok()?
         .get_value::<u32, _>("InstallDate").ok()?;
-    Some(SystemTime::UNIX_EPOCH + Duration::from_secs(install_date.into()))
+    Some(SystemTime::UNIX_EPOCH + Duration::from_secs(install_time.into()))
 }
 
+/// Handles requests for the install time action.
 pub fn handle<S: Session>(session: &mut S, _: ()) -> session::Result<()> {
     session.reply(Response {time: get_install_time()})?;
     Ok(())
 }
-
-// TODO: write docs
 
 #[cfg(test)]
 mod tests {
@@ -208,7 +247,7 @@ mod tests {
     use humantime::Timestamp;
 
     #[test]
-    fn test_installation_date() {
+    fn test_install_time() {
         let mut session = session::test::Fake::new();
         assert!(handle(&mut session, ()).is_ok());
         assert_eq!(session.reply_count(), 1);
