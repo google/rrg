258333cee331c58859b1330f4c721e6fb51c1ea6
Łukasz Hanuszczak <hanuszczak@google.com>
2020-08-21 17:36:00 +0200

    Flatten the walking logic.

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -6,13 +6,13 @@ use log::warn;
 pub fn walk<P: AsRef<Path>>(root: P) -> std::io::Result<Walk> {
     let iter = std::fs::read_dir(root)?;
     Ok(Walk {
-        pending: vec!(iter),
+        pending: vec!(WalkDir::from_read_dir(iter)),
     })
 }
 
 pub struct Walk {
     // TODO: Add support for stopping at device boundaries.
-    pending: Vec<std::fs::ReadDir>,
+    pending: Vec<WalkDir>,
 }
 
 pub struct WalkEntry {
@@ -20,32 +20,12 @@ pub struct WalkEntry {
     metadata: Metadata,
 }
 
-impl WalkEntry {
-
-    fn from_dir_entry(entry: std::fs::DirEntry) -> Option<WalkEntry> {
-        let path = entry.path();
-
-        let metadata = match entry.metadata() {
-            Ok(metadata) => metadata,
-            Err(error) => {
-                warn!("failed to stat file '{}': {}", path.display(), error);
-                return None;
-            },
-        };
-
-        Some(WalkEntry {
-            path: path,
-            metadata: metadata,
-        })
-    }
-}
-
 impl Walk {
 
     fn push(&mut self, entry: &WalkEntry) {
         match std::fs::read_dir(&entry.path) {
             Ok(iter) => {
-                self.pending.push(iter);
+                self.pending.push(WalkDir::from_read_dir(iter));
             },
             Err(error) => {
                 warn!("failed to read '{}': {}", entry.path.display(), error);
@@ -56,15 +36,7 @@ impl Walk {
     fn pop(&mut self) -> Option<WalkEntry> {
         while let Some(iter) = self.pending.last_mut() {
             for entry in iter {
-                match entry {
-                    Ok(entry) => {
-                        let entry = WalkEntry::from_dir_entry(entry);
-                        if entry.is_some() {
-                            return entry;
-                        }
-                    },
-                    Err(error) => warn!("directory iteration error: {}", error),
-                }
+                return Some(entry);
             }
 
             self.pending.pop();
@@ -82,5 +54,51 @@ impl std::iter::Iterator for Walk {
         let entry = self.pop()?;
         self.push(&entry);
         Some(entry)
+    }
+}
+
+struct WalkDir {
+    iter: std::fs::ReadDir,
+}
+
+impl WalkDir {
+
+    fn from_read_dir(iter: std::fs::ReadDir) -> WalkDir {
+        WalkDir {
+            iter: iter,
+        }
+    }
+}
+
+impl std::iter::Iterator for WalkDir {
+
+    type Item = WalkEntry;
+
+    fn next(&mut self) -> Option<WalkEntry> {
+        for entry in &mut self.iter {
+            let entry = match entry {
+                Ok(entry) => entry,
+                Err(error) => {
+                    warn!("directory iteration error: {}", error);
+                    continue
+                },
+            };
+
+            let path = entry.path();
+            let metadata = match entry.metadata() {
+                Ok(metadata) => metadata,
+                Err(error) => {
+                    warn!("failed to stat '{}': {}", path.display(), error);
+                    continue
+                },
+            };
+
+            return Some(WalkEntry {
+                path: path,
+                metadata: metadata,
+            });
+        }
+
+        None
     }
 }
