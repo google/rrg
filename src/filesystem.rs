2feba07daa71333840cecce2fdfe935eb53695fd
Łukasz Hanuszczak <hanuszczak@google.com>
2020-08-22 13:11:40 +0200

    Write tests for the filesystem walk iterator.

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -199,4 +199,192 @@ mod tests {
         assert_eq!(results[1].path, target);
         assert!(results[1].metadata.file_type().is_symlink());
     }
+
+    // TODO: The iterator should yield information about the root folder on its
+    // first tick.
+
+    #[test]
+    fn test_walk_empty() {
+        let tempdir = tempfile::tempdir().unwrap();
+
+        let mut iter = walk(&tempdir).unwrap();
+        assert!(iter.next().is_none());
+    }
+
+    #[test]
+    fn test_walk_with_flat_files() {
+        let tempdir = tempfile::tempdir().unwrap();
+        File::create(tempdir.path().join("abc")).unwrap();
+        File::create(tempdir.path().join("def")).unwrap();
+        File::create(tempdir.path().join("ghi")).unwrap();
+
+        let mut results = walk(&tempdir).unwrap().collect::<Vec<_>>();
+        results.sort_by_key(|entry| entry.path.clone());
+
+        assert_eq!(results.len(), 3);
+
+        assert_eq!(results[0].path, tempdir.path().join("abc"));
+        assert!(results[0].metadata.is_file());
+
+        assert_eq!(results[1].path, tempdir.path().join("def"));
+        assert!(results[1].metadata.is_file());
+
+        assert_eq!(results[2].path, tempdir.path().join("ghi"));
+        assert!(results[2].metadata.is_file());
+    }
+
+    #[test]
+    fn test_walk_with_flat_dirs() {
+        let tempdir = tempfile::tempdir().unwrap();
+        std::fs::create_dir(tempdir.path().join("abc")).unwrap();
+        std::fs::create_dir(tempdir.path().join("def")).unwrap();
+
+        let mut results = walk(&tempdir).unwrap().collect::<Vec<_>>();
+        results.sort_by_key(|entry| entry.path.clone());
+
+        assert_eq!(results.len(), 2);
+
+        assert_eq!(results[0].path, tempdir.path().join("abc"));
+        assert!(results[0].metadata.is_dir());
+
+        assert_eq!(results[1].path, tempdir.path().join("def"));
+        assert!(results[1].metadata.is_dir());
+    }
+
+    #[test]
+    fn test_walk_with_nested_dirs() {
+        let tempdir = tempfile::tempdir().unwrap();
+        std::fs::create_dir(tempdir.path().join("abc")).unwrap();
+        std::fs::create_dir(tempdir.path().join("abc").join("def")).unwrap();
+
+
+        let mut results = walk(&tempdir).unwrap().collect::<Vec<_>>();
+        results.sort_by_key(|entry| entry.path.clone());
+
+        assert_eq!(results.len(), 2);
+
+        assert_eq!(results[0].path, tempdir.path().join("abc"));
+        assert!(results[0].metadata.is_dir());
+
+        assert_eq!(results[1].path, tempdir.path().join("abc").join("def"));
+        assert!(results[1].metadata.is_dir());
+    }
+
+    #[test]
+    fn test_walk_with_deeply_nested_dirs() {
+        let tempdir = tempfile::tempdir().unwrap();
+
+        let mut dir = tempdir.path().to_path_buf();
+        for _ in 0..512 {
+            dir.push("foo");
+            std::fs::create_dir(&dir).unwrap();
+        }
+
+        let mut results = walk(&tempdir).unwrap().collect::<Vec<_>>();
+        results.sort_by_key(|entry| entry.path.clone());
+
+        assert_eq!(results.len(), 512);
+
+        for entry in results {
+            assert!(entry.path.starts_with(&tempdir));
+            assert!(entry.path.ends_with("foo"));
+            assert!(entry.metadata.is_dir());
+        }
+    }
+
+    #[test]
+    fn test_walk_with_files_inside_dir() {
+        let tempdir = tempfile::tempdir().unwrap();
+        std::fs::create_dir(tempdir.path().join("foo")).unwrap();
+        File::create(tempdir.path().join("foo").join("abc")).unwrap();
+        File::create(tempdir.path().join("foo").join("def")).unwrap();
+
+        let mut results = walk(&tempdir).unwrap().collect::<Vec<_>>();
+        results.sort_by_key(|entry| entry.path.clone());
+
+        assert_eq!(results.len(), 3);
+
+        assert_eq!(results[0].path, tempdir.path().join("foo"));
+        assert!(results[0].metadata.is_dir());
+
+        assert_eq!(results[1].path, tempdir.path().join("foo").join("abc"));
+        assert!(results[1].metadata.is_file());
+
+        assert_eq!(results[2].path, tempdir.path().join("foo").join("def"));
+        assert!(results[1].metadata.is_file());
+    }
+
+    #[cfg(target_family = "unix")]
+    #[test]
+    fn test_walk_with_dir_symlinks() {
+        let tempdir = tempfile::tempdir().unwrap();
+        let dir = tempdir.path().join("abc");
+        let file = dir.join("def");
+        let symlink = tempdir.path().join("ghi");
+
+        std::fs::create_dir(&dir).unwrap();
+        File::create(&file).unwrap();
+        std::os::unix::fs::symlink(&dir, &symlink).unwrap();
+
+        let mut results = walk(&tempdir).unwrap().collect::<Vec<_>>();
+        results.sort_by_key(|entry| entry.path.clone());
+
+        assert_eq!(results.len(), 3);
+
+        assert_eq!(results[0].path, dir);
+        assert!(results[0].metadata.file_type().is_dir());
+
+        assert_eq!(results[1].path, file);
+        assert!(results[1].metadata.file_type().is_file());
+
+        assert_eq!(results[2].path, symlink);
+        assert!(results[2].metadata.file_type().is_symlink());
+    }
+
+    #[cfg(target_family = "unix")]
+    #[test]
+    fn test_walk_with_circular_symlinks() {
+        let tempdir = tempfile::tempdir().unwrap();
+        let dir = tempdir.path().join("foo");
+        let symlink = tempdir.path().join("foo").join("bar");
+
+        std::fs::create_dir(&dir).unwrap();
+        std::os::unix::fs::symlink(&dir, &symlink).unwrap();
+
+        let mut results = walk(&tempdir).unwrap().collect::<Vec<_>>();
+        results.sort_by_key(|entry| entry.path.clone());
+
+        assert_eq!(results.len(), 2);
+
+        assert_eq!(results[0].path, dir);
+        assert!(results[0].metadata.file_type().is_dir());
+
+        assert_eq!(results[1].path, symlink);
+        assert!(results[1].metadata.file_type().is_symlink());
+    }
+
+    #[test]
+    fn test_walk_with_unicode_names() {
+        let tempdir = tempfile::tempdir().unwrap();
+        File::create(tempdir.path().join("zażółć gęślą jaźń")).unwrap();
+
+        let mut results = walk(&tempdir).unwrap().collect::<Vec<_>>();
+        results.sort_by_key(|entry| entry.path.clone());
+
+        assert_eq!(results.len(), 1);
+        assert_eq!(results[0].path, tempdir.path().join("zażółć gęślą jaźń"));
+    }
+
+    #[test]
+    fn test_walk_metadata_size() {
+        let tempdir = tempfile::tempdir().unwrap();
+        std::fs::write(tempdir.path().join("foo"), b"123456789").unwrap();
+
+        let mut iter = walk(&tempdir).unwrap();
+
+        let entry = iter.next().unwrap();
+        assert_eq!(entry.metadata.len(), 9);
+
+        assert!(iter.next().is_none());
+    }
 }
