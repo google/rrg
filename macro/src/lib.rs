33c1cc7b1f54bab410a538a2ef2682ec2d02f3de
Łukasz Hanuszczak <hanuszczak@google.com>
2020-09-18 18:43:01 +0200

    Create RRG-specific wrappers for logging macros.

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -2,3 +2,38 @@
 //
 // Use of this source code is governed by an MIT-style license that can be found
 // in the LICENSE file or at https://opensource.org/licenses/MIT.
+
+#[macro_export]
+macro_rules! error {
+    ($($arg:tt)*) => {
+        ::log::error!(target: "rrg", $($arg)*);
+    }
+}
+
+#[macro_export]
+macro_rules! warn {
+    ($($arg:tt)*) => {
+        ::log::warn!(target: "rrg", $($arg)*);
+    }
+}
+
+#[macro_export]
+macro_rules! info {
+    ($($arg:tt)*) => {
+        ::log::info!(target: "rrg", $($arg)*);
+    }
+}
+
+#[macro_export]
+macro_rules! debug {
+    ($($arg:tt)*) => {
+        ::log::debug!(target: "rrg", $($arg)*);
+    }
+}
+
+#[macro_export]
+macro_rules! trace {
+    ($($arg:tt)*) => {
+        ::log::trace!(target: "rrg", $($arg)*);
+    }
+}
