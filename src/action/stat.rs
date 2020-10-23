f07062b4fdd6ece3c1cb0f460c48dfc7384f2a84
Łukasz Hanuszczak <hanuszczak@google.com>
2020-10-23 19:51:26 +0200

    Remove no longer used code.

--- src/action/stat.rs
+++ src/action/stat.rs
@@ -7,13 +7,10 @@
 //!
 //! A file stat action responses with stat of a given file
 
-use std::fs;
 use std::fs::Metadata;
-use std::path::{Path, PathBuf};
-use std::time::{SystemTime};
+use std::path::PathBuf;
 
 use log::warn;
-use rrg_proto::{GetFileStatRequest, path_spec::Options, path_spec::PathType, PathSpec, StatEntry};
 
 use crate::session::{self, Error, Session};
 
@@ -96,63 +93,9 @@ pub fn handle<S: Session>(session: &mut S, request: Request) -> session::Result<
     Ok(())
 }
 
-#[cfg(target_os = "linux")]
-fn get_time_option<E: std::fmt::Display>(time: Result<SystemTime, E>) -> Option<SystemTime> {
-    match time {
-        Ok(time_value) => Some(time_value),
-        Err(err) => {
-            warn!("Unable to get time value: {}", err);
-            None
-        }
-    }
-}
-
-fn get_time_since_unix_epoch(sys_time: &Option<SystemTime>) -> Option<u64> {
-    match sys_time {
-        Some(time_value) => match rrg_proto::micros(*time_value) {
-            Ok(micros_value) => Some(micros_value),
-            Err(error) => {
-                warn!("failed to convert time: {}", error);
-                None
-            }
-        }
-        None => None
-    }
-}
-
-#[cfg(target_os = "linux")]
-fn get_status_change_time(metadata: &fs::Metadata) -> Option<SystemTime> {
-    use std::time::Duration;
-    use std::os::unix::fs::MetadataExt;
-    use std::time::UNIX_EPOCH;
-
-    UNIX_EPOCH.checked_add(Duration::from_secs(metadata.ctime() as u64))
-}
-
-fn collapse_pathspec(pathspec: PathSpec) -> PathBuf {
-    fn recursive_collapse(pathspec: PathSpec) -> PathBuf {
-        match pathspec.path {
-            Some(path) => {
-                let path_buf = PathBuf::from(path);
-                match pathspec.nested_path {
-                    Some(nested_path_box) => path_buf.join(recursive_collapse(*nested_path_box)),
-                    None => path_buf,
-                }
-            }
-            None => PathBuf::default()
-        }
-    }
-
-    let mut result = recursive_collapse(pathspec);
-    if !result.has_root() {
-        result = PathBuf::from("/").join(result);
-    }
-    result
-}
-
 impl super::Request for Request {
 
-    type Proto = GetFileStatRequest;
+    type Proto = rrg_proto::GetFileStatRequest;
 
     fn from_proto(proto: Self::Proto) -> Result<Self, session::ParseError> {
         use std::convert::TryInto as _;
@@ -173,7 +116,7 @@ impl super::Response for Response {
 
     const RDF_NAME: Option<&'static str> = Some("StatEntry");
 
-    type Proto = StatEntry;
+    type Proto = rrg_proto::StatEntry;
 
     fn into_proto(self) -> Self::Proto {
         use rrg_proto::convert::IntoLossy as _;
@@ -189,48 +132,4 @@ impl super::Response for Response {
 
 #[cfg(test)]
 mod tests {
-    use tempfile::tempdir;
-
-    use crate::action::Request;
-
-    use super::*;
-
-    #[test]
-    fn test_path_collapse() {
-        let pathspec = PathSpec {
-            nested_path: Some(Box::new(
-                PathSpec {
-                    nested_path: Some(Box::new(
-                        PathSpec {
-                            path: Some(String::from("file")),
-                            ..Default::default()
-                        }
-                    )),
-                    path: Some(String::from("to")),
-                    ..Default::default()
-                })),
-            path: Some(String::from("path")),
-            ..Default::default()
-        };
-
-        assert_eq!(collapse_pathspec(pathspec), PathBuf::from("/path/to/file"));
-
-        let pathspec = PathSpec {
-            nested_path: Some(Box::new(
-                PathSpec {
-                    nested_path: Some(Box::new(
-                        PathSpec {
-                            path: Some(String::from("on/device")),
-                            ..Default::default()
-                        }
-                    )),
-                    path: Some(String::from("some/file")),
-                    ..Default::default()
-                })),
-            path: Some(String::from("path/to")),
-            ..Default::default()
-        };
-
-        assert_eq!(collapse_pathspec(pathspec), PathBuf::from("/path/to/some/file/on/device"));
-    }
 }
