df3f8c1f0cdec6d6fb6be9e7b728bb858d57a87e
Maciej Oziebly <spawek@google.com>
2020-10-30 16:22:13 +0100

    Minor comment improvements.

--- src/action/finder/glob.rs
+++ src/action/finder/glob.rs
@@ -7,8 +7,13 @@ use crate::session::RegexParseError;
 use regex::Regex;
 
 /// Converts glob expression into a Rust Regex.
-/// E.g. "asd*[!123]??" will be converted into "asd.*[^123]..".
-/// This implementation is a Rust port of the cpython code:
+///
+/// # Examples
+/// ```
+/// assert_eq!(glob_to_regex("asd*[!12]??").unwrap().as_str(), "asd.*[^12]..");
+/// ```
+///
+/// This implementation is inspired by CPython code:
 /// https://github.com/python/cpython/blob/2.7/Lib/fnmatch.py
 fn glob_to_regex(pat: &str) -> Result<Regex, RegexParseError> {
     let chars: Vec<char> = pat.chars().collect();
@@ -51,7 +56,8 @@ fn glob_to_regex(pat: &str) -> Result<Regex, RegexParseError> {
         }
     }
 
-    // cpython version produces output with escaped slashes, which is not desired here.
+    // CPython version produces output with escaped slashes, which is
+    // not desired here.
     res = res.replace(r"\\", r"\");
 
     match Regex::new(&res) {
@@ -68,7 +74,6 @@ mod tests {
 
     #[test]
     fn test_glob_to_regex() {
-        // Original cpython tests:
         assert_eq!(glob_to_regex("*").unwrap().as_str(), ".*");
         assert_eq!(glob_to_regex("?").unwrap().as_str(), ".");
         assert_eq!(glob_to_regex("a?b*").unwrap().as_str(), "a.b.*");
@@ -77,8 +82,6 @@ mod tests {
         assert_eq!(glob_to_regex("[!x]").unwrap().as_str(), "[^x]");
         assert_eq!(glob_to_regex("[^x]").unwrap().as_str(), r"[\^x]");
         assert_eq!(glob_to_regex("[x").unwrap().as_str(), r"\[x");
-
-        // Additional tests:
         assert_eq!(glob_to_regex("[a]]").unwrap().as_str(), r"[a]\]");
         assert_eq!(glob_to_regex(r"[\\]\\").unwrap().as_str(), r"[\\]\\");
         assert_eq!(glob_to_regex("ąźć").unwrap().as_str(), "ąźć");
