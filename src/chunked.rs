726a61dca6e6d98c08873bc0a0cd3a6701c4eed8
Łukasz Hanuszczak <hanuszczak@google.com>
2020-11-03 14:45:33 +0100

    Create a streaming decoder for the chunked format.

--- src/chunked.rs
+++ src/chunked.rs
@@ -46,3 +46,65 @@ where
         self.cur.read(buf)
     }
 }
+
+struct Decode<R> {
+    buf: R,
+}
+
+impl<R: std::io::Read> Decode<R> {
+
+    fn read_len(&mut self) -> std::io::Result<Option<usize>> {
+        use byteorder::ReadBytesExt as _;
+
+        let mut buf = [0; 8];
+        match self.buf.read(&mut buf[..])? {
+            8 => (),
+            0 => return Ok(None),
+            _ => return Err(SizeTagError.into()),
+        }
+
+        let len = (&buf[..]).read_u64::<LittleEndian>()? as usize;
+        Ok(Some(len))
+    }
+}
+
+impl<R: std::io::Read> Iterator for Decode<R> {
+
+    type Item = std::io::Result<Vec<u8>>;
+
+    fn next(&mut self) -> Option<std::io::Result<Vec<u8>>> {
+        let len = match self.read_len() {
+            Ok(Some(len)) => len,
+            Ok(None) => return None,
+            Err(error) => return Some(Err(error)),
+        };
+
+        let mut buf = vec!(0; len);
+        match self.buf.read_exact(&mut buf[..]) {
+            Ok(()) => (),
+            Err(error) => return Some(Err(error)),
+        }
+
+        Some(Ok(buf))
+    }
+}
+
+#[derive(Debug)]
+struct SizeTagError;
+
+impl std::fmt::Display for SizeTagError {
+
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(fmt, "incorrect length of the size tag")
+    }
+}
+
+impl std::error::Error for SizeTagError {
+}
+
+impl From<SizeTagError> for std::io::Error {
+
+    fn from(error: SizeTagError) -> std::io::Error {
+        std::io::Error::new(std::io::ErrorKind::InvalidData, error)
+    }
+}
