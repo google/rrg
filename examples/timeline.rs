ea5a4760117dc38f7b70b835bd31397e8c18cef9
Łukasz Hanuszczak <hanuszczak@google.com>
2020-12-29 13:16:37 +0100

    Write documentation for the timelining example.

--- examples/timeline.rs
+++ examples/timeline.rs
@@ -1,27 +1,49 @@
+//! An example that timelines the specified folder.
+//!
+//! The timeline action is one of the filesystem tools that the GRR agent
+//! exposes. Its description is pretty simple: given a folder, it returns a
+//! stream of stat entries for all the files in it (including all subfolders).
+//!
+//! This example is a tiny wrapper around this action that allows to execute it
+//! as a standalone binary without all the RRG setup required. A primary reason
+//! for creating it is to compare the efficiency Rust implementation against the
+//! existing Python one.
+
 use std::fs::File;
 use std::path::{Path, PathBuf};
 
 use rrg::session::Sink;
 use structopt::StructOpt;
 
+/// A type for the timelining example command-line arguments.
 #[derive(StructOpt)]
 #[structopt(name = "timeline", about = "A binary for the timeline action.")]
 struct Opts {
+    /// A path to the root directory to timeline.
     #[structopt(long="root", name="FILE", default_value="/",
                 help="A path to the root directory to timeline.")]
     root: PathBuf,
 
+    /// A path to a file to dump the results into.
     #[structopt(long="output", about="FILE",
                 help="A path to a file to dump the results into.")]
     output: PathBuf,
 }
 
+/// A session type of the timeline example.
+///
+/// Every action handler needs a session object. "Real" sessions handle comms
+/// with the GRR server, error reporting and alike. However, this is not needed
+/// here and the example session simply dumps everything into the specified file
+/// encoding data in the gzchunked format.
 struct Session {
+    /// A file into which the data chunks of the output are dumped.
     output: File,
 }
 
 impl Session {
 
+    /// Creates a session object that will write to the given `output` path.
     fn open<P: AsRef<Path>>(output: P) -> Session {
         let output = File::create(output)
             .expect("failed to create the output file");
