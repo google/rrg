a360cdbec533339715c1f6c423efa11112b57530
Maciej Oziebly <spawek@google.com>
2021-01-29 14:10:25 +0100

    Fix hash returning byte hexes instead of just result bytes.

--- src/action/finder/hash.rs
+++ src/action/finder/hash.rs
@@ -8,6 +8,7 @@ use rrg_proto::Hash as HashEntry;
 use std::cmp::min;
 use std::fs::File;
 use std::io::Read;
+use std::iter::repeat;
 
 /// Hashes data writen to it using SHA-1, SHA-256 and MD5 algorithms.
 struct Hasher {
@@ -54,38 +55,32 @@ pub fn hash(entry: &Entry, config: &HashActionOptions) -> Option<HashEntry> {
     };
 
     let file = ack! {
-    File::open(&entry.path), error: "failed to open file: {}, error: {}",
-        entry.path.display(),
-        err}?;
+        File::open(&entry.path),
+        error: "failed to open file: {}", entry.path.display()
+    }?;
     let mut file = file.take(config.max_size);
 
     let mut hasher = Hasher::new();
-    match std::io::copy(&mut file, &mut hasher) {
-        Ok(read_bytes) => {
-            let expected_bytes = min(entry.metadata.len(), config.max_size);
-            if read_bytes != expected_bytes {
-                warn!(
-                    "failed to read all data from: {}, {} bytes were read, but {} were expected",
-                    entry.path.display(),
-                    &read_bytes,
-                    expected_bytes
-                );
-                return None;
-            }
-        }
-        Err(err) => {
-            warn!(
-                "failed to copy data from: {}. Error: {}",
-                entry.path.display(),
-                &err
-            );
-        }
+    let read_bytes = ack! {
+        std::io::copy(&mut file, &mut hasher),
+        error: "failed to copy data from: {}", entry.path.display()
+    }?;
+
+    let expected_bytes = min(entry.metadata.len(), config.max_size);
+    if read_bytes != expected_bytes {
+        warn!(
+            "failed to read all data from: {}, {} bytes were read, but {} were expected",
+            entry.path.display(),
+            &read_bytes,
+            expected_bytes
+        );
+        return None;
     }
 
     Some(HashEntry {
-        sha1: Some(hasher.sha1.result_str().as_bytes().to_vec()),
-        sha256: Some(hasher.sha256.result_str().as_bytes().to_vec()),
-        md5: Some(hasher.md5.result_str().as_bytes().to_vec()),
+        sha1: Some(result_vec(&mut hasher.sha1)),
+        sha256: Some(result_vec(&mut hasher.sha256)),
+        md5: Some(result_vec(&mut hasher.md5)),
         pecoff_sha1: None,
         pecoff_md5: None,
         pecoff_sha256: None,
@@ -95,6 +90,12 @@ pub fn hash(entry: &Entry, config: &HashActionOptions) -> Option<HashEntry> {
     })
 }
 
+fn result_vec<T: Digest>(digest: &mut T) -> Vec<u8> {
+    let mut vec = repeat(0).take(digest.output_bytes()).collect::<Vec<u8>>();
+    digest.result(&mut vec);
+    vec
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -102,10 +103,9 @@ mod tests {
 
     #[test]
     fn test_hash_values() {
-        let test_string = "some_test_data";
         let tempdir = tempfile::tempdir().unwrap();
         let path = tempdir.path().join("f");
-        std::fs::write(&path, &test_string).unwrap();
+        std::fs::write(&path, "some_test_data").unwrap();
         let entry = Entry {
             metadata: path.metadata().unwrap(),
             path: path,
@@ -116,35 +116,45 @@ mod tests {
             &HashActionOptions {
                 max_size: 14,
                 oversized_file_policy: OversizedFilePolicy::Skip,
+                collect_ext_attrs: false,
             },
         )
         .unwrap();
 
         assert_eq!(
             result.sha1.unwrap(),
-            "a62a6d5991238ae72d81fe6e4769b3043d9fe670"
-                .as_bytes()
-                .to_vec()
+            vec![
+                0xa6, 0x2a, 0x6d, 0x59, 0x91, 0x23, 0x8a, 0xe7, 0x2d, 0x81,
+                0xfe, 0x6e, 0x47, 0x69, 0xb3, 0x04, 0x3d, 0x9f, 0xe6, 0x70
+            ]
+            .to_vec()
         );
         assert_eq!(
             result.sha256.unwrap(),
-            "d76d85adca8afad205edebc11f9b5086bca75acb512a748bc79660e1346af546"
-                .as_bytes()
-                .to_vec()
+            vec![
+                0xd7, 0x6d, 0x85, 0xad, 0xca, 0x8a, 0xfa, 0xd2, 0x05, 0xed,
+                0xeb, 0xc1, 0x1f, 0x9b, 0x50, 0x86, 0xbc, 0xa7, 0x5a, 0xcb,
+                0x51, 0x2a, 0x74, 0x8b, 0xc7, 0x96, 0x60, 0xe1, 0x34, 0x6a,
+                0xf5, 0x46
+            ]
+            .to_vec()
         );
         assert_eq!(
             result.md5.unwrap(),
-            "e091b6f1a233049d22d2807fa8086f3f".as_bytes().to_vec()
+            vec![
+                0xe0, 0x91, 0xb6, 0xf1, 0xa2, 0x33, 0x04, 0x9d, 0x22, 0xd2,
+                0x80, 0x7f, 0xa8, 0x08, 0x6f, 0x3f
+            ]
+            .to_vec()
         );
         assert_eq!(result.num_bytes.unwrap(), 14);
     }
 
     #[test]
     fn test_trim_file_over_max_size() {
-        let test_string = "some_test_data";
         let tempdir = tempfile::tempdir().unwrap();
         let path = tempdir.path().join("f");
-        std::fs::write(&path, &test_string).unwrap();
+        std::fs::write(&path, "some_test_data").unwrap();
         let entry = Entry {
             metadata: path.metadata().unwrap(),
             path,
@@ -155,6 +165,7 @@ mod tests {
             &HashActionOptions {
                 max_size: 10,
                 oversized_file_policy: OversizedFilePolicy::HashTruncated,
+                collect_ext_attrs: false,
             },
         )
         .unwrap();
@@ -164,10 +175,9 @@ mod tests {
 
     #[test]
     fn test_skip_file_over_max_size() {
-        let test_string = "some_test_data";
         let tempdir = tempfile::tempdir().unwrap();
         let path = tempdir.path().join("f");
-        std::fs::write(&path, &test_string).unwrap();
+        std::fs::write(&path, "some_test_data").unwrap();
         let entry = Entry {
             metadata: path.metadata().unwrap(),
             path,
@@ -177,7 +187,8 @@ mod tests {
             &entry,
             &HashActionOptions {
                 max_size: 10,
-                oversized_file_policy: OversizedFilePolicy::Skip
+                oversized_file_policy: OversizedFilePolicy::Skip,
+                collect_ext_attrs: false,
             },
         )
         .is_none());
