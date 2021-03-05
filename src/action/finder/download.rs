781a47bbf54cc861a6025485363faacf3642bcf4
Maciej Oziebly <spawek@google.com>
2021-03-05 13:56:51 +0100

    Move chunks to separate file.

--- src/action/finder/download.rs
+++ src/action/finder/download.rs
@@ -1,3 +1,4 @@
+use crate::action::finder::chunks::{chunks, Chunks, ChunksConfig};
 use crate::action::finder::request::{
     DownloadActionOptions, HashActionOptions,
 };
@@ -5,6 +6,7 @@ use crate::fs::Entry;
 use log::warn;
 use rrg_proto::file_finder_download_action_options::OversizedFilePolicy as DownloadOversizedFilePolicy;
 use rrg_proto::file_finder_hash_action_options::OversizedFilePolicy as HashOversizedFilePolicy;
+use sha2::{Digest, Sha256};
 use std::fs::File;
 use std::io::{BufReader, Read, Take};
 
@@ -20,10 +22,7 @@ pub enum Response {
 
 /// Performs `download` action logic and returns file contents to be uploaded
 /// or another action to be executed.
-pub fn download(
-    entry: &Entry,
-    config: &DownloadActionOptions,
-) -> Response {
+pub fn download(entry: &Entry, config: &DownloadActionOptions) -> Response {
     if entry.metadata.len() > config.max_size {
         match config.oversized_file_policy {
             DownloadOversizedFilePolicy::Skip => {
@@ -54,48 +53,70 @@ pub fn download(
     };
 
     let reader = BufReader::new(file);
-    Response::DownloadData(chunks(reader, config.chunk_size))
+    Response::DownloadData(chunks(
+        reader,
+        ChunksConfig {
+            bytes_per_chunk: config.chunk_size,
+            overlap_bytes: 0,
+        },
+    ))
 }
 
-/// Implements `Iterator` trait splitting underlying `bytes` into chunks.
-#[derive(Debug)]
-pub struct Chunks<R> {
-    /// Data source for the chunks.
-    data: std::io::Bytes<R>,
-    /// Desired number of bytes in chunks. Only the last chunk can be smaller
-    /// than the `bytes_per_chunk`
-    bytes_per_chunk: u64,
+/// A type representing unique identifier of a given chunk.
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct ChunkId {
+    /// A SHA-256 digest of the referenced chunk data.
+    sha256: [u8; 32],
+    offset: u64,
+    length: u64,
 }
 
-impl<R: std::io::Read> std::iter::Iterator for Chunks<R> {
-    type Item = std::io::Result<Vec<u8>>;
-
-    fn next(&mut self) -> Option<std::io::Result<Vec<u8>>> {
-        let mut ret = vec![];
-        for byte in &mut self.data {
-            let byte = match byte {
-                Ok(byte) => byte,
-                Err(err) => return Some(Err(err)),
-            };
-            ret.push(byte);
-
-            if ret.len() == self.bytes_per_chunk as usize {
-                return Some(Ok(ret));
-            }
+impl ChunkId {
+    /// Creates a chunk identifier for the given chunk.
+    pub fn make(chunk: &Vec<u8>, offset: u64) -> ChunkId {
+        ChunkId {
+            sha256: Sha256::digest(&chunk).into(),
+            length: chunk.len() as u64,
+            offset,
         }
-        if !ret.is_empty() {
-            return Some(Ok(ret));
-        }
-
-        return None;
     }
 }
 
-/// Returns an iterator over `reader` returning chunks of bytes.
-fn chunks<R: std::io::Read>(reader: R, bytes_per_chunk: u64) -> Chunks<R> {
-    Chunks {
-        data: reader.bytes(),
-        bytes_per_chunk,
+#[derive(Debug)]
+pub struct DownloadEntry {
+    pub chunk_ids: Vec<ChunkId>,
+    pub chunk_size: u64,
+}
+
+impl From<DownloadEntry> for rrg_proto::BlobImageDescriptor {
+    fn from(entry: DownloadEntry) -> rrg_proto::BlobImageDescriptor {
+        rrg_proto::BlobImageDescriptor {
+            chunks: entry
+                .chunk_ids
+                .into_iter()
+                .map(|x| rrg_proto::BlobImageChunkDescriptor {
+                    offset: Some(x.offset),
+                    length: Some(x.length),
+                    digest: Some(x.sha256.to_vec()),
+                })
+                .collect::<Vec<_>>(),
+            chunk_size: Some(entry.chunk_size),
+        }
+    }
+}
+
+/// A type representing a particular chunk of the returned timeline.
+pub struct Chunk {
+    pub data: Vec<u8>,
+}
+
+impl super::super::Response for Chunk {
+    const RDF_NAME: Option<&'static str> = Some("DataBlob");
+
+    type Proto = rrg_proto::DataBlob;
+
+    fn into_proto(self) -> rrg_proto::DataBlob {
+        self.data.into()
     }
 }
 
@@ -259,5 +280,18 @@ mod tests {
             "e_".bytes().collect::<Vec<_>>()
         );
         assert!(chunks.next().is_none());
+    }
+
+    #[test]
+    fn test_chunk_id() {
+        let chunk = ChunkId::make(&"some_test_data".as_bytes().to_vec(), 5);
+        assert_eq!(&chunk.length, &14);
+        assert_eq!(&chunk.offset, &5);
+        assert_eq!(&chunk.sha256, &[
+            0xd7, 0x6d, 0x85, 0xad, 0xca, 0x8a, 0xfa, 0xd2, 0x05, 0xed,
+            0xeb, 0xc1, 0x1f, 0x9b, 0x50, 0x86, 0xbc, 0xa7, 0x5a, 0xcb,
+            0x51, 0x2a, 0x74, 0x8b, 0xc7, 0x96, 0x60, 0xe1, 0x34, 0x6a,
+            0xf5, 0x46
+        ]);
     }
 }
