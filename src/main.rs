5f00613d0bc2b09b5daf186ce90de3431b68356e
Łukasz Hanuszczak <hanuszczak@google.com>
2022-07-08 12:10:20 +0200

    Improve logging in the main function.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@
 // Use of this source code is governed by an MIT-style license that can be found
 // in the LICENSE file or at https://opensource.org/licenses/MIT.
 
-use log::{error, info};
-
 use rrg::action;
 use rrg::session;
 use rrg::opts::{self, Opts};
@@ -13,18 +11,21 @@ fn main() {
     let opts = opts::from_args();
     init(&opts);
 
+    log::info!("sending Fleetspeak startup information");
     fleetspeak::startup(env!("CARGO_PKG_VERSION"))
         .expect("failed to initialize Fleetspeak connection");
 
+    log::info!("sending RRG startup information");
     match action::startup::handle(&mut session::Adhoc, ()) {
         Err(error) => {
-            error!("failed to collect startup information: {}", error);
+            log::error!("failed to collect startup information: {}", error);
         }
         Ok(()) => {
-            info!("successfully sent startup information");
+            log::info!("successfully sent startup information");
         }
     }
 
+    log::info!("listening for messages");
     rrg::listen(&opts);
 }
 
