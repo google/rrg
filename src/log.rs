fbcaeda22034e19546875af118a3118f42e6cf74
Łukasz Hanuszczak <hanuszczak@google.com>
2022-09-23 15:05:19 +0200

    Add missing license header to the `log` module.

--- src/log.rs
+++ src/log.rs
@@ -1,3 +1,8 @@
+// Copyright 2020 Google LLC
+//
+// Use of this source code is governed by an MIT-style license that can be found
+// in the LICENSE file or at https://opensource.org/licenses/MIT.
+
 /// Initializes the logging submodule.
 ///
 /// This function should be called only once at the beginning of the process
