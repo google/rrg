80a516f30a7d805517e3389f9cef52c539b013f7
Łukasz Hanuszczak <hanuszczak@google.com>
2022-10-01 19:49:49 +0200

    Define parcel types.

--- src/session/sink.rs
+++ src/session/sink.rs
@@ -16,6 +16,53 @@
 use crate::action;
 use crate::session;
 
+/// Data sent to a server not belonging to a particular action call.
+///
+/// Sometimes the agent should send data to the server that is not connected
+/// with a particular flow request. An example can be the agent startup data
+/// sent to the server regardless of whether there was a request for it or not.
+/// Another example can be file contents that are delivered to the server not
+/// as part of the flow results but go to a separate store that has logic for
+/// deduplicating data that we already collected in the past.
+pub trait Parcel {
+    /// Low-level Protocol Buffers type representing the parcel data.
+    type Protobuf: protobuf::Message;
+
+    /// A name of the corresponding RDF class in GRR.
+    const RDF_NAME: &'static str;
+
+    /// Converts the parcel to its low-level representation.
+    fn into_proto(self) -> Self::Protobuf;
+
+    /// Converts a Protocol Buffers message to high-level type.
+    fn from_proto(message: Self::Protobuf) -> Self;
+}
+
+/// A parcel addressed to a particular server-side sink.
+pub struct AddressedParcel<P: Parcel> {
+    /// Destination of the parcel.
+    dest: Sink,
+    /// Actual parcel.
+    parcel: P,
+}
+
+impl<P: Parcel> std::convert::TryInto<rrg_proto::jobs::GrrMessage> for AddressedParcel<P> {
+
+    type Error = protobuf::ProtobufError;
+
+    fn try_into(self) -> Result<rrg_proto::jobs::GrrMessage, protobuf::ProtobufError> {
+        let mut message = rrg_proto::jobs::GrrMessage::new();
+        message.set_session_id(String::from(self.dest.id));
+        message.set_field_type(rrg_proto::jobs::GrrMessage_Type::MESSAGE);
+        message.set_args_rdf_name(String::from(P::RDF_NAME));
+
+        let proto = self.parcel.into_proto();
+        message.set_args(protobuf::Message::write_to_bytes(&proto)?);
+
+        Ok(message)
+    }
+}
+
 /// Handle to a specific sink.
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Hash)]
 pub struct Sink {
@@ -34,6 +81,15 @@ impl Sink {
     // TODO: Refactor sinks to use custom `Message` type rather than `Response`
     // and make response always have `request_id` and `response_id` fields.
 
+    /// Adresses a given `parcel` to this sink.
+    pub fn address<P: Parcel>(&self, parcel: P) -> AddressedParcel<P> {
+        AddressedParcel {
+            dest: *self,
+            parcel,
+        }
+    }
+
+    // TODO: Remove the function below once only parcels are used.
     /// Wraps an action response to a sink-specific session response.
     pub fn wrap<R>(&self, response: R) -> session::Response<R>
     where
