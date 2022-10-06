053607e011b4b6e56ffeea4baf509cfe871c1db5
Łukasz Hanuszczak <hanuszczak@google.com>
2022-10-06 09:08:10 +0200

    Implement proto conversions for message wrappers.

--- src/comms/request.rs
+++ src/comms/request.rs
@@ -12,9 +12,9 @@ pub struct Request {
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct RequestId {
     /// A server-issued session identifier (usually corresponds to a flow).
-    session_id: String,
+    pub(super) session_id: String,
     /// A server-issued request identifier.
-    request_id: u64,
+    pub(super) request_id: u64,
 }
 
 impl Request {
@@ -36,4 +36,91 @@ impl Request {
 
         A::from_proto(proto_args)
     }
+}
+
+impl std::convert::TryFrom<rrg_proto::jobs::GrrMessage> for Request {
+
+    type Error = ParseRequestError;
+
+    fn try_from(mut proto: rrg_proto::jobs::GrrMessage) -> Result<Request, ParseRequestError> {
+        use ParseRequestErrorKind::*;
+
+        if !proto.has_session_id() {
+            return Err(NoSessionId.into());
+        }
+        if !proto.has_request_id() {
+            return Err(NoRequestId.into());
+        }
+        let request_id = RequestId {
+            session_id: proto.take_session_id(),
+            request_id: proto.get_request_id(),
+        };
+
+        if !proto.has_name() {
+            return Err(NoActionName.into());
+        }
+        let action = proto.take_name();
+
+        let serialized_args = if proto.has_args() {
+            Some(proto.take_args())
+        } else {
+            None
+        };
+
+        Ok(Request {
+            id: request_id,
+            action: action,
+            serialized_args,
+        })
+    }
+}
+
+/// The error type for cases when action request parsing fails.
+#[derive(Debug)]
+pub struct ParseRequestError {
+    /// A corresponding [`ParseRequestErrorKind`] of this error.
+    kind: ParseRequestErrorKind,
+}
+
+/// Kinds of errors that can happen when parsing an action request.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum ParseRequestErrorKind {
+    /// No session identifier in the action request protobuf message.
+    NoSessionId,
+    /// No request identifier in the action request protobuf message.
+    NoRequestId,
+    /// No action name in the action request protobuf message.
+    NoActionName,
+}
+
+impl ParseRequestErrorKind {
+
+    fn as_str(&self) -> &'static str {
+        use ParseRequestErrorKind::*;
+
+        match *self {
+            NoSessionId => "no session id in the action request",
+            NoRequestId => "no request id in the action request",
+            NoActionName => "no action name in the action request",
+        }
+    }
+}
+
+impl From<ParseRequestErrorKind> for ParseRequestError {
+
+    fn from(kind: ParseRequestErrorKind) -> Self {
+        ParseRequestError {
+            kind,
+        }
+    }
+}
+
+impl std::fmt::Display for ParseRequestError {
+
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(fmt, "{}", self.kind.as_str())
+    }
+}
+
+impl std::error::Error for ParseRequestError {
 }
--- src/comms/response.rs
+++ src/comms/response.rs
@@ -33,7 +33,7 @@ pub struct Status<E: std::error::Error> {
 
 /// A unique identifier of a response.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-pub struct ResponseId(u64);
+pub struct ResponseId(pub(super) u64);
 
 /// Response factory for building many responses to a single request.
 pub struct ResponseBuilder {
@@ -84,5 +84,80 @@ impl ResponseBuilder {
             response_id,
             item,
         }
+    }
+}
+
+impl<I> Into<rrg_proto::jobs::GrrMessage> for Item<I>
+where
+    I: crate::action::Item,
+{
+    fn into(self) -> rrg_proto::jobs::GrrMessage {
+        use protobuf::Message as _;
+
+        let mut proto = rrg_proto::jobs::GrrMessage::new();
+        proto.set_session_id(self.request_id.session_id);
+        proto.set_request_id(self.request_id.request_id);
+        proto.set_response_id(self.response_id.0);
+        proto.set_field_type(rrg_proto::jobs::GrrMessage_Type::MESSAGE);
+
+        let serialized_item = self.item
+            .into_proto()
+            .write_to_bytes()
+            // It is not clear what should we do in case of an error. It is very
+            // hard to imagine a scenario when serialization fails so for now we
+            // just fail hard. If we observe any problems with this assumption,
+            // we can always change this behaviour.
+            .expect("failed to serialize an action item");
+
+        // The protobuf message uses a field named "args" for storing the action
+        // item, so of course we repeat that. One day this should be improved,
+        // hopefully.
+        proto.set_args_rdf_name(String::from(I::RDF_NAME));
+        proto.set_args(serialized_item);
+
+        proto
+    }
+}
+
+impl<E> Into<rrg_proto::jobs::GrrMessage> for Status<E>
+where
+    E: std::error::Error,
+{
+    fn into(self) -> rrg_proto::jobs::GrrMessage {
+        use protobuf::Message as _;
+
+        let mut proto_status = rrg_proto::jobs::GrrStatus::new();
+        match self.result {
+            Ok(()) => {
+                proto_status.set_status(rrg_proto::jobs::GrrStatus_ReturnedStatus::OK);
+            },
+            Err(error) => {
+                // TODO(@panhania): Use more specific error types once we have
+                // custom error type for actions (see also the comment about the
+                // genericity of the `Status` type.
+                proto_status.set_status(rrg_proto::jobs::GrrStatus_ReturnedStatus::GENERIC_ERROR);
+                proto_status.set_error_message(error.to_string());
+            }
+        }
+
+        let serialized_status = proto_status
+            .write_to_bytes()
+            // See a comment in the conversion of the `Item` type for details on
+            // why we panic on errors here.
+            .expect("failed to serialized action status");
+
+        let mut proto = rrg_proto::jobs::GrrMessage::new();
+        proto.set_session_id(self.request_id.session_id);
+        proto.set_request_id(self.request_id.request_id);
+        proto.set_response_id(self.response_id.0);
+        proto.set_field_type(rrg_proto::jobs::GrrMessage_Type::STATUS);
+
+        // Again, for some reason GRR expects the status to be passed as as a
+        // serialized proto in a field named "args". This should be definitely
+        // fixed when designing the new protocol.
+        proto.set_args_rdf_name(String::from("GrrStatus"));
+        proto.set_args(serialized_status);
+
+        proto
     }
 }
