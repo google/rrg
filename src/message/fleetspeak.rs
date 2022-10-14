bc9bdd9539edc64617489d252b07ce2c6e9060d1
Łukasz Hanuszczak <hanuszczak@google.com>
2022-10-14 12:44:18 +0200

    Add license headers to the `message` submodules.

--- src/message/fleetspeak.rs
+++ src/message/fleetspeak.rs
@@ -1,3 +1,8 @@
+// Copyright 2020 Google LLC
+//
+// Use of this source code is governed by an MIT-style license that can be found
+// in the LICENSE file or at https://opensource.org/licenses/MIT.
+
 /// Awaits for the raw GRR Protocol Buffers message object from Fleetspeak.
 ///
 /// This function will block until the message is available. While waiting, it
