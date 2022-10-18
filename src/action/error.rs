956f165b1364a3dcc847d8a51d0e13a2d22c8bbf
Łukasz Hanuszczak <hanuszczak@google.com>
2022-10-18 13:37:36 +0200

    Move action errors to a separate module.

---
+++ src/action/error.rs
@@ -0,0 +1,130 @@
+// Copyright 2020 Google LLC
+//
+// Use of this source code is governed by an MIT-style license that can be found
+// in the LICENSE file or at https://opensource.org/licenses/MIT.
+
+/// The error type for cases when action argument parsing fails.
+#[derive(Debug)]
+pub struct ParseArgsError {
+    /// A corresponding [`ParseArgsErrorKind`] of this error.
+    kind: ParseArgsErrorKind,
+    /// A detailed payload associated with the error.
+    error: Box<dyn std::error::Error + Send + Sync>,
+}
+
+impl ParseArgsError {
+
+    /// Creates a new error instance caused by some invalid field error.
+    pub fn invalid_field<E>(error: E) -> ParseArgsError
+    where
+        E: std::error::Error + Send + Sync + 'static,
+    {
+        ParseArgsError {
+            kind: ParseArgsErrorKind::InvalidField,
+            error: Box::new(error),
+        }
+    }
+}
+
+/// Kinds of errors that can happen when parsing action arguments.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum ParseArgsErrorKind {
+    /// The serialized message with arguments was impossible to deserialize.
+    InvalidProto,
+    // TODO(panhania@): Augment with field name.
+    /// One of the fields of the arguments struct is invalid.
+    InvalidField,
+}
+
+impl ParseArgsErrorKind {
+
+    fn as_str(&self) -> &'static str {
+        use ParseArgsErrorKind::*;
+
+        match *self {
+            InvalidProto => "invalid serialized protobuf message",
+            InvalidField => "invalid argument field",
+        }
+    }
+}
+
+impl std::fmt::Display for ParseArgsError {
+
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(fmt, "{}: {}", self.kind.as_str(), self.error)
+    }
+}
+
+impl std::error::Error for ParseArgsError {
+
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        self.error.source()
+    }
+}
+
+impl From<protobuf::ProtobufError> for ParseArgsError {
+
+    fn from(error: protobuf::ProtobufError) -> Self {
+        ParseArgsError {
+            kind: ParseArgsErrorKind::InvalidProto,
+            error: Box::new(error),
+        }
+    }
+}
+
+/// The error type for cases when action dispatch (or execution) fails.
+#[derive(Debug)]
+pub struct DispatchError {
+    /// A corresponding [`DispatchErrorKind`] of this error.
+    kind: DispatchErrorKind,
+    /// A detailed error object.
+    error: Option<Box<dyn std::error::Error + Send + Sync>>,
+}
+
+/// Kinds of errors that can happen when dispatching an action.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum DispatchErrorKind {
+    /// The action handler is unknown or does not exist yet.
+    UnknownAction(&'static str),
+    /// The action arguments were invalid.
+    InvalidArgs,
+}
+
+impl std::fmt::Display for DispatchError {
+
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.error {
+            Some(ref error) => write!(fmt, "{}: {}", self.kind, error),
+            None => write!(fmt, "{}", self.kind),
+        }
+    }
+}
+
+impl std::error::Error for DispatchError {
+
+    fn cause(&self) -> Option<&dyn std::error::Error> {
+        self.error.as_deref().map(|error| error as &_)
+    }
+}
+
+impl std::fmt::Display for DispatchErrorKind {
+
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use DispatchErrorKind::*;
+
+        match *self {
+            UnknownAction(name) => write!(fmt, "unknown action '{}'", name),
+            InvalidArgs => write!(fmt, "invalid action arguments"),
+        }
+    }
+}
+
+impl From<ParseArgsError> for DispatchError {
+
+    fn from(error: ParseArgsError) -> DispatchError {
+        DispatchError {
+            kind: DispatchErrorKind::InvalidArgs,
+            error: Some(Box::new(error)),
+        }
+    }
+}
--- src/action/mod.rs
+++ src/action/mod.rs
@@ -15,6 +15,8 @@
 //! instance of the corresponding request type and send some (zero or more)
 //! instances of the corresponding response type.
 
+mod error;
+
 #[cfg(feature = "action-filesystems")]
 #[cfg(target_os = "linux")]
 pub mod filesystems;
@@ -47,6 +49,10 @@ pub mod memsize;
 #[cfg(feature = "action-finder")]
 pub mod finder;
 
+pub use error::{
+    ParseArgsError, ParseArgsErrorKind,
+    DispatchError, DispatchErrorKind,
+};
 use crate::session::{self, Session};
 
 /// Abstraction for action-specific requests.
@@ -220,131 +226,5 @@ impl Item for () {
 
     fn into_proto(self) -> protobuf::well_known_types::Empty {
         protobuf::well_known_types::Empty::new()
-    }
-}
-
-/// The error type for cases when action argument parsing fails.
-#[derive(Debug)]
-pub struct ParseArgsError {
-    /// A corresponding [`ParseArgsErrorKind`] of this error.
-    kind: ParseArgsErrorKind,
-    /// A detailed payload associated with the error.
-    error: Box<dyn std::error::Error + Send + Sync>,
-}
-
-impl ParseArgsError {
-
-    /// Creates a new error instance caused by some invalid field error.
-    fn invalid_field<E>(error: E) -> ParseArgsError
-    where
-        E: std::error::Error + Send + Sync + 'static,
-    {
-        ParseArgsError {
-            kind: ParseArgsErrorKind::InvalidField,
-            error: Box::new(error),
-        }
-    }
-}
-
-/// Kinds of errors that can happen when parsing action arguments.
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-pub enum ParseArgsErrorKind {
-    /// The serialized message with arguments was impossible to deserialize.
-    InvalidProto,
-    // TODO(panhania@): Augment with field name.
-    /// One of the fields of the arguments struct is invalid.
-    InvalidField,
-}
-
-impl ParseArgsErrorKind {
-
-    fn as_str(&self) -> &'static str {
-        use ParseArgsErrorKind::*;
-
-        match *self {
-            InvalidProto => "invalid serialized protobuf message",
-            InvalidField => "invalid argument field",
-        }
-    }
-}
-
-impl std::fmt::Display for ParseArgsError {
-
-    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(fmt, "{}: {}", self.kind.as_str(), self.error)
-    }
-}
-
-impl std::error::Error for ParseArgsError {
-
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        self.error.source()
-    }
-}
-
-impl From<protobuf::ProtobufError> for ParseArgsError {
-
-    fn from(error: protobuf::ProtobufError) -> Self {
-        ParseArgsError {
-            kind: ParseArgsErrorKind::InvalidProto,
-            error: Box::new(error),
-        }
-    }
-}
-
-/// The error type for cases when action dispatch (or execution) fails.
-#[derive(Debug)]
-pub struct DispatchError {
-    /// A corresponding [`DispatchErrorKind`] of this error.
-    kind: DispatchErrorKind,
-    /// A detailed error object.
-    error: Option<Box<dyn std::error::Error + Send + Sync>>,
-}
-
-/// Kinds of errors that can happen when dispatching an action.
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-pub enum DispatchErrorKind {
-    /// The action handler is unknown or does not exist yet.
-    UnknownAction(&'static str),
-    /// The action arguments were invalid.
-    InvalidArgs,
-}
-
-impl std::fmt::Display for DispatchError {
-
-    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self.error {
-            Some(ref error) => write!(fmt, "{}: {}", self.kind, error),
-            None => write!(fmt, "{}", self.kind),
-        }
-    }
-}
-
-impl std::error::Error for DispatchError {
-
-    fn cause(&self) -> Option<&dyn std::error::Error> {
-        self.error.as_deref().map(|error| error as &_)
-    }
-}
-
-impl std::fmt::Display for DispatchErrorKind {
-
-    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        use DispatchErrorKind::*;
-
-        match *self {
-            UnknownAction(name) => write!(fmt, "unknown action '{}'", name),
-            InvalidArgs => write!(fmt, "invalid action arguments"),
-        }
-    }
-}
-
-impl From<ParseArgsError> for DispatchError {
-
-    fn from(error: ParseArgsError) -> DispatchError {
-        DispatchError {
-            kind: DispatchErrorKind::InvalidArgs,
-            error: Some(Box::new(error)),
-        }
     }
 }
