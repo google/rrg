d14cc61757d0f1f496297698b9b451fca6f33650
Łukasz Hanuszczak <hanuszczak@google.com>
2022-11-07 15:54:06 +0100

    Refactor net interfaces action not to use `pnet`.

--- src/action/interfaces.rs
+++ src/action/interfaces.rs
@@ -8,13 +8,7 @@
 //! The interfaces action lists all network interfaces available on the client,
 //! collecting their names, MAC and IP addresses.
 
-use log::error;
-
-use pnet::{
-    datalink::{self, NetworkInterface},
-    ipnetwork::IpNetwork,
-    util::MacAddr,
-};
+use rrg_macro::warn;
 
 use crate::session::{self, Session};
 
@@ -22,57 +16,19 @@ use crate::session::{self, Session};
 #[derive(Debug)]
 pub struct Response {
     /// Information about an interface.
-    interface: NetworkInterface,
+    interface: crate::net::Interface,
 }
 
 /// Handles requests for the interfaces action.
 pub fn handle<S: Session>(session: &mut S, _: ()) -> session::Result<()> {
-    for interface in datalink::interfaces() {
+    // TODO(panhania@): Fix error handling.
+    for interface in crate::net::interfaces().unwrap() {
         session.reply(Response {
             interface: interface,
         })?;
     }
 
     Ok(())
-}
-
-/// Converts a [`MacAddr`][mac_addr] to a vector of bytes,
-/// which is what protobuf expects as a MAC.
-///
-/// [mac_addr]: ../../../pnet/util/struct.MacAddr.html
-fn mac_to_vec(mac: MacAddr) -> Vec<u8> {
-    vec![mac.0, mac.1, mac.2, mac.3, mac.4, mac.5]
-}
-
-/// Converts a single [`IpNetwork`][ip_network] to a protobuf struct
-/// corresponding to an IP address.
-///
-/// [ip_network]: ../../../ipnetwork/enum.IpNetwork.html
-fn ip_to_proto(ip_network: IpNetwork) -> rrg_proto::jobs::NetworkAddress {
-    use std::net::IpAddr::{V4, V6};
-    use rrg_proto::jobs::NetworkAddress_Family::*;
-
-    let mut proto = rrg_proto::jobs::NetworkAddress::new();
-    match ip_network.ip() {
-        V4(ipv4) => {
-            proto.set_address_type(INET);
-            proto.set_packed_bytes(ipv4.octets().to_vec());
-        },
-        V6(ipv6) => {
-            proto.set_address_type(INET6);
-            proto.set_packed_bytes(ipv6.octets().to_vec());
-        },
-    }
-
-    proto
-}
-
-/// Maps a vector of [`IpNetwork`][ip_network]s to a vector
-/// of protobuf structs corresponding to an IP address.
-///
-/// [ip_network]: ../../../ipnetwork/enum.IpNetwork.html
-fn ips_to_protos(ips: Vec<IpNetwork>) -> Vec<rrg_proto::jobs::NetworkAddress> {
-    ips.into_iter().map(ip_to_proto).collect()
 }
 
 impl super::Item for Response {
@@ -84,22 +40,33 @@ impl super::Item for Response {
     fn into_proto(self) -> rrg_proto::jobs::Interface {
         let mut proto = rrg_proto::jobs::Interface::new();
 
-        match self.interface.mac {
-            Some(mac) => {
-                proto.set_mac_address(mac_to_vec(mac));
-            }
-            None => {
-                error!(
-                    "unable to get MAC address for {} interface",
-                    self.interface.name,
-                );
-            },
-        };
+        let name = self.interface.name().to_string_lossy();
+        if let std::borrow::Cow::Owned(_) = &name {
+            warn!("network interface name with invalid bytes: {:?}", name);
+        }
+        proto.set_ifname(name.to_string());
 
-        proto.set_ifname(self.interface.name);
+        if let Some(mac_addr) = self.interface.mac_addr() {
+            proto.set_mac_address(mac_addr.octets().into());
+        } else {
+            warn!("network interface '{}' without MAC address", name);
+        }
 
-        for address in ips_to_protos(self.interface.ips) {
-            proto.mut_addresses().push(address);
+        for ip_addr in self.interface.ip_addrs() {
+            use rrg_proto::jobs::NetworkAddress_Family::{INET, INET6};
+
+            let mut ip_addr_proto = rrg_proto::jobs::NetworkAddress::new();
+            match ip_addr {
+                std::net::IpAddr::V4(ip_addr) => {
+                    ip_addr_proto.set_address_type(INET);
+                    ip_addr_proto.set_packed_bytes(ip_addr.octets().into());
+                },
+                std::net::IpAddr::V6(ip_addr) => {
+                    ip_addr_proto.set_address_type(INET6);
+                    ip_addr_proto.set_packed_bytes(ip_addr.octets().into());
+                },
+            };
+            proto.mut_addresses().push(ip_addr_proto);
         }
 
         proto
@@ -119,7 +86,9 @@ mod tests {
         let mut is_loopback_present = false;
         for i in 0..session.reply_count() {
             let interface = &session.reply::<Response>(i).interface;
-            is_loopback_present |= interface.is_loopback();
+            for ip_addr in interface.ip_addrs() {
+                is_loopback_present |= ip_addr.is_loopback();
+            }
         }
         assert!(is_loopback_present);
     }
