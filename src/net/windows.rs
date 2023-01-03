aef3112a2be532f58e23eafad16bc4053d7e4b74
Łukasz Hanuszczak <hanuszczak@google.com>
2023-01-03 16:45:53 +0100

    Abstract code for listing connections on Windows.

--- src/net/windows.rs
+++ src/net/windows.rs
@@ -617,6 +617,306 @@ pub fn all_udp_v6_connections() -> std::io::Result<UdpConnections> {
     })
 }
 
+mod connection {
+
+    use windows_sys::Win32::NetworkManagement::IpHelper::*;
+
+    use super::*;
+
+    /// Returns an iterator over all system TCP IPv4 connections.
+    pub fn all_tcp_v4() -> std::io::Result<impl Iterator<Item = TcpConnection>> {
+        all::<MIB_TCPTABLE_OWNER_PID>()
+    }
+
+    /// Returns an iterator over all system TCP IPv6 connections.
+    pub fn all_tcp_v6() -> std::io::Result<impl Iterator<Item = TcpConnection>> {
+        all::<MIB_TCP6TABLE_OWNER_PID>()
+    }
+
+    /// Returns an iterator over all system UDP IPv4 connections.
+    pub fn all_udp_v4() -> std::io::Result<impl Iterator<Item = UdpConnection>> {
+        all::<MIB_UDPTABLE_OWNER_PID>()
+    }
+
+    /// Returns an iterator over all system UDP IPv6 connections.
+    pub fn all_udp_v6() -> std::io::Result<impl Iterator<Item = UdpConnection>> {
+        all::<MIB_UDP6TABLE_OWNER_PID>()
+    }
+
+    /// An abstraction over Windows TCP and UDP connection tables.
+    ///
+    /// This trait makes it possible to work with TCP and UDP tables returned
+    /// by the Windows API in a generic way. It should be only implemented for
+    /// the following four types:
+    ///
+    ///   * [`MIB_TCPTABLE_OWNER_PID`]
+    ///   * [`MIB_TCP6TABLE_OWNER_PID`]
+    ///   * [`MIB_UDPTABLE_OWNER_PID`]
+    ///   * [`MIB_UDP6TABLE_OWNER_PID`]
+    ///
+    /// It is not intended to ever be exposed and should be used only to avoid
+    /// code duplication in concrete implementations that care about specific
+    /// portocol and version combinations.
+    trait Table {
+        /// The Windows type of the table rows.
+        type Row;
+
+        /// An idiomatic Rust type that the table contains information for.
+        type Connection;
+
+        /// Calls the native `GetExtended*Table` system function.
+        ///
+        /// Implementers should fill `buf` using the `GetExtended*Table` with
+        /// table information.
+        ///
+        /// `buf_size` should be set to the number of bytes written in case of
+        /// success and to the number of bytes needed to fill the buffer in case
+        /// of an error due to insufficiently large buffer.
+        ///
+        /// The function should return the error code as returned by the system
+        /// function.
+        ///
+        /// See documentation for the system functions for more details:
+        ///
+        ///   * [`GetExtendedTcpTable`]
+        ///   * [`GetExtendedUdpTable`]
+        ///
+        /// [`GetExtendedTcpTable`]: https://learn.microsoft.com/en-us/windows/win32/api/iphlpapi/nf-iphlpapi-getextendedtcptable
+        /// [`GetExtendedUdpTable`]: https://learn.microsoft.com/en-us/windows/win32/api/iphlpapi/nf-iphlpapi-getextendedudptable
+        ///
+        /// # Safety
+        ///
+        /// Implementers can assume that `buf` has length of at least `buf_size`
+        /// bytes but should not assume anything about memory being initialized.
+        unsafe fn get(buf: *mut std::ffi::c_void, buf_size: &mut u32) -> u32;
+
+        /// Returns array of rows contained in the table.
+        ///
+        /// # Safety
+        ///
+        /// Implementers can assume that the table has been already correctly
+        /// initialized through an appropriate call to the `GetExteded*Table`
+        /// function (and thus `table` and `dwNumEntries` fields uphold to the
+        /// invariants specified in the Windows documentation).
+        unsafe fn rows(&self) -> &[Self::Row];
+
+        /// Transforms a low-level row structore to an idiomatic Rust type.
+        fn parse_row(
+            row: &Self::Row,
+        ) -> Result<Self::Connection, ParseConnectionError>;
+    }
+
+    impl Table for MIB_TCPTABLE_OWNER_PID {
+
+        type Row = MIB_TCPROW_OWNER_PID;
+
+        type Connection = TcpConnection;
+
+        // https://learn.microsoft.com/en-us/windows/win32/api/iphlpapi/nf-iphlpapi-getextendedtcptable#parameters
+        unsafe fn get(buf: *mut std::ffi::c_void, buf_size: &mut u32) -> u32 {
+            GetExtendedTcpTable(
+                buf,
+                buf_size,
+                false.into(),
+                windows_sys::Win32::Networking::WinSock::AF_INET,
+                TCP_TABLE_OWNER_PID_ALL,
+                0,
+            )
+        }
+
+        // https://learn.microsoft.com/en-us/windows/win32/api/tcpmib/ns-tcpmib-mib_tcptable_owner_pid#members
+        unsafe fn rows(&self) -> &[MIB_TCPROW_OWNER_PID] {
+            std::slice::from_raw_parts(
+                self.table.as_ref().as_ptr(),
+                self.dwNumEntries as usize,
+            )
+        }
+
+        fn parse_row(
+            row: &MIB_TCPROW_OWNER_PID,
+        ) -> Result<TcpConnection, ParseConnectionError> {
+            // TODO(@panhania): Pass by reference.
+            super::parse_tcp_v4_row(*row)
+        }
+    }
+
+    impl Table for MIB_TCP6TABLE_OWNER_PID {
+
+        type Row = MIB_TCP6ROW_OWNER_PID;
+
+        type Connection = TcpConnection;
+
+        // https://learn.microsoft.com/en-us/windows/win32/api/iphlpapi/nf-iphlpapi-getextendedtcptable#parameters
+        unsafe fn get(buf: *mut std::ffi::c_void, buf_size: &mut u32) -> u32 {
+            GetExtendedTcpTable(
+                buf,
+                buf_size,
+                false.into(),
+                windows_sys::Win32::Networking::WinSock::AF_INET6,
+                TCP_TABLE_OWNER_PID_ALL,
+                0,
+            )
+        }
+
+        // https://learn.microsoft.com/en-us/windows/win32/api/tcpmib/ns-tcpmib-mib_tcp6table_owner_pid#members
+        unsafe fn rows(&self) -> &[MIB_TCP6ROW_OWNER_PID] {
+            std::slice::from_raw_parts(
+                self.table.as_ref().as_ptr(),
+                self.dwNumEntries as usize,
+            )
+        }
+
+        fn parse_row(
+            row: &MIB_TCP6ROW_OWNER_PID,
+        ) -> Result<TcpConnection, ParseConnectionError> {
+            // TODO(@panhania): Pass by reference.
+            super::parse_tcp_v6_row(*row)
+        }
+    }
+
+    impl Table for MIB_UDPTABLE_OWNER_PID {
+
+        type Row = MIB_UDPROW_OWNER_PID;
+
+        type Connection = UdpConnection;
+
+        // https://learn.microsoft.com/en-us/windows/win32/api/iphlpapi/nf-iphlpapi-getextendedudptable
+        unsafe fn get(buf: *mut std::ffi::c_void, buf_size: &mut u32) -> u32 {
+            GetExtendedUdpTable(
+                buf,
+                buf_size,
+                false.into(),
+                windows_sys::Win32::Networking::WinSock::AF_INET,
+                UDP_TABLE_OWNER_PID,
+                0,
+            )
+        }
+
+        // https://learn.microsoft.com/en-us/windows/win32/api/udpmib/ns-udpmib-mib_udptable#members
+        unsafe fn rows(&self) -> &[MIB_UDPROW_OWNER_PID] {
+            std::slice::from_raw_parts(
+                self.table.as_ref().as_ptr(),
+                self.dwNumEntries as usize,
+            )
+        }
+
+        fn parse_row(
+            row: &MIB_UDPROW_OWNER_PID,
+        ) -> Result<UdpConnection, ParseConnectionError> {
+            // TODO(@panhania): Pass by reference.
+            super::parse_udp_v4_row(*row)
+        }
+    }
+
+    impl Table for MIB_UDP6TABLE_OWNER_PID {
+
+        type Row = MIB_UDP6ROW_OWNER_PID;
+
+        type Connection = UdpConnection;
+
+        // https://learn.microsoft.com/en-us/windows/win32/api/iphlpapi/nf-iphlpapi-getextendedudptable
+        unsafe fn get(buf: *mut std::ffi::c_void, buf_size: &mut u32) -> u32 {
+            GetExtendedUdpTable(
+                buf,
+                buf_size,
+                false.into(),
+                windows_sys::Win32::Networking::WinSock::AF_INET6,
+                UDP_TABLE_OWNER_PID,
+                0,
+            )
+        }
+
+        // https://learn.microsoft.com/en-us/windows/win32/api/udpmib/ns-udpmib-mib_udp6table_owner_pid#members
+        unsafe fn rows(&self) -> &[MIB_UDP6ROW_OWNER_PID] {
+            std::slice::from_raw_parts(
+                self.table.as_ref().as_ptr(),
+                self.dwNumEntries as usize,
+            )
+        }
+
+        fn parse_row(
+            row: &MIB_UDP6ROW_OWNER_PID,
+        ) -> Result<UdpConnection, ParseConnectionError> {
+            // TODO(@panhania): Pass by reference.
+            super::parse_udp_v6_row(*row)
+        }
+    }
+
+    /// Returns an iterator over all system connections of a specific type.
+    fn all<T>() -> std::io::Result<impl Iterator<Item = T::Connection>>
+    where
+        T: Table,
+    {
+        use std::convert::TryFrom as _;
+
+        let mut buf_size = u32::try_from(DEFAULT_BUF_SIZE)
+            .expect("default buffer size too big");
+
+        let buf_layout = std::alloc::Layout::from_size_align(
+            buf_size as usize,
+            std::mem::align_of::<T>(),
+        ).expect("invalid layout for adapter addresses table");
+
+        let mut buf = crate::alloc::Allocation::new(buf_layout)
+            .ok_or_else(|| std::io::ErrorKind::OutOfMemory)?;
+
+        // SAFETY: As required, we allocated a buffer and pass its size with it.
+        // In case the allocated buffer is too small, we handle this case below.
+        let mut code = unsafe {
+            T::get(buf.as_ptr().cast().as_ptr(), &mut buf_size)
+        };
+
+        if code == windows_sys::Win32::Foundation::ERROR_BUFFER_OVERFLOW {
+            // Just a sanity check. Since the default buffer was too small, a
+            // "good" one should be strictly bigger.
+            assert!(DEFAULT_BUF_SIZE < (buf_size as usize));
+
+            buf = buf.resize(buf_size as usize)
+                .map_err(|_| std::io::ErrorKind::OutOfMemory)?;
+
+            // SAFETY: We call the function the same as above but with larger
+            // buffer. Note that this can still fail in an unlikely case where a
+            // new device was added between the previous call and this one. We
+            // do not do another attempt and fail if this is the case.
+            code = unsafe {
+                T::get(buf.as_ptr().cast().as_ptr(), &mut buf_size)
+            };
+        }
+
+        if code != windows_sys::Win32::Foundation::NO_ERROR {
+            let code = i32::try_from(code)
+                .expect("invalid error code");
+
+            return Err(std::io::Error::from_raw_os_error(code));
+        }
+
+        // SAFETY: The buffer was allocated with layout specific to the table
+        // type and the allocation is guaranteed to be correct. The buffer was
+        // filled successfully, so it is safe to assume the memory is correctly
+        // initialized now.
+        let table = unsafe {
+            buf.as_ptr().cast::<T>().as_ref()
+        };
+
+        // SAFETY: The `table` is guaranteed to be initialized now and so all
+        // invariants assumed by the `rows` method should uphold.
+        let rows = unsafe {
+            table.rows()
+        };
+
+        let conns = rows
+            .iter()
+            .map(T::parse_row)
+            // TODO(@panhania): Eliminate vector collection.
+            .collect::<Result<Vec<_>, _>>()
+            .map_err(|error| {
+                std::io::Error::new(std::io::ErrorKind::InvalidData, error)
+            })?;
+
+        Ok(conns.into_iter())
+    }
+}
+
 /// Parses a connection row of a TCP IPv4 table.
 fn parse_tcp_v4_row(
     row: windows_sys::Win32::NetworkManagement::IpHelper::MIB_TCPROW_OWNER_PID,
