60f8bfab8175db1e7b6b551f5480590da9d5d76d
Łukasz Hanuszczak <hanuszczak@google.com>
2023-02-14 14:11:33 +0100

    Refactor macOS connections listing code.

--- src/net/macos.rs
+++ src/net/macos.rs
@@ -183,145 +183,225 @@ pub fn interfaces() -> std::io::Result<impl Iterator<Item = Interface>> {
     Ok(ifaces.into_iter())
 }
 
-/// Returns an iterator over all TCP connections for the specified process.
-pub fn tcp_connections(pid: u32) -> std::io::Result<impl Iterator<Item = std::io::Result<Connection>>> {
-    use std::convert::TryFrom as _;
+/// An iterator over all connections for the specified process.
+struct Connections {
+    /// An identifier of the process for which this iterator yields items.
+    pid: u32,
+    /// An iterator over low-level macOS file descriptor metadata.
+    iter: std::vec::IntoIter<crate::libc::proc_fdinfo>,
+}
 
-    let pid_i32 = i32::try_from(pid)
-        .map_err(|_| std::io::ErrorKind::InvalidInput)?;
+impl Connections {
 
-    // SAFETY: We call the function with null buffer. This should return the
-    // size for the buffer that we need or 0 in case of an error.
-    let buf_size = unsafe {
-        libc::proc_pidinfo(pid_i32, crate::libc::PROC_PIDLISTFDS, 0, std::ptr::null_mut(), 0)
-    };
-    if buf_size == 0 {
-        return Err(std::io::Error::last_os_error());
-    }
+    /// Creates a new iterator over connections for the specified process.
+    ///
+    /// # Errors
+    ///
+    /// This function will yield an error if metadata cannot be obtained for the
+    /// specified process.
+    pub fn new(pid: u32) -> std::io::Result<Connections> {
+        use std::convert::TryFrom as _;
 
-    let buf_layout = std::alloc::Layout::from_size_align(
-        buf_size as usize,
-        std::mem::align_of::<crate::libc::proc_fdinfo>(),
-    ).expect("invalid layout for `proc_fdinfo` struct");
-
-    let buf = crate::alloc::Allocation::new(buf_layout)
-        .ok_or_else(|| std::io::ErrorKind::OutOfMemory)?;
-
-    let buf_ptr = buf.as_ptr().cast::<libc::c_void>().as_ptr();
-
-    // SAFETY: We call the function as above but with allocated buffer. Both the
-    // buffer and the passed size are correct with respect to each other. Again,
-    // the fucntion will return 0 in case of an error.
-    let buf_size = unsafe {
-        libc::proc_pidinfo(pid_i32, crate::libc::PROC_PIDLISTFDS, 0, buf_ptr, buf_size)
-    };
-    if buf_size == 0 {
-        return Err(std::io::Error::last_os_error());
-    }
-
-    // Note that we calculate the number of records after the second call since
-    // the number of records estimated by the first call may no longer be valid
-    // at the time we do the second call.
-    let fdinfos_len = buf_size as usize / std::mem::size_of::<crate::libc::proc_fdinfo>();
-
-    // SAFETY: The call to `proc_pidinfo` succeeded, so the buffer should be
-    // filled with the amount of records as calculated above.
-    let fdinfos = unsafe {
-        std::slice::from_raw_parts(buf_ptr.cast::<crate::libc::proc_fdinfo>(), fdinfos_len)
-    };
-
-    let mut conns = Vec::new();
-
-    for fdinfo in fdinfos {
-        if fdinfo.proc_fdtype != crate::libc::PROX_FDTYPE_SOCKET as u32 {
-            continue;
-        }
-
-        let mut sock_fdinfo = std::mem::MaybeUninit::<crate::libc::socket_fdinfo>::uninit();
-
-        // SAFETY: We verify that the file descriptor corresponds to a socket
-        // above and then we just pass a pointer to `sock_fdinfo` along with its
-        // size. In case something goes wrong, the function should report an
-        // error (which we verify below) but the call itself should be safe.
-        let size = unsafe {
-            libc::proc_pidfdinfo(
-                pid_i32,
-                fdinfo.proc_fd,
-                crate::libc::PROC_PIDFDSOCKETINFO,
-                sock_fdinfo.as_mut_ptr().cast::<libc::c_void>(),
-                std::mem::size_of::<crate::libc::socket_fdinfo>() as i32,
-            )
+        const PROC_FDINFO_SIZE: i32 = {
+            std::mem::size_of::<crate::libc::proc_fdinfo>() as i32
         };
 
-        if size == 0 || size < std::mem::size_of::<crate::libc::socket_fdinfo>() as i32 {
+        let pid_i32 = i32::try_from(pid)
+            .map_err(|_| std::io::ErrorKind::InvalidInput)?;
+
+        // SAFETY: We call the function with null buffer. This should return the
+        // size for the buffer that we need or 0 in case of an error.
+        let buf_size = unsafe {
+            libc::proc_pidinfo(
+                pid_i32,
+                crate::libc::PROC_PIDLISTFDS,
+                0,
+                std::ptr::null_mut(),
+                0,
+            )
+        };
+        if buf_size == 0 {
             return Err(std::io::Error::last_os_error());
         }
 
-        // SAFETY: We verified that the call prod `proc_pidfdinfo` succeeded,
-        // so `sock_fdinfo` should be filled with valid data now.
-        let sock_fdinfo = unsafe {
-            sock_fdinfo.assume_init()
+        let mut buf_len = buf_size / PROC_FDINFO_SIZE;
+        if buf_size % PROC_FDINFO_SIZE != 0 {
+            buf_len += 1;
+        }
+
+        let mut buf = {
+            Vec::<crate::libc::proc_fdinfo>::with_capacity(buf_len as usize)
         };
 
+        // SAFETY: We call the function as above but with allocated buffer. The
+        // function will return 0 in case of an error which we verify later on.
+        let buf_size = unsafe {
+            libc::proc_pidinfo(
+                pid_i32,
+                crate::libc::PROC_PIDLISTFDS,
+                0,
+                buf.as_mut_ptr().cast::<libc::c_void>(),
+                buf_size,
+            )
+        };
+        if buf_size == 0 {
+            return Err(std::io::Error::last_os_error());
+        }
+        if buf_size % PROC_FDINFO_SIZE != 0 {
+            return Err(std::io::ErrorKind::InvalidData.into());
+        }
+
+        let buf_len = buf_size / PROC_FDINFO_SIZE;
+
+        // SAFETY: We verified that the call to `proc_pidinfo` succeeded. The
+        // call returns the number of bytes filled in the buffer, so we can set
+        // the length to the number of bytes divided by the length of individual
+        // entry.
+        unsafe {
+            buf.set_len(buf_len as usize);
+        }
+
+        Ok(Connections {
+            pid,
+            iter: buf.into_iter(),
+        })
+    }
+}
+
+impl Connections {
+
+    /// Parses a macOS TCP socket metadata into platform-agnostic type.
+    fn parse_tcp_sockinfo(
+        &self,
+        info: &crate::libc::socket_fdinfo,
+    ) -> Result<TcpConnection, ParseConnectionError> {
         use ParseConnectionError::*;
 
-        let conn = match sock_fdinfo.psi.soi_kind {
-            crate::libc::SOCKINFO_TCP => (|| -> _ {
-                if sock_fdinfo.psi.soi_protocol != libc::IPPROTO_TCP {
-                    return Err(InvalidProtocol(sock_fdinfo.psi.soi_protocol));
-                }
+        if info.psi.soi_protocol != libc::IPPROTO_TCP {
+            return Err(InvalidProtocol(info.psi.soi_protocol));
+        }
 
-                // SAFETY: We verified that we have a TCP socket, so we can
-                // safely access the `pri_tcp` field.
-                let info = unsafe { sock_fdinfo.psi.soi_proto.pri_tcp };
-                match sock_fdinfo.psi.soi_family {
-                    libc::AF_INET => {
-                        // SAFETY: We verified that it is an IPv4 connection.
-                        unsafe { parse_tcp_v4_sockinfo(pid, info) }
-                    }.map(|conn| conn.into()),
-                    libc::AF_INET6 => {
-                        // SAFETY: We verified that it is an IPv6 connection.
-                        unsafe { parse_tcp_v6_sockinfo(pid, info) }
-                    }.map(|conn| conn.into()),
-                    _ => {
-                        Err(InvalidAddressFamily(sock_fdinfo.psi.soi_family))
-                    }
-                }
-            })().map_err(|error| error.into()),
-            crate::libc::SOCKINFO_IN => (|| -> _ {
-                if sock_fdinfo.psi.soi_protocol != libc::IPPROTO_UDP {
-                    return Err(InvalidProtocol(sock_fdinfo.psi.soi_protocol));
-                }
+        // This should be ensured by the caller.
+        assert_eq!(info.psi.soi_kind, crate::libc::SOCKINFO_TCP);
 
-                // SAFETY: We verified that we have a generic socket, so we can
-                // safely access the `pri_in` field.
-                let info = unsafe { sock_fdinfo.psi.soi_proto.pri_in };
-                match sock_fdinfo.psi.soi_family {
-                    libc::AF_INET => {
-                        // SAFETY: We verified that it is an IPv4 connection.
-                        unsafe { parse_udp_v4_sockinfo(pid, info) }
-                    }.map(|conn| conn.into()),
-                    libc::AF_INET6 => {
-                        // SAFETY: We verified that it is an IPv6 connection.
-                        unsafe { parse_udp_v6_sockinfo(pid, info) }
-                    }.map(|conn| conn.into()),
-                    _ => {
-                        Err(InvalidAddressFamily(sock_fdinfo.psi.soi_family))
-                    }
-                }
-            })().map_err(|error| error.into()),
-            _ => continue,
-        };
+        // SAFETY: We verified that we have a TCP socket, so we can
+        // safely access the `pri_tcp` field.
+        let tcp_info = unsafe { info.psi.soi_proto.pri_tcp };
 
-        conns.push(conn)
+        match info.psi.soi_family {
+            // SAFETY: We verify that it is an IPv4 connection.
+            libc::AF_INET => unsafe {
+                parse_tcp_v4_sockinfo(self.pid, tcp_info)
+            }.map(|conn| conn.into()),
+            // SAFETY: We verified that it is an IPv6 connection.
+            libc::AF_INET6 => unsafe {
+                parse_tcp_v6_sockinfo(self.pid, tcp_info)
+            }.map(|conn| conn.into()),
+            _ => Err(InvalidAddressFamily(info.psi.soi_family))
+        }
     }
 
-    Ok(conns.into_iter())
+    /// Parses a macOS UDP socket metadata into platform-agnostic type.
+    fn parse_udp_sockinfo(
+        &self,
+        info: &crate::libc::socket_fdinfo,
+    ) -> Result<UdpConnection, ParseConnectionError> {
+        use ParseConnectionError::*;
+
+        if info.psi.soi_protocol != libc::IPPROTO_UDP {
+            return Err(InvalidProtocol(info.psi.soi_protocol));
+        }
+
+        // This should be ensured by the caller.
+        assert_eq!(info.psi.soi_kind, crate::libc::SOCKINFO_IN);
+
+        // SAFETY: We verified that we have a generic socket, so we can
+        // safely access the `pri_in` field.
+        let udp_info = unsafe { info.psi.soi_proto.pri_in };
+
+        match info.psi.soi_family {
+            // SAFETY: We verified that it is an IPv4 connection.
+            libc::AF_INET => unsafe {
+                parse_udp_v4_sockinfo(self.pid, udp_info)
+            }.map(|conn| conn.into()),
+            // SAFETY: We verified that it is an IPv6 connection.
+            libc::AF_INET6 => unsafe {
+                parse_udp_v6_sockinfo(self.pid, udp_info)
+            }.map(|conn| conn.into()),
+            _ => {
+                Err(InvalidAddressFamily(info.psi.soi_family))
+            }
+        }
+    }
+}
+
+impl Iterator for Connections {
+
+    type Item = std::io::Result<Connection>;
+
+    fn next(&mut self) -> Option<std::io::Result<Connection>> {
+        const SOCKET_FDINFO_SIZE: i32 = {
+            std::mem::size_of::<crate::libc::socket_fdinfo>() as i32
+        };
+
+        // We verified that `self.pid` fits in `i32` in the constructor, so this
+        // should not panic.
+        let pid_i32 = self.pid as i32;
+
+        for fdinfo in &mut self.iter {
+            if fdinfo.proc_fdtype != crate::libc::PROX_FDTYPE_SOCKET as u32 {
+                continue;
+            }
+
+            let mut info = {
+                std::mem::MaybeUninit::<crate::libc::socket_fdinfo>::uninit()
+            };
+
+            // SAFETY: We verifed that the file descriptor corresponds to a
+            // socket above. Then we just pass a pointer to the uninitialized
+            // struct along with with its size. In case something goes wrong,
+            // the function should report an error (which we verify below).
+            let size = unsafe {
+                libc::proc_pidfdinfo(
+                    self.pid as i32,
+                    fdinfo.proc_fd,
+                    crate::libc::PROC_PIDFDSOCKETINFO,
+                    info.as_mut_ptr().cast::<libc::c_void>(),
+                    SOCKET_FDINFO_SIZE,
+                )
+            };
+            if size == 0 || size < SOCKET_FDINFO_SIZE {
+                return Some(Err(std::io::Error::last_os_error()));
+            }
+
+            // SAFETY: We verified that the call to `proc_pidfdinfo` succeeded,
+            // so the struct should be filled with valid data now.
+            let info = unsafe { info.assume_init() };
+
+            match info.psi.soi_kind {
+                crate::libc::SOCKINFO_TCP => {
+                    match self.parse_tcp_sockinfo(&info) {
+                        Ok(conn) => return Some(Ok(conn.into())),
+                        Err(error) => return Some(Err(error.into())),
+                    }
+                }
+                crate::libc::SOCKINFO_IN => {
+                    match self.parse_udp_sockinfo(&info) {
+                        Ok(conn) => return Some(Ok(conn.into())),
+                        Err(error) => return Some(Err(error.into())),
+                    }
+                }
+                _ => continue,
+            }
+        }
+
+        None
+    }
 }
 
 /// Returns an iterator over IPv4 TCP connections for the specified process.
 pub fn tcp_v4_connections(pid: u32) -> std::io::Result<impl Iterator<Item = std::io::Result<TcpConnectionV4>>> {
-    let conns = tcp_connections(pid)?;
+    let conns = Connections::new(pid)?;
     Ok(conns.filter_map(|conn| match conn {
         Ok(Connection::Tcp(TcpConnection::V4(conn))) => Some(Ok(conn)),
         Ok(_) => None,
@@ -336,7 +416,7 @@ pub fn tcp_v4_connections(pid: u32) -> std::io::Result<impl Iterator<Item = std:
 
 /// Returns an iterator over IPv6 TCP connections for the specified process.
 pub fn tcp_v6_connections(pid: u32) -> std::io::Result<impl Iterator<Item = std::io::Result<TcpConnectionV6>>> {
-    let conns = tcp_connections(pid)?;
+    let conns = Connections::new(pid)?;
     Ok(conns.filter_map(|conn| match conn {
         Ok(Connection::Tcp(TcpConnection::V6(conn))) => Some(Ok(conn)),
         Ok(_) => None,
