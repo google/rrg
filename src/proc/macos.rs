a0f6529c3f5554ef77984f010a04a65feb8b57cf
Łukasz Hanuszczak <hanuszczak@google.com>
2023-02-20 19:41:21 +0100

    Add support for listing pids on macOS.

--- src/proc/macos.rs
+++ src/proc/macos.rs
@@ -8,3 +8,102 @@ pub fn ids() -> std::io::Result<impl Iterator<Item = std::io::Result<u32>>> {
     // TOOD(@panhania): Implement this method.
     Err::<std::iter::Empty<_>, _>(std::io::ErrorKind::Unsupported.into())
 }
+
+/// A macOS-specific implementation of the iterator over process identifiers.
+struct Ids {
+    /// An iterator over the process metadata returned by a `sysctl` call.
+    iter: std::vec::IntoIter<kinfo_proc>,
+}
+
+impl Ids {
+
+    /// Creates a new iterator over system process identifiers.
+    fn new() -> std::io::Result<Ids> {
+        let mut mib = [libc::CTL_KERN, libc::KERN_PROC_ALL];
+
+        let mut buf_size = std::mem::MaybeUninit::uninit();
+
+        // SAFETY: We call the `sysctl` function as described in the FreeBSD
+        // documentation [1] (macOS's kernel derives from FreeBSD). We check for
+        // errors afterwards.
+        //
+        // This is the first call where we don't pass any buffer and we just
+        // want to estimate the size of the buffer to hold the data. It should
+        // be returned thought the fourth (`oldlenp`) argument.
+        //
+        // Note that the `namelen` parameter (second argument) is the _length_
+        // of the array passed as the `name` argument (not size in bytes), while
+        // the remaining two expect size in bytes.
+        //
+        // [1]: https://man.freebsd.org/cgi/man.cgi?sysctl(3)
+        let code = unsafe {
+            libc::sysctl(
+                mib.as_mut_ptr(), mib.len() as libc::c_uint,
+                std::ptr::null_mut(), buf_size.as_mut_ptr(),
+                std::ptr::null_mut(), 0,
+            )
+        };
+        if code != 0 {
+            return Err(std::io::Error::last_os_error());
+        }
+
+        // SAFETY: If the call to `sysctl` succeeded, we can assume that the
+        // `buf_size` no is filled with the expected size of the buffer.
+        let mut buf_size = unsafe {
+            buf_size.assume_init()
+        } as usize;
+
+        let mut buf_len = buf_size / KINFO_PROC_SIZE;
+        if buf_size % KINFO_PROC_SIZE != 0 {
+            buf_len += 1;
+        }
+
+        let mut buf = Vec::<kinfo_proc>::with_capacity(buf_len);
+
+        // SAFETY: We create a buffer of the size specified by the previous call
+        // to `sysctl`. Note that between the two calls the required buffer size
+        // might have changed in which case `ENOMEM` ought to be returned. The
+        // operating system should round up the required buffer size to handle
+        // such cases. We verify whether the call succeeded below.
+        //
+        // The rest is as with the `sysctl` call above (not the comment about
+        // the length parameters).
+        let code = unsafe {
+            libc::sysctl(
+                mib.as_mut_ptr(), mib.len() as libc::c_uint,
+                buf.as_mut_ptr().cast::<libc::c_void>(), &mut buf_size,
+                std::ptr::null_mut(), 0,
+            )
+        };
+        if code != 0 {
+            return Err(std::io::Error::last_os_error());
+        }
+
+        if buf_size % KINFO_PROC_SIZE != 0 {
+            return Err(std::io::ErrorKind::InvalidData.into());
+        }
+
+        let buf_len = buf_size / KINFO_PROC_SIZE;
+
+        // SAFETY: The `syctl` call succeeded and we calculated the length of
+        // the buffer above.
+        unsafe {
+            buf.set_len(buf_len);
+        }
+
+        Ok(Ids {
+            iter: buf.into_iter(),
+        })
+    }
+}
+
+// TODO(@panhania): Move to the `libc` module (or ideally: to the `libc` crate).
+#[repr(C)]
+#[derive(Clone, Copy)]
+struct kinfo_proc {
+    // TODO(@panhania): Fill with the actual definition.
+    __padding: [u8; 40],
+    k_pid: libc::pid_t,
+}
+
+const KINFO_PROC_SIZE: usize = std::mem::size_of::<kinfo_proc>();
