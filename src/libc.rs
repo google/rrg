b475a3123d3d8016ec63a215e55c2c4909f72522
Łukasz Hanuszczak <hanuszczak@google.com>
2023-02-22 17:42:12 +0100

    Move macOS C types to the `libc` module.

--- src/libc.rs
+++ src/libc.rs
@@ -355,6 +355,175 @@ mod macos {
         pub pfi: proc_fileinfo,
         pub psi: socket_info,
     }
+
+    // Types used for listing network connections.
+
+    #[derive(Clone, Copy)]
+    #[repr(C)]
+    pub struct __c_anonymous_p_st1 {
+        pub __p_forw: *mut libc::c_void,
+        pub __p_back: *mut libc::c_void,
+    }
+
+    #[derive(Clone, Copy)]
+    #[repr(C)]
+    pub union __c_anonymous_p_un {
+        pub p_st1: __c_anonymous_p_st1,
+        pub __p_starttime: libc::timeval,
+    }
+
+    #[derive(Clone, Copy)]
+    #[repr(C)]
+    pub struct extern_proc {
+        pub p_un: __c_anonymous_p_un,
+        pub p_vmspace: *mut vmspace,
+        pub p_sigacts: *mut libc::c_void,
+        pub p_flag: libc::c_int,
+        pub p_stat: libc::c_char,
+        pub p_pid: libc::pid_t,
+        pub p_oppid: libc::pid_t,
+        pub p_dupfd: libc::c_int,
+        pub user_stack: caddr_t,
+        pub exit_thread: *mut libc::c_void,
+        pub p_debugger: libc::c_int,
+        pub sigwait: libc::boolean_t,
+        pub p_estcpu: libc::c_uint,
+        pub p_cpticks: libc::c_int,
+        pub p_pctcpu: fixpt_t,
+        pub p_wchan: *mut libc::c_void,
+        pub p_wmesg: *mut libc::c_char,
+        pub p_swtime: libc::c_uint,
+        pub p_slptime: libc::c_uint,
+        pub p_realtimer: libc::itimerval,
+        pub p_rtime: libc::timeval,
+        pub p_uticks: u_quad_t,
+        pub p_sticks: u_quad_t,
+        pub p_iticks: u_quad_t,
+        pub p_traceflag: libc::c_int,
+        pub p_tracep: *mut libc::c_void,
+        pub p_siglist: libc::c_int,
+        pub p_textvp: *mut libc::c_void,
+        pub p_holdcnt: libc::c_int,
+        pub p_sigmask: libc::sigset_t,
+        pub p_sigignore: libc::sigset_t,
+        pub p_sigcatch: libc::sigset_t,
+        pub p_priority: libc::c_uchar,
+        pub p_usrpri: libc::c_uchar,
+        pub p_nice: libc::c_char,
+        pub p_comm: [libc::c_char; libc::MAXCOMLEN + 1],
+        pub p_pgrp: *mut libc::c_void,
+        pub p_addr: *mut libc::c_void,
+        pub p_xstat: libc::c_ushort,
+        pub p_acflag: libc::c_ushort,
+        pub p_ru: *mut libc::c_void,
+    }
+
+    #[derive(Clone, Copy)]
+    #[repr(C)]
+    pub struct _pcred {
+        /// Opaque content.
+        pub pc_lock: [libc::c_char; 72],
+        /// Current credentials.
+        pub pc_ucred: *mut libc::c_void,
+        /// Real user identifier.
+        pub p_ruid: libc::uid_t,
+        /// Saved effective user identifier.
+        pub p_svuid: libc::uid_t,
+        /// Real group identifier.
+        pub p_rgid: libc::gid_t,
+        /// Saved effective group identifier.
+        pub p_svgid: libc::gid_t,
+        /// Reference count.
+        pub p_refcnt: libc::c_int,
+    }
+
+    pub const NGROUPS_MAX: libc::c_int = 16;
+    pub const NGROUPS: libc::c_int = NGROUPS_MAX;
+
+    pub const WMESGLEN: libc::c_int = 7;
+
+    pub const COMAPT_MAXLOGNAME: libc::c_int = 12;
+
+    #[allow(non_camel_case_types)]
+    pub type caddr_t = *mut libc::c_void;
+
+    #[allow(non_camel_case_types)]
+    pub type segsz_t = i32;
+
+    #[allow(non_camel_case_types)]
+    pub type fixpt_t = u32;
+
+    #[allow(non_camel_case_types)]
+    pub type u_quad_t = u64;
+
+    #[derive(Clone, Copy)]
+    #[repr(C)]
+    pub struct _ucred {
+        /// Reference count.
+        pub cr_ref: i32,
+        /// Effective user identifier.
+        pub cr_uid: libc::uid_t,
+        /// Group count.
+        pub cr_ngroups: libc::c_short,
+        /// Group identifiers.
+        pub cr_groups: [libc::gid_t; NGROUPS as usize],
+    }
+
+    #[derive(Clone, Copy)]
+    #[repr(C)]
+    pub struct vmspace {
+        // `dummy*` is literally what is used in the original header.
+        pub dummy: i32,
+        pub dummy2: caddr_t,
+        pub dummy3: [i32; 5],
+        pub dummy4: [caddr_t; 3],
+    }
+
+    #[derive(Clone, Copy)]
+    #[repr(C)]
+    pub struct eproc {
+        /// Process address.
+        pub e_paddr: *mut libc::c_void,
+        /// Session pointer.
+        pub e_sess: *mut libc::c_void,
+        /// Process credentials.
+        pub e_pcred: _pcred,
+        /// Current credentials.
+        pub e_ucred: _ucred,
+        /// Address space.
+        pub e_vm: vmspace,
+        /// Parent process identifier.
+        pub e_ppid: libc::pid_t,
+        /// Process group identifier.
+        pub e_pgid: libc::pid_t,
+        /// Job control counter.
+        pub e_jobc: libc::c_short,
+        /// Controlling TTY device identifier.
+        pub e_tdev: libc::dev_t,
+        /// Controlling TTY process group identifier.
+        pub e_tpgid: libc::pid_t,
+        /// Controlling TTY session pointer.
+        pub e_tsess: *mut libc::c_void,
+        /// Waiting channel message.
+        pub e_wmesg: [libc::c_char; WMESGLEN as usize + 1],
+        /// Text size.
+        pub e_xsize: segsz_t,
+        /// Text resident set size.
+        pub e_xrssize: libc::c_short,
+        /// Text reference count.
+        pub e_xccount: libc::c_short,
+        pub e_xswrss: libc::c_short,
+        pub e_flag: i32,
+        pub e_login: [libc::c_char; COMAPT_MAXLOGNAME as usize],
+        pub e_spare: [i32; 4],
+    }
+
+    #[derive(Clone, Copy)]
+    #[repr(C)]
+    pub struct kinfo_proc {
+        pub kp_proc: extern_proc,
+        pub kp_eproc: eproc,
+    }
 }
 
 #[cfg(target_os = "macos")]
