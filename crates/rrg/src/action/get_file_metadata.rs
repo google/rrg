fcbcaf953bd59710d5d7601fd1ffda4d2a89c1b3
Łukasz Hanuszczak <hanuszczak@google.com>
2023-04-05 17:16:51 +0200

    Implement basic version of `get_file_metadata`.

--- crates/rrg/src/action/get_file_metadata.rs
+++ crates/rrg/src/action/get_file_metadata.rs
@@ -7,13 +7,13 @@ use std::path::PathBuf;
 /// Arguments of the `get_file_metadata` action.
 pub struct Args {
     /// Path the file to get the metadata of.
-    path: std::path::PathBuf,
+    path: PathBuf,
 }
 
 /// Result of the `get_file_metadata` action.
 struct Item {
     /// Canonical path to the file we retrieve the metadata of.
-    path: std::path::PathBuf,
+    path: PathBuf,
     /// Retrieved metadata of the file we retrieved.
     metadata: std::fs::Metadata,
 }
@@ -23,7 +23,15 @@ pub fn handle<S>(session: &mut S, args: Args) -> crate::session::Result<()>
 where
     S: crate::session::Session,
 {
-    todo!()
+    let metadata = args.path.symlink_metadata()
+        .map_err(crate::session::Error::action)?;
+
+    session.reply(Item {
+        path: args.path, // TODO(@panhania): This should be canonicalized.
+        metadata,
+    })?;
+
+    Ok(())
 }
 
 impl crate::request::Args for Args {
@@ -31,7 +39,14 @@ impl crate::request::Args for Args {
     type Proto = rrg_proto::v2::get_file_metadata::Args;
 
     fn from_proto(proto: Self::Proto) -> Result<Args, crate::request::ParseArgsError> {
-        todo!()
+        use crate::request::ParseArgsError::*;
+
+        let path = PathBuf::try_from(proto.path)
+            .map_err(|error| invalid_field("path", error))?;
+
+        Ok(Args {
+            path,
+        })
     }
 }
 
@@ -40,6 +55,10 @@ impl crate::response::Item for Item {
     type Proto = rrg_proto::v2::get_file_metadata::Result;
 
     fn into_proto(self) -> Self::Proto {
-        todo!()
+        let mut proto = rrg_proto::v2::get_file_metadata::Result::default();
+        proto.set_path(self.path.into());
+        proto.set_metadata(self.metadata.into());
+
+        proto
     }
 }
