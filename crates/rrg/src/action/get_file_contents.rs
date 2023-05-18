cbf005fc7d6b5f91684cb237ac0e0390074d2486
Łukasz Hanuszczak <hanuszczak@google.com>
2023-05-18 21:25:24 +0200

    Write tests for `get_file_contents`.

--- crates/rrg/src/action/get_file_contents.rs
+++ crates/rrg/src/action/get_file_contents.rs
@@ -133,3 +133,186 @@ impl std::fmt::Display for LenError {
 
 impl std::error::Error for LenError {
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn handle_empty_file() {
+        let tempdir = tempfile::tempdir()
+            .unwrap();
+
+        std::fs::write(tempdir.path().join("foo"), b"")
+            .unwrap();
+
+        let args = Args {
+            path: tempdir.path().join("foo"),
+            offset: 0,
+            len: usize::MAX,
+        };
+
+        let mut session = crate::session::FakeSession::new();
+        handle(&mut session, args)
+            .unwrap();
+
+        assert_eq!(session.reply_count(), 0);
+        assert_eq!(session.parcel_count(crate::Sink::Blob), 0);
+    }
+
+    #[test]
+    fn handle_small_file_all() {
+        let tempdir = tempfile::tempdir()
+            .unwrap();
+
+        std::fs::write(tempdir.path().join("foo"), b"0123456789")
+            .unwrap();
+
+        let args = Args {
+            path: tempdir.path().join("foo"),
+            offset: 0,
+            len: usize::MAX,
+        };
+
+        let mut session = crate::session::FakeSession::new();
+        handle(&mut session, args)
+            .unwrap();
+
+        assert_eq!(session.reply_count(), 1);
+
+        let item = session.reply::<Item>(0);
+        assert_eq!(item.offset, 0);
+        assert_eq!(item.len, 10);
+
+        assert_eq!(session.parcel_count(crate::Sink::Blob), 1);
+
+        let blob = session.parcel::<crate::blob::Blob>(crate::Sink::Blob, 0);
+        assert_eq!(blob.as_bytes(), b"0123456789");
+    }
+
+    #[test]
+    fn handle_small_file_from_offset() {
+        let tempdir = tempfile::tempdir()
+            .unwrap();
+
+        std::fs::write(tempdir.path().join("foo"), b"0123456789")
+            .unwrap();
+
+        let args = Args {
+            path: tempdir.path().join("foo"),
+            offset: 5,
+            len: usize::MAX,
+        };
+
+        let mut session = crate::session::FakeSession::new();
+        handle(&mut session, args)
+            .unwrap();
+
+        assert_eq!(session.reply_count(), 1);
+
+        let item = session.reply::<Item>(0);
+        assert_eq!(item.offset, 5);
+        assert_eq!(item.len, 5);
+
+        assert_eq!(session.parcel_count(crate::Sink::Blob), 1);
+
+        let blob = session.parcel::<crate::blob::Blob>(crate::Sink::Blob, 0);
+        assert_eq!(blob.as_bytes(), b"56789");
+    }
+
+    #[test]
+    fn handle_small_file_to_len() {
+        let tempdir = tempfile::tempdir()
+            .unwrap();
+
+        std::fs::write(tempdir.path().join("foo"), b"0123456789")
+            .unwrap();
+
+        let args = Args {
+            path: tempdir.path().join("foo"),
+            offset: 0,
+            len: 5,
+        };
+
+        let mut session = crate::session::FakeSession::new();
+        handle(&mut session, args)
+            .unwrap();
+
+        assert_eq!(session.reply_count(), 1);
+
+        let item = session.reply::<Item>(0);
+        assert_eq!(item.offset, 0);
+        assert_eq!(item.len, 5);
+
+        assert_eq!(session.parcel_count(crate::Sink::Blob), 1);
+
+        let blob = session.parcel::<crate::blob::Blob>(crate::Sink::Blob, 0);
+        assert_eq!(blob.as_bytes(), b"01234");
+    }
+
+    #[test]
+    // `/dev/zero` is not available on Windows (nor there is an equivalent).
+    #[cfg_attr(target_family = "windows", ignore)]
+    fn handle_big_file_to_len() {
+        let args = Args {
+            path: PathBuf::from("/dev/zero"),
+            offset: 0,
+            len: MAX_BLOB_LEN * 2 + 1337,
+        };
+
+        let mut session = crate::session::FakeSession::new();
+        handle(&mut session, args)
+            .unwrap();
+
+        assert_eq!(session.reply_count(), 3);
+
+        let item = session.reply::<Item>(0);
+        assert_eq!(item.offset, 0);
+        assert_eq!(item.len, MAX_BLOB_LEN);
+
+        let item = session.reply::<Item>(1);
+        assert_eq!(item.offset, MAX_BLOB_LEN as u64);
+        assert_eq!(item.len, MAX_BLOB_LEN);
+
+        let item = session.reply::<Item>(2);
+        assert_eq!(item.offset, MAX_BLOB_LEN as u64 * 2);
+        assert_eq!(item.len, 1337);
+
+        assert_eq!(session.parcel_count(crate::Sink::Blob), 3);
+
+        let blob = session.parcel::<crate::blob::Blob>(crate::Sink::Blob, 0);
+        assert_eq!(blob.as_bytes().len(), MAX_BLOB_LEN);
+
+        let blob = session.parcel::<crate::blob::Blob>(crate::Sink::Blob, 1);
+        assert_eq!(blob.as_bytes().len(), MAX_BLOB_LEN);
+
+        let blob = session.parcel::<crate::blob::Blob>(crate::Sink::Blob, 2);
+        assert_eq!(blob.as_bytes().len(), 1337);
+    }
+
+    #[test]
+    // `/dev/zero` is not available on Windows (nor there is an equivalent).
+    #[cfg_attr(target_family = "windows", ignore)]
+    fn handle_big_file_from_offset_to_len() {
+        let args = Args {
+            path: PathBuf::from("/dev/zero"),
+            offset: 0xb33f,
+            len: MAX_BLOB_LEN + 1337,
+        };
+
+        let mut session = crate::session::FakeSession::new();
+        handle(&mut session, args)
+            .unwrap();
+
+        assert_eq!(session.reply_count(), 2);
+
+        let item = session.reply::<Item>(0);
+        assert_eq!(item.offset, 0xb33f);
+        assert_eq!(item.len, MAX_BLOB_LEN);
+
+        let item = session.reply::<Item>(1);
+        assert_eq!(item.offset, 0xb33f + MAX_BLOB_LEN as u64);
+        assert_eq!(item.len, 1337);
+    }
+}
