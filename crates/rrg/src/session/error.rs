28e71b64139c25d3ff4f0f1bc5f88d4e2cbe496d
Łukasz Hanuszczak <hanuszczak@google.com>
2023-05-22 21:18:28 +0200

    Create a type for unsupported action errors.

--- crates/rrg/src/session/error.rs
+++ crates/rrg/src/session/error.rs
@@ -18,6 +18,8 @@ pub struct Error {
 pub enum ErrorKind {
     /// The requested action is not known.
     UnknownAction,
+    /// The requested action is not supported.
+    UnsupportedAction,
     /// The arguments given for the action were malformed.
     InvalidArgs,
     // strictly necessary, we can be consistent here and rename this variant.
@@ -48,6 +50,14 @@ impl Error {
             error: Box::new(UnknownActionError { action }),
         }
     }
+
+    /// Converts an action that is not supported to a session error.
+    pub fn unsupported_action(action: crate::request::Action) -> Error {
+        Error {
+            kind: ErrorKind::UnsupportedAction,
+            error: Box::new(UnsupportedActionError { action }),
+        }
+    }
 }
 
 impl Display for Error {
@@ -60,6 +70,11 @@ impl Display for Error {
                 // `self.error` is an instance of `UnknownActionError` which
                 // contains meaningful message, we don't need to provide it
                 // ourselves here.
+                write!(fmt, "{}", self.error)
+            }
+            UnsupportedAction => {
+                // Same as with `UnknownAction` variant, the `self.error` is an
+                // instance of `UnsupportedActionError` and has enough details.
                 write!(fmt, "{}", self.error)
             }
             InvalidArgs => {
@@ -107,6 +122,7 @@ impl From<ErrorKind> for rrg_proto::v2::rrg::Status_Error_Type {
 
         match kind {
             UnknownAction => Self::UNKNOWN_ACTION,
+            UnsupportedAction => Self::UNSUPPORTED_ACTION,
             InvalidArgs => Self::INVALID_ARGS,
             ActionFailure => Self::ACTION_FAILURE,
         }
@@ -127,4 +143,21 @@ impl std::fmt::Display for UnknownActionError {
 }
 
 impl std::error::Error for UnknownActionError {
+}
+
+/// An error type for when the action specified in the request is not supported.
+#[derive(Debug)]
+struct UnsupportedActionError {
+    action: crate::request::Action,
+}
+
+impl std::fmt::Display for UnsupportedActionError {
+
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        // TODO(@panhania): Use `Display`, not `Debug` once it is implemented.
+        write!(fmt, "unsupported action '{:?}'", self.action)
+    }
+}
+
+impl std::error::Error for UnsupportedActionError {
 }
