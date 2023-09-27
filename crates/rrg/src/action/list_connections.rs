eb3c9c0512df8c44d405d3619c927b8822aea51d
Łukasz Hanuszczak <hanuszczak@google.com>
2023-09-27 16:25:21 +0200

    Implement the `list_connections` action.

--- crates/rrg/src/action/list_connections.rs
+++ crates/rrg/src/action/list_connections.rs
@@ -3,7 +3,47 @@
 // Use of this source code is governed by an MIT-style license that can be found
 // in the LICENSE file or at https://opensource.org/licenses/MIT.
 
-pub fn handle<S>(_session: &mut S, _: ()) -> crate::session::Result<()> {
-    let error = std::io::Error::from(std::io::ErrorKind::Unsupported);
-    Err(crate::session::Error::action(error))
+use log::warn;
+
+/// A result of the `list_connections` action.
+struct Item {
+    // Information about the individual connection.
+    conn: ospect::net::Connection,
+}
+
+// Handles invocations of the `list_connections` action.
+pub fn handle<S>(session: &mut S, _: ()) -> crate::session::Result<()>
+where
+    S: crate::session::Session,
+{
+    let conns = ospect::net::all_connections()
+        .map_err(crate::session::Error::action)?;
+
+    for conn in conns {
+        let conn = match conn {
+            Ok(conn) => conn,
+            Err(error) => {
+                warn!("failed to obtain connection information: {}", error);
+                continue;
+            }
+        };
+
+        session.reply(Item {
+            conn,
+        })?;
+    }
+
+    Ok(())
+}
+
+impl crate::response::Item for Item {
+
+    type Proto = rrg_proto::v2::list_connections::Result;
+
+    fn into_proto(self) -> rrg_proto::v2::list_connections::Result {
+        let mut proto = rrg_proto::v2::list_connections::Result::new();
+        proto.set_connection(self.conn.into());
+
+        proto
+    }
 }
