0e96f7dbb968b62ccb103ba8a38af50da82b3fed
Łukasz Hanuszczak <hanuszczak@google.com>
2023-10-12 19:46:16 +0200

    Reuse interfaces tests from deprecated action.

--- crates/rrg/src/action/list_interfaces.rs
+++ crates/rrg/src/action/list_interfaces.rs
@@ -37,3 +37,26 @@ impl crate::response::Item for Item {
         proto
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn loopback_exists() {
+        let mut session = crate::session::FakeSession::new();
+        assert!(handle(&mut session, ()).is_ok());
+
+        // A single network interface can be associated with many IP addresses,
+        // some of which might not be traditional loopback addresses. Therefore,
+        // we use `any` instead of `all`.
+        fn is_loopback(iface: &ospect::net::Interface) -> bool {
+            iface.ip_addrs().any(std::net::IpAddr::is_loopback)
+        }
+
+        assert! {
+            session.replies().any(|item: &Item| is_loopback(&item.iface))
+        }
+    }
+}
