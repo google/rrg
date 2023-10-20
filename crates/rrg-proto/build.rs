fd7a99f0969509bda69f8a4fac8173a057f16b2b
Łukasz Hanuszczak <hanuszczak@google.com>
2023-10-20 15:10:20 +0200

    Remove generation of deprecated proto messages.

--- crates/rrg-proto/build.rs
+++ crates/rrg-proto/build.rs
@@ -5,20 +5,6 @@
 
 use std::path::PathBuf;
 
-const PROTOS: &'static [&'static str] = &[
-    "../../vendor/grr/grr/proto/grr_response_proto/semantic.proto",
-    "../../vendor/grr/grr/proto/grr_response_proto/sysinfo.proto",
-    "../../vendor/grr/grr/proto/grr_response_proto/knowledge_base.proto",
-    "../../vendor/grr/grr/proto/grr_response_proto/jobs.proto",
-    "../../vendor/grr/grr/proto/grr_response_proto/timeline.proto",
-    "../../vendor/grr/grr/proto/grr_response_proto/anomaly.proto",
-    "../../vendor/grr/grr/proto/grr_response_proto/export.proto",
-    "../../vendor/grr/grr/proto/grr_response_proto/objects.proto",
-    "../../vendor/grr/grr/proto/grr_response_proto/output_plugin.proto",
-    "../../vendor/grr/grr/proto/grr_response_proto/flows.proto",
-    "../../vendor/grr/grr/proto/grr_response_proto/user.proto",
-];
-
 const PROTOS_V2: &'static [&'static str] = &[
     "../../proto/rrg.proto",
     "../../proto/rrg/blob.proto",
@@ -39,21 +25,6 @@ fn main() {
     let outdir: PathBuf = std::env::var("OUT_DIR")
         .expect("no output directory")
         .into();
-
-
-    let proto_out_dir = outdir.join("proto");
-    std::fs::create_dir_all(&proto_out_dir).unwrap();
-
-    protobuf_codegen_pure::Codegen::new()
-        .out_dir(&proto_out_dir)
-        .include("../../vendor/grr/grr/proto")
-        .include("../../vendor/protobuf/src")
-        .inputs(PROTOS)
-        .customize(protobuf_codegen_pure::Customize {
-            gen_mod_rs: Some(true),
-            ..Default::default()
-        })
-        .run().unwrap();
 
     for proto in PROTOS_V2 {
         println!("cargo:rerun-if-changed={}", proto);
