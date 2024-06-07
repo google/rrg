37f676f41e5aff5b137523cad2706b6aa995c182
Łukasz Hanuszczak <hanuszczak@google.com>
2024-06-07 06:22:12 +0200

    Unimplement conversion traits for predefined keys.

--- crates/winreg/src/windows.rs
+++ crates/winreg/src/windows.rs
@@ -19,48 +19,21 @@ impl PredefinedKey {
     pub fn open(&self, subkey_name: &std::ffi::OsStr) -> std::io::Result<OpenKey> {
         // SAFETY: Predefined keys are guaranteed to be valid open keys.
         unsafe {
-            open_raw_key((*self).into(), subkey_name)
+            open_raw_key(self.as_raw_key(), subkey_name)
         }
     }
 
     pub fn info(&self) -> std::io::Result<KeyInfo> {
         // SAFETY: Predefined keys are guaranteed to be valid open keys.
         unsafe {
-            query_raw_key_info((*self).into())
+            query_raw_key_info(self.as_raw_key())
         }
     }
-}
 
-impl TryFrom<windows_sys::Win32::System::Registry::HKEY> for PredefinedKey {
-
-    type Error = InvalidPredefinedKeyError;
-
-    fn try_from(
-        hkey: windows_sys::Win32::System::Registry::HKEY,
-    ) -> Result<PredefinedKey, InvalidPredefinedKeyError> {
+    pub fn as_raw_key(&self) -> windows_sys::Win32::System::Registry::HKEY {
         use windows_sys::Win32::System::Registry::*;
 
-        match hkey {
-            HKEY_CLASSES_ROOT => Ok(PredefinedKey::ClassesRoot),
-            HKEY_CURRENT_CONFIG => Ok(PredefinedKey::CurrentConfig),
-            HKEY_CURRENT_USER => Ok(PredefinedKey::CurrentUser),
-            HKEY_CURRENT_USER_LOCAL_SETTINGS => Ok(PredefinedKey::CurrentUserLocalSettings),
-            HKEY_LOCAL_MACHINE => Ok(PredefinedKey::LocalMachine),
-            HKEY_PERFORMANCE_DATA => Ok(PredefinedKey::PerformanceData),
-            HKEY_PERFORMANCE_NLSTEXT => Ok(PredefinedKey::PerformanceNlstext),
-            HKEY_PERFORMANCE_TEXT => Ok(PredefinedKey::PerformanceText),
-            HKEY_USERS => Ok(PredefinedKey::Users),
-            _ => Err(InvalidPredefinedKeyError { hkey }),
-        }
-    }
-}
-
-impl From<PredefinedKey> for windows_sys::Win32::System::Registry::HKEY {
-
-    fn from(key: PredefinedKey) -> windows_sys::Win32::System::Registry::HKEY {
-        use windows_sys::Win32::System::Registry::*;
-
-        match key {
+        match self {
             PredefinedKey::ClassesRoot => HKEY_CLASSES_ROOT,
             PredefinedKey::CurrentConfig => HKEY_CURRENT_CONFIG,
             PredefinedKey::CurrentUser => HKEY_CURRENT_USER,
@@ -72,23 +45,6 @@ impl From<PredefinedKey> for windows_sys::Win32::System::Registry::HKEY {
             PredefinedKey::Users => HKEY_USERS,
         }
     }
-}
-
-/// The error type used when an attempt to convert a raw integer value into
-/// [`PredefinedKey`] fails.
-#[derive(Debug)]
-pub struct InvalidPredefinedKeyError {
-    hkey: windows_sys::Win32::System::Registry::HKEY,
-}
-
-impl std::fmt::Display for InvalidPredefinedKeyError {
-
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "invalid predefined key value: {}", self.hkey)
-    }
-}
-
-impl std::error::Error for InvalidPredefinedKeyError {
 }
 
 pub struct OpenKey(windows_sys::Win32::System::Registry::HKEY);
