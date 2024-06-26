a105aebd46ea685e3bdab9c998b6df36e299419f
Łukasz Hanuszczak <hanuszczak@google.com>
2024-06-26 16:00:45 +0200

    Add missing license header.

--- crates/rrg/src/action/list_winreg_values.rs
+++ crates/rrg/src/action/list_winreg_values.rs
@@ -1,3 +1,8 @@
+// Copyright 2024 Google LLC
+//
+// Use of this source code is governed by an MIT-style license that can be found
+// in the LICENSE file or at https://opensource.org/licenses/MIT.
+
 /// Arguments of the `list_winreg_values` action.
 #[cfg(target_family = "windows")]
 pub struct Args {
