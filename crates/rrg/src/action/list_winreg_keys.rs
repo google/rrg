d31cfb8c41e774a82162f1550949aaba50467040
Łukasz Hanuszczak <hanuszczak@google.com>
2024-06-27 08:11:24 +0200

    Define arguments and result types.

--- crates/rrg/src/action/list_winreg_keys.rs
+++ crates/rrg/src/action/list_winreg_keys.rs
@@ -6,11 +6,21 @@
 /// Arguments of the `list_winreg_keys` action.
 #[cfg(target_family = "windows")]
 pub struct Args {
+    /// Root predefined key of the key to list subkeys of.
+    root: winreg::PredefinedKey,
+    /// Key relative to `root` to list subkeys of.
+    key: std::ffi::OsString,
 }
 
 /// A result of the `list_winreg_keys` action.
 #[cfg(target_family = "windows")]
 struct Item {
+    /// Root predefined key of the listed subkey.
+    root: winreg::PredefinedKey,
+    /// Key relative to `root` of the listed subkey.
+    key: std::ffi::OsString,
+    /// Listed subkey.
+    subkey: std::ffi::OsString,
 }
 
 /// Handles invocations of the `list_winreg_keys` action.
@@ -38,7 +48,17 @@ impl crate::request::Args for Args {
     type Proto = rrg_proto::list_winreg_keys::Args;
 
     fn from_proto(mut proto: Self::Proto) -> Result<Args, crate::request::ParseArgsError> {
-        todo!()
+        let root = match proto.root.enum_value() {
+            Ok(root) => winreg::PredefinedKey::try_from(root),
+            Err(value) => Err(rrg_proto::ParseWinregPredefinedKeyError { value }),
+        }.map_err(|error| {
+            crate::request::ParseArgsError::invalid_field("root", error)
+        })?;
+
+        Ok(Args {
+            root,
+            key: std::ffi::OsString::from(proto.take_key()),
+        })
     }
 }
 
@@ -48,6 +68,11 @@ impl crate::response::Item for Item {
     type Proto = rrg_proto::list_winreg_keys::Result;
 
     fn into_proto(self) -> Self::Proto {
-        todo!()
+        let mut proto = rrg_proto::list_winreg_keys::Result::new();
+        proto.set_root(self.root.into());
+        proto.set_key(self.key.to_string_lossy().into_owned());
+        proto.set_subkey(self.subkey.to_string_lossy().into_owned());
+
+        proto
     }
 }
