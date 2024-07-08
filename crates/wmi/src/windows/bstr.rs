f181cf5d631edf285043e192fe4477b88074162f
Łukasz Hanuszczak <hanuszczak@google.com>
2024-07-08 09:32:07 +0200

    Add borrowed variant of the `BSTR` wrapper.

--- crates/wmi/src/windows/bstr.rs
+++ crates/wmi/src/windows/bstr.rs
@@ -3,6 +3,7 @@
 // Use of this source code is governed by an MIT-style license that can be found
 // in the LICENSE file or at https://opensource.org/licenses/MIT.
 
+/// Owned wrapper around [`windows_sys::core::BSTR`].
 pub struct BString(windows_sys::core::BSTR);
 
 impl BString {
@@ -50,34 +51,25 @@ impl BString {
     pub unsafe fn from_raw_bstr(raw: windows_sys::core::BSTR) -> BString {
         BString(raw)
     }
-
     /// Returns the length of the string in bytes.
-    pub fn count_bytes(&self) -> usize {
-        // SAFETY: Every `BSTR` instance is prefixed with 4-byte length of the
-        // string (excluding the null terminator) [1]. This value is placed
-        // directly *before* the pointer that we have, so we offset it and read
-        // from there.
-        //
-        // [1] https://learn.microsoft.com/en-us/previous-versions/windows/desktop/automat/bstr#remarks
-        unsafe {
-            *self.0.cast::<u8>().offset(-4).cast::<u32>() as usize
-        }
+    pub fn count_bytes(self) -> usize {
+        self.as_bstr().count_bytes()
     }
 
     /// Copies the string into an owned [`std::ffi::OsString`].
-    pub fn to_os_string(&self) -> std::ffi::OsString {
-        use std::os::windows::ffi::OsStringExt as _;
-
-        let len = self.count_bytes() / std::mem::size_of::<u16>();
-
-        // SAFETY: We know that the pointer is valid and calculate its length by
-        // taking it byte length and dividing by the size of each character (so,
-        // 2 bytes).
-        std::os::windows::ffi::OsStringExt::from_wide(unsafe {
-            std::slice::from_raw_parts(self.0, len)
-        })
+    pub fn to_os_string(self) -> std::ffi::OsString {
+        self.as_bstr().to_os_string()
     }
 
+    /// Converts the string to its borrowed variant.
+    pub fn as_bstr<'a>(&'a self) -> BStr<'a> {
+        // SAFETY: We hold a valid `BSTR` instance with guaranteed lifetime.
+        unsafe {
+            BStr::from_raw_bstr(self.0)
+        }
+    }
+
+    /// Returns the raw `BSTR` backing the string.
     pub fn as_raw_bstr(&self) -> windows_sys::core::BSTR {
         self.0
     }
@@ -93,6 +85,61 @@ impl Drop for BString {
         unsafe {
             windows_sys::Win32::Foundation::SysFreeString(self.0)
         }
+    }
+}
+
+/// Borrowed wrapper around [`windows_sys::core::BSTR`].
+#[derive(Copy, Clone)]
+pub struct BStr<'a> {
+    raw: windows_sys::core::BSTR,
+    phantom: std::marker::PhantomData<&'a ()>,
+}
+
+impl<'a> BStr<'a> {
+
+    /// Creates a new `BSTR` wrapper from raw pointer without taking ownership.
+    ///
+    /// # Safety
+    ///
+    /// The pointer must be valid instance of `BSTR`. It has similar semantics
+    /// and requirements as [`std::slice::from_raw_parts`].
+    pub unsafe fn from_raw_bstr(raw: windows_sys::core::BSTR) -> BStr<'a> {
+        BStr {
+            raw: raw,
+            phantom: std::marker::PhantomData,
+        }
+    }
+
+    /// Returns the length of the string in bytes.
+    pub fn count_bytes(self) -> usize {
+        // SAFETY: Every `BSTR` instance is prefixed with 4-byte length of the
+        // string (excluding the null terminator) [1]. This value is placed
+        // directly *before* the pointer that we have, so we offset it and read
+        // from there.
+        //
+        // [1] https://learn.microsoft.com/en-us/previous-versions/windows/desktop/automat/bstr#remarks
+        unsafe {
+            *self.raw.cast::<u8>().offset(-4).cast::<u32>() as usize
+        }
+    }
+
+    /// Copies the string into an owned [`std::ffi::OsString`].
+    pub fn to_os_string(self) -> std::ffi::OsString {
+        use std::os::windows::ffi::OsStringExt as _;
+
+        let len = self.count_bytes() / std::mem::size_of::<u16>();
+
+        // SAFETY: We know that the pointer is valid and calculate its length by
+        // taking it byte length and dividing by the size of each character (so,
+        // 2 bytes).
+        std::os::windows::ffi::OsStringExt::from_wide(unsafe {
+            std::slice::from_raw_parts(self.raw, len)
+        })
+    }
+
+    /// Returns the raw `BSTR` backing the string.
+    pub fn as_raw_bstr(self) -> windows_sys::core::BSTR {
+        self.raw
     }
 }
 
