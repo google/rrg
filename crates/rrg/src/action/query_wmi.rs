a9ee79e1c00453a446ed3dfafd4ece1dcac066e3
Łukasz Hanuszczak <hanuszczak@google.com>
2024-07-12 22:25:36 +0200

    Define `query_wmi` argument and result types.

--- crates/rrg/src/action/query_wmi.rs
+++ crates/rrg/src/action/query_wmi.rs
@@ -6,11 +6,17 @@
 /// Arguments of the `query_wmi` action.
 #[cfg(target_family = "windows")]
 pub struct Args {
+    /// WQL query [1] to run.
+    ///
+    /// [1]: https://learn.microsoft.com/en-us/windows/win32/wmisdk/wql-sql-for-wmi
+    query: std::ffi::OsString,
 }
 
 /// A result of the `query_wmi` action.
 #[cfg(target_family = "windows")]
 struct Item {
+    /// Single row of the query result.
+    row: wmi::QueryRow,
 }
 
 /// Handles invocations of the `query_wmi` action.
@@ -38,7 +44,9 @@ impl crate::request::Args for Args {
     type Proto = rrg_proto::query_wmi::Args;
 
     fn from_proto(mut proto: Self::Proto) -> Result<Args, crate::request::ParseArgsError> {
-        todo!()
+        Ok(Args {
+            query: std::ffi::OsString::from(proto.take_query()),
+        })
     }
 }
 
@@ -48,6 +56,56 @@ impl crate::response::Item for Item {
     type Proto = rrg_proto::query_wmi::Result;
 
     fn into_proto(self) -> Self::Proto {
-        todo!()
+        let mut proto = rrg_proto::query_wmi::Result::new();
+
+        for (name, value) in self.row {
+            let proto_name = name.to_string_lossy().into_owned();
+            let mut proto_value = rrg_proto::query_wmi::Value::new();
+
+            match value {
+                wmi::QueryValue::None => (),
+                wmi::QueryValue::Bool(bool) => {
+                    proto_value.set_bool(bool)
+                }
+                wmi::QueryValue::U8(u8) => {
+                    proto_value.set_uint(u64::from(u8))
+                }
+                wmi::QueryValue::I8(i8) => {
+                    proto_value.set_int(i64::from(i8))
+                }
+                wmi::QueryValue::U16(u16) => {
+                    proto_value.set_uint(u64::from(u16))
+                }
+                wmi::QueryValue::I16(i16) => {
+                    proto_value.set_int(i64::from(i16))
+                }
+                wmi::QueryValue::U32(u32) => {
+                    proto_value.set_uint(u64::from(u32))
+                }
+                wmi::QueryValue::I32(i32) => {
+                    proto_value.set_int(i64::from(i32))
+                }
+                wmi::QueryValue::U64(u64) => {
+                    proto_value.set_uint(u64)
+                }
+                wmi::QueryValue::I64(i64) => {
+                    proto_value.set_int(i64)
+                }
+                wmi::QueryValue::F32(f32) => {
+                    proto_value.set_float(f32)
+                }
+                wmi::QueryValue::F64(f64) => {
+                    proto_value.set_double(f64)
+                }
+                wmi::QueryValue::String(string) => {
+                    proto_value.set_string(string.to_string_lossy().into_owned())
+                }
+                wmi::QueryValue::Unsupported(_) => (),
+            }
+
+            proto.mut_row().insert(proto_name, proto_value);
+        }
+
+        proto
     }
 }
