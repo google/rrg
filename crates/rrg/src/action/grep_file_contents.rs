9b336ad2be427da546c47b7f515f4fefa63ae09d
Łukasz Hanuszczak <hanuszczak@google.com>
2024-09-25 16:06:10 +0200

    Define `grep_file_contents` interface.

--- crates/rrg/src/action/grep_file_contents.rs
+++ crates/rrg/src/action/grep_file_contents.rs
@@ -3,12 +3,22 @@
 // Use of this source code is governed by an MIT-style license that can be found
 // in the LICENSE file or at https://opensource.org/licenses/MIT.
 
+use std::path::PathBuf;
+
 /// Arguments of the `grep_file_contents` action.
 pub struct Args {
+    /// Path to the file to grep the contents of.
+    path: PathBuf,
+    /// Regular expression to search for in the file contents.
+    regex: regex::Regex,
 }
 
 /// Result of the `grep_file_contents` action.
 pub struct Item {
+    /// Byte offset within the file from which the content matched.
+    offset: u64,
+    /// Content that matched the specified regular expression.
+    content: String,
 }
 
 /// Handles invocations of the `grep_file_contents` action.
@@ -23,8 +33,19 @@ impl crate::request::Args for Args {
 
     type Proto = rrg_proto::grep_file_contents::Args;
 
-    fn from_proto(proto: Self::Proto) -> Result<Args, crate::request::ParseArgsError> {
-        todo!()
+    fn from_proto(mut proto: Self::Proto) -> Result<Args, crate::request::ParseArgsError> {
+        use crate::request::ParseArgsError;
+
+        let path = PathBuf::try_from(proto.take_path())
+            .map_err(|error| ParseArgsError::invalid_field("path", error))?;
+
+        let regex = regex::Regex::new(proto.regex())
+            .map_err(|error| ParseArgsError::invalid_field("regex", error))?;
+
+        Ok(Args {
+            path,
+            regex,
+        })
     }
 }
 
@@ -33,6 +54,10 @@ impl crate::response::Item for Item {
     type Proto = rrg_proto::grep_file_contents::Result;
 
     fn into_proto(self) -> Self::Proto {
-        todo!()
+        let mut proto = Self::Proto::default();
+        proto.set_offset(self.offset);
+        proto.set_content(self.content);
+
+        proto
     }
 }
