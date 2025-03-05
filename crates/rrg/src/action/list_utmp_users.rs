a0256e3f5fed6b3a0ed2ebbe5c133d3b711f00ce
Łukasz Hanuszczak <hanuszczak@google.com>
2025-03-05 19:48:43 +0100

    Implement basic logic for `list_utmp_users`.

--- crates/rrg/src/action/list_utmp_users.rs
+++ crates/rrg/src/action/list_utmp_users.rs
@@ -6,13 +6,17 @@
 /// Arguments of the `list_utmp_users` action.
 #[cfg(target_os = "linux")]
 pub struct Args {
-    // TODO.
+    /// Path to the file to use as a source for `utmp` records.
+    ///
+    /// Typically this should be `/var/log/wtmp`.
+    path: std::path::PathBuf,
 }
 
 /// Result of the `list_utmp_users` action.
 #[cfg(target_os = "linux")]
 pub struct Item {
-    // TODO.
+    /// Name of an individual user retrieved from `utmp` records.
+    username: Vec<u8>,
 }
 
 /// Handles invocations of the `list_utmp_users` action.
@@ -21,7 +25,59 @@ pub fn handle<S>(session: &mut S, args: Args) -> crate::session::Result<()>
 where
     S: crate::session::Session,
 {
-    todo!()
+    let mut file = std::fs::File::open(&args.path)
+        .map_err(crate::session::Error::action)?;
+
+    let mut usernames = std::collections::HashSet::new();
+
+    loop {
+        use std::io::Read as _;
+
+        let mut buf = [0u8; std::mem::size_of::<libc::utmpx>()];
+        match file.read_exact(&mut buf) {
+            Ok(()) => (),
+            Err(error) if error.kind() == std::io::ErrorKind::UnexpectedEof => {
+                break;
+            }
+            Err(error) => {
+                return Err(crate::session::Error::action(error));
+            }
+        }
+
+        const TYPE_OFFSET: usize = std::mem::offset_of!(libc::utmpx, ut_type);
+        const TYPE_SIZE: usize = std::mem::size_of::<libc::c_short>();
+
+        let type_bytes = <_>::try_from(&buf[TYPE_OFFSET..TYPE_OFFSET + TYPE_SIZE])
+            .expect("invalid type of utmp");
+        // According to [1], `utmp` uses litle-endian byte order. However, it
+        // might be the case that it actually uses native-endian. This should be
+        // verified and adjusted if needed.
+        //
+        // [1]: https://github.com/libyal/dtformats/blob/main/documentation/Utmp%20login%20records%20format.asciidoc
+        let r#type = libc::c_short::from_le_bytes(type_bytes);
+
+        if r#type != libc::USER_PROCESS {
+            continue;
+        }
+
+        const USER_OFFSET: usize = std::mem::offset_of!(libc::utmpx, ut_user);
+
+        let user = std::ffi::CStr::from_bytes_until_nul(&buf[USER_OFFSET..])
+            .map_err(|error| {
+                std::io::Error::new(std::io::ErrorKind::InvalidData, error)
+            })
+            .map_err(crate::session::Error::action)?;
+
+        usernames.insert(user.to_bytes().to_vec());
+    }
+
+    for username in usernames {
+        session.reply(Item {
+            username,
+        })?;
+    }
+
+    Ok(())
 }
 
 #[cfg(not(target_os = "linux"))]
@@ -39,7 +95,14 @@ impl crate::request::Args for Args {
     type Proto = rrg_proto::list_utmp_users::Args;
 
     fn from_proto(mut proto: Self::Proto) -> Result<Args, crate::request::ParseArgsError> {
-        todo!()
+        use crate::request::ParseArgsError;
+
+        let path = std::path::PathBuf::try_from(proto.take_path())
+            .map_err(|error| ParseArgsError::invalid_field("path", error))?;
+
+        Ok(Args {
+            path,
+        })
     }
 }
 
@@ -49,6 +112,35 @@ impl crate::response::Item for Item {
     type Proto = rrg_proto::list_utmp_users::Result;
 
     fn into_proto(self) -> Self::Proto {
-        todo!()
+        let mut proto = rrg_proto::list_utmp_users::Result::new();
+        proto.set_username(self.username);
+
+        proto
+    }
+}
+
+#[cfg(target_os = "linux")]
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn handle_var_log_wtmp_logged_in_user() {
+        let args = Args {
+            path: "/var/log/wtmp".into(),
+        };
+
+        let mut session = crate::session::FakeSession::new();
+        assert!(handle(&mut session, args).is_ok());
+
+        let username_env = std::env::var_os("USER")
+            .expect("no $USER env var");
+
+        assert! {
+            session.replies::<Item>().any(|item| {
+                item.username == username_env.clone().into_encoded_bytes()
+            })
+        }
     }
 }
