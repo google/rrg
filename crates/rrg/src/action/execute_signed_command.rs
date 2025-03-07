61dc6b03ec1ce203da4a08c843138386f48f01fa
Łukasz Hanuszczak <hanuszczak@google.com>
2025-03-07 17:24:29 +0100

    Remove top-level imports.

--- crates/rrg/src/action/execute_signed_command.rs
+++ crates/rrg/src/action/execute_signed_command.rs
@@ -3,14 +3,6 @@
 // Use of this source code is governed by an MIT-style license that can be found
 // in the LICENSE file or at https://opensource.org/licenses/MIT.
 
-use std::{
-    fmt::Debug,
-    io::{Read, Write},
-    process::{Command, ExitStatus},
-};
-
-use protobuf::Message;
-
 // TODO(s-westphal): Check and update max size.
 const MAX_OUTPUT_SIZE: usize = 4048;
 const COMMAND_EXECUTION_CHECK_INTERVAL: std::time::Duration = std::time::Duration::from_millis(100);
@@ -32,7 +24,7 @@ pub struct Args {
 /// Result of the `execute_signed_command` action.
 pub struct Item {
     /// Exit status of the command subprocess.
-    exit_status: ExitStatus,
+    exit_status: std::process::ExitStatus,
     /// Standard output of the command executiom.
     stdout: Vec<u8>,
     /// Wheather standard output is truncated.
@@ -73,6 +65,7 @@ pub fn handle<S>(session: &mut S, args: Args) -> crate::session::Result<()>
 where
     S: crate::session::Session,
 {
+    use std::io::{Read as _, Write as _};
     use crate::request::ParseArgsError;
 
     match session.args().command_verification_key {
@@ -82,7 +75,7 @@ where
         None => return Err(crate::session::Error::action(MissingCommandVerificationKeyError)),
     };
 
-    let mut command_process = Command::new(&args.path)
+    let mut command_process = std::process::Command::new(&args.path)
         .stdin(std::process::Stdio::piped())
         .args(args.args)
         .env_clear()
@@ -177,6 +170,7 @@ impl crate::request::Args for Args {
 
     fn from_proto(mut proto: Self::Proto) -> Result<Args, crate::request::ParseArgsError> {
         use crate::request::ParseArgsError;
+        use protobuf::Message as _;
 
         let raw_signature = proto.take_command_ed25519_signature();
 
@@ -443,6 +437,7 @@ mod tests {
 
     #[test]
     fn handle_invalid_signature() {
+        use protobuf::Message as _;
         use crate::request::Args as _;
 
         let signing_key = ed25519_dalek::SigningKey::generate(&mut rand::rngs::OsRng);
