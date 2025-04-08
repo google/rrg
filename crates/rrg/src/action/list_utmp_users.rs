e68710ced9cf1783019ad65b571b2337156b61ac
Łukasz Hanuszczak <hanuszczak@google.com>
2025-04-08 16:22:02 +0200

    Disable custom `utmp` file tests on non-`x86_64`.

--- crates/rrg/src/action/list_utmp_users.rs
+++ crates/rrg/src/action/list_utmp_users.rs
@@ -135,6 +135,10 @@ mod tests {
     use super::*;
 
     #[test]
+    // Looks like `utmp` entries can have different size depending on the
+    // platform (e.g. on `aarch64` it has a size of 400 bytes) and so `x86_64`
+    // samples will not work there.
+    #[cfg_attr(not(target_arch = "x86_64"), ignore)]
     fn handle_custom_utmp_file() {
         use std::io::Write as _;
 
