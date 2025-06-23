9e6ae9a4a3c5416249bf925f076accbba60667e4
Łukasz Hanuszczak <hanuszczak@google.com>
2025-06-23 21:36:29 +0200

    Remove no longer used `copy_until` utility.

--- crates/rrg/src/io.rs
+++ crates/rrg/src/io.rs
@@ -9,71 +9,10 @@
 //! available in `std::io`, mostly operating on the standard `Read` and `Write`
 //! traits.
 
-use std::io::{Read, Write, Result};
+use std::io::Read;
 
 // The same as in the Rust's standard library.
 const DEFAULT_BUF_SIZE: usize = 8 * 1024;
-
-/// Copies contents of one buffer into the other until a condition is met.
-///
-/// This function should behave similarly to the standard `std::io::copy` with
-/// the difference that it can be given a condition when copying should stop.
-///
-/// On success, the total number of bytes copied from `reader` to `writer` is
-/// returned.
-///
-/// Note that the predicate is not checked after each copied byte. Therefore,
-/// there are no guarantees about when exactly and how often it will be called.
-///
-/// # Errors
-///
-/// The errors is reported immediately if there is an error when reading from
-/// the input or writing to the output.
-///
-/// Like `std::io::copy`, this function will retry instances of `Interrupted`
-/// errors.
-///
-/// # Examples
-///
-/// ```no_run
-/// use std::fs::File;
-///
-/// let mut rand = File::open("/dev/random").unwrap();
-/// let mut buf = vec!();
-///
-/// rrg::io::copy_until(&mut rand, &mut buf, |_, writer| writer.len() >= 1024);
-///
-/// println!("random bytes: {:?}", buf);
-/// ```
-pub fn copy_until<R, W, P>(reader: &mut R, writer: &mut W, mut pred: P)
-    -> Result<u64>
-where
-    R: Read,
-    W: Write,
-    P: FnMut(&R, &W) -> bool,
-{
-    let mut buf = [0; DEFAULT_BUF_SIZE];
-    let mut written = 0;
-
-    loop {
-        if pred(reader, writer) {
-            break;
-        }
-
-        use std::io::ErrorKind::*;
-        let len = match reader.read(&mut buf[..]) {
-            Ok(0) => break,
-            Ok(len) => len,
-            Err(ref error) if error.kind() == Interrupted => continue,
-            Err(error) => return Err(error),
-        };
-
-        writer.write_all(&buf[..len])?;
-        written += len as u64;
-    }
-
-    Ok(written)
-}
 
 /// Buffered reader for efficent line reading.
 ///
@@ -284,55 +223,6 @@ mod tests {
     use super::*;
 
     use quickcheck::quickcheck;
-
-    #[test]
-    fn test_copy_until_with_empty_buffer() {
-        let mut reader: &[u8] = b"";
-        let mut writer = vec!();
-
-        let result = copy_until(&mut reader, &mut writer, |_, _| false);
-        assert_eq!(result.unwrap(), 0);
-        assert_eq!(writer, b"");
-    }
-
-    #[test]
-    fn test_copy_until_begin() {
-        let mut reader: &[u8] = b"foobar";
-        let mut writer = vec!();
-
-        let result = copy_until(&mut reader, &mut writer, |_, _| true);
-        assert_eq!(result.unwrap(), 0);
-        assert_eq!(writer, b"");
-    }
-
-    #[test]
-    fn test_copy_until_end() {
-        let mut reader: &[u8] = b"foobar";
-        let mut writer = vec!();
-
-        let result = copy_until(&mut reader, &mut writer, |_, _| false);
-        assert_eq!(result.unwrap(), 6);
-        assert_eq!(writer, b"foobar");
-    }
-
-    #[test]
-    fn test_copy_until_specific_size() {
-        let limit = 4 * 1024 * 1024;
-
-        let mut reader = std::io::repeat(0x42);
-        let mut writer = vec!();
-
-        // This should verify that copying eventually stops after the condition
-        // is met since the reader is infinite.
-        assert! {
-            copy_until(&mut reader, &mut writer, |_, writer| {
-                writer.len() > limit
-            }).is_ok()
-        };
-
-        assert!(writer.iter().all(|item| *item == 0x42));
-        assert!(writer.len() > limit);
-    }
 
     #[test]
     fn line_reader_empty() {
