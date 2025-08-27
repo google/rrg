441c5a56d9a20c73f883cc086da1af0e24e0d7e4
Łukasz Hanuszczak <hanuszczak@google.com>
2025-08-27 09:19:24 +0200

    Add test case with mixed argument usage.

--- crates/rrg/src/action/execute_signed_command.rs
+++ crates/rrg/src/action/execute_signed_command.rs
@@ -1050,6 +1050,41 @@ mod tests {
     }
 
     #[test]
+    fn args_form_proto_args_mixed() {
+        let signing_key = ed25519_dalek::SigningKey::generate(&mut rand::rngs::OsRng);
+
+        let mut command_proto = rrg_proto::execute_signed_command::Command::new();
+        command_proto.mut_path().set_raw_bytes(b"/foo/bar".into());
+
+        let mut arg_quux = rrg_proto::execute_signed_command::command::Arg::new();
+        arg_quux.set_unsigned_allowed(true);
+        command_proto.mut_args().push(arg_quux);
+
+        let mut arg_norf = rrg_proto::execute_signed_command::command::Arg::new();
+        arg_norf.set_signed(String::from("norf"));
+        command_proto.mut_args().push(arg_norf);
+
+        let mut arg_thud = rrg_proto::execute_signed_command::command::Arg::new();
+        arg_thud.set_unsigned_allowed(true);
+        command_proto.mut_args().push(arg_thud);
+
+        use protobuf::Message as _;
+        let command_bytes = command_proto.write_to_bytes()
+            .unwrap();
+
+        let mut args_proto = rrg_proto::execute_signed_command::Args::new();
+        args_proto.set_command_ed25519_signature(signing_key.sign(&command_bytes).to_vec());
+        args_proto.set_command(command_bytes);
+        args_proto.mut_unsigned_args().push(String::from("quux"));
+        args_proto.mut_unsigned_args().push(String::from("thud"));
+
+        let args = <Args as crate::request::Args>::from_proto(args_proto)
+            .unwrap();
+        assert_eq!(args.path, std::path::Path::new("/foo/bar"));
+        assert_eq!(args.args, ["quux", "norf", "thud"]);
+    }
+
+    #[test]
     fn args_from_proto_args_unsigned_missing() {
         let signing_key = ed25519_dalek::SigningKey::generate(&mut rand::rngs::OsRng);
 
