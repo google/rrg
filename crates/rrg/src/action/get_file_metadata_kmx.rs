f88e7cd4f2b0bae117f3efce8d8b3595783e17c7
Łukasz Hanuszczak <hanuszczak@google.com>
2025-11-26 19:14:55 +0100

    Create test utilities for loop devices.

--- crates/rrg/src/action/get_file_metadata_kmx.rs
+++ crates/rrg/src/action/get_file_metadata_kmx.rs
@@ -38,3 +38,119 @@ impl crate::response::Item for Item {
         todo!()
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+
+    struct LoopDev {
+        path: std::path::PathBuf,
+        is_closed: bool,
+    }
+
+    impl LoopDev {
+
+        fn new<P>(file_path: P) -> std::io::Result<LoopDev>
+        where
+            P: AsRef<std::path::Path>,
+        {
+            use regex::Regex;
+
+            let output = std::process::Command::new("udisksctl")
+                .arg("loop-setup")
+                .arg("--file").arg(file_path.as_ref())
+                .arg("--no-user-interaction")
+                .output()?;
+            if !output.status.success() {
+                return Err(std::io::Error::new(std::io::ErrorKind::Other, format! {
+                    "failed to run `udisksctl loop-setup` (stdout: {:?}, stderr: {:?})",
+                    String::from_utf8_lossy(&output.stdout).as_ref(),
+                    String::from_utf8_lossy(&output.stderr).as_ref(),
+                }))
+            }
+            let output_stdout = String::from_utf8_lossy(&output.stdout);
+
+            match Regex::new("Mapped file .* as (?P<devloop>.*)\\.")
+                .unwrap()
+                .captures(&output_stdout)
+            {
+                Some(captures) => Ok(LoopDev {
+                    path: std::path::PathBuf::from(&captures["devloop"]),
+                    is_closed: false,
+                }),
+                None => return Err(std::io::Error::new(std::io::ErrorKind::Other, format! {
+                    "unexpected `udisksctl loop-setup` output: {:?}",
+                    output_stdout,
+                })),
+            }
+        }
+
+        fn close(mut self) -> std::io::Result<()> {
+            assert!(!self.is_closed);
+            // We set this bit even before the file is actually closed (which
+            // may fail and not actually close the device!). This is because in
+            // case closing fails, we don't want to allow closing again. we need
+            // this behaviour especially because of the `drop` method that is
+            // bound to run eventually, attempting to close again any unclosed
+            // device.
+            self.is_closed = true;
+
+            let output = std::process::Command::new("udisksctl")
+                .arg("loop-delete")
+                .arg("--block-device").arg(&self.path)
+                .arg("--no-user-interaction")
+                .output()?;
+            if !output.status.success() {
+                return Err(std::io::Error::new(std::io::ErrorKind::Other, format! {
+                    "failed to run `udisksctl loop-delete` (stdout: {:?}, stderr: {:?})",
+                    String::from_utf8_lossy(&output.stdout).as_ref(),
+                    String::from_utf8_lossy(&output.stderr).as_ref(),
+                }))
+            }
+
+            Ok(())
+        }
+    }
+
+    impl Drop for LoopDev {
+
+        fn drop(&mut self) {
+            if !self.is_closed {
+                // `close` takes an owned value, so we replace `self` with some
+                // dummy closed device (it being closed is important to avoid
+                // infinite recursion) and then call explicit close on obtained
+                // owned value.
+                let closed = LoopDev {
+                    path: std::path::PathBuf::new(),
+                    is_closed: true,
+                };
+
+                std::mem::replace(self, closed).close()
+                    .expect("failed to close the loop device");
+            }
+        }
+    }
+
+    #[test]
+    fn loop_dev_new_and_close() {
+        let file = tempfile::NamedTempFile::new()
+            .unwrap();
+
+        let loop_dev = LoopDev::new(&file)
+            .unwrap();
+
+        loop_dev.close()
+            .unwrap();
+    }
+
+    #[test]
+    fn loop_dev_new_and_drop() {
+        let file = tempfile::NamedTempFile::new()
+            .unwrap();
+
+        let loop_dev = LoopDev::new(&file)
+            .unwrap();
+
+        drop(loop_dev);
+    }
+}
