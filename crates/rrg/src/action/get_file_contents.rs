905bf4fb30f350d51fb7dd562b24303d694b5b47
Łukasz Hanuszczak <hanuszczak@google.com>
2026-05-13 16:08:53 +0200

    Add inline transfer mode in `get_file_contents`.

--- crates/rrg/src/action/get_file_contents.rs
+++ crates/rrg/src/action/get_file_contents.rs
@@ -7,6 +7,26 @@ use std::path::PathBuf;
 /// Limit on the size of individual file part blob sent to the blob sink.
 const MAX_BLOB_LEN: usize = 1 * 1024 * 1024; // 1 MiB.
 
+/// Mode of the blob transfer to the GRR server.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Mode {
+    /// Blobs are sent to the blob sink.
+    Sink,
+    /// Blobs are sent directly in the reply.
+    Inline,
+}
+
+/// File part contents reported to the GRR server.
+///
+/// Different variants are used depending on the chosen [mode][`Mode`].
+#[derive(Debug)]
+enum Blob {
+    /// SHA-256 digest of the file part sent to the blob sink.
+    Sha256([u8; 32]),
+    /// Actual contents of the file part.
+    Contents(Vec<u8>),
+}
+
 /// Arguments of the `get_file_contents` action.
 pub struct Args {
     /// Paths to the files to get the contents of.
@@ -15,6 +35,8 @@ pub struct Args {
     offset: u64,
     /// Number of bytes to read from the file.
     len: usize,
+    /// Mode of the blob transfer to use for the files.
+    mode: Mode,
 }
 
 /// Result of the `get_file_contents` action.
@@ -29,8 +51,8 @@ struct OkItem {
     offset: u64,
     /// Number of bytes of the file part sent to the blob sink.
     len: usize,
-    /// SHA-256 digest of the file part sent to the blob sink.
-    blob_sha256: [u8; 32],
+    /// File part contents reported to the GRR server.
+    blob: Blob,
 }
 
 /// Result of the `get_file_contents` action in case of an error.
@@ -102,15 +124,24 @@ where
 
             buf.truncate(len_read);
 
-            let blob = crate::blob::Blob::from(buf);
-            let blob_sha256 = sha2::Sha256::digest(blob.as_bytes()).into();
+            let blob = match args.mode {
+                Mode::Sink => {
+                    let blob = crate::blob::Blob::from(buf);
+                    let blob_sha256 = sha2::Sha256::digest(blob.as_bytes())
+                        .into();
 
-            session.send(crate::Sink::Blob, blob)?;
+                    session.send(crate::Sink::Blob, blob)?;
+
+                    Blob::Sha256(blob_sha256)
+                }
+                Mode::Inline => Blob::Contents(buf),
+            };
+
             session.reply(Ok(OkItem {
                 path: path.clone(),
                 offset,
                 len: len_read,
-                blob_sha256,
+                blob,
             }))?;
 
             offset += len_read as u64;
@@ -140,10 +171,16 @@ impl crate::request::Args for Args {
             len => len as usize,
         };
 
+        let mode = match proto.mode.enum_value_or_default() {
+            rrg_proto::get_file_contents::Mode::SINK => Mode::Sink,
+            rrg_proto::get_file_contents::Mode::INLINE => Mode::Inline,
+        };
+
         Ok(Args {
             paths,
             offset: proto.offset(),
             len,
+            mode,
         })
     }
 }
@@ -160,7 +197,15 @@ impl crate::response::Item for Item {
                 proto.set_path(item.path.into());
                 proto.set_offset(item.offset);
                 proto.set_length(item.len as u64);
-                proto.set_blob_sha256(item.blob_sha256.into());
+
+                match item.blob {
+                    Blob::Sha256(blob_sha256) => {
+                        proto.set_blob_sha256(blob_sha256.into());
+                    }
+                    Blob::Contents(blob_contents) => {
+                        proto.set_blob_contents(blob_contents);
+                    }
+                }
             }
             Err(item) => {
                 proto.set_path(item.path.into());
@@ -222,6 +267,7 @@ mod tests {
             paths: vec![tempdir.path().join("idonotexist")],
             offset: 0,
             len: usize::MAX,
+            mode: Mode::Sink,
         };
 
         let mut session = crate::session::FakeSession::new();
@@ -249,6 +295,7 @@ mod tests {
             paths: vec![tempdir.path().join("foo")],
             offset: 0,
             len: usize::MAX,
+            mode: Mode::Sink,
         };
 
         let mut session = crate::session::FakeSession::new();
@@ -271,6 +318,7 @@ mod tests {
             paths: vec![tempdir.path().join("foo")],
             offset: 0,
             len: usize::MAX,
+            mode: Mode::Sink,
         };
 
         let mut session = crate::session::FakeSession::new();
@@ -302,6 +350,7 @@ mod tests {
             paths: vec![tempdir.path().join("foo")],
             offset: 5,
             len: usize::MAX,
+            mode: Mode::Sink,
         };
 
         let mut session = crate::session::FakeSession::new();
@@ -333,6 +382,7 @@ mod tests {
             paths: vec![tempdir.path().join("foo")],
             offset: 0,
             len: 5,
+            mode: Mode::Sink,
         };
 
         let mut session = crate::session::FakeSession::new();
@@ -360,6 +410,7 @@ mod tests {
             paths: vec![PathBuf::from("/dev/zero")],
             offset: 0,
             len: MAX_BLOB_LEN * 2 + 1337,
+            mode: Mode::Sink,
         };
 
         let mut session = crate::session::FakeSession::new();
@@ -403,6 +454,7 @@ mod tests {
             paths: vec![PathBuf::from("/dev/zero")],
             offset: 0xb33f,
             len: MAX_BLOB_LEN + 1337,
+            mode: Mode::Sink,
         };
 
         let mut session = crate::session::FakeSession::new();
@@ -443,6 +495,7 @@ mod tests {
             ],
             offset: 0,
             len: usize::MAX,
+            mode: Mode::Sink,
         };
 
         let mut session = crate::session::FakeSession::new();
@@ -470,17 +523,17 @@ mod tests {
         let item_foo = items_by_path[&tempdir.join("foo")];
         assert_eq!(item_foo.offset, 0);
         assert_eq!(item_foo.len, 3);
-        assert_eq!(blobs_by_sha256[&item_foo.blob_sha256].as_bytes(), b"012");
+        assert_eq!(blobs_by_sha256[&item_blob_sha256(&item_foo)].as_bytes(), b"012");
 
         let item_bar = items_by_path[&tempdir.join("bar")];
         assert_eq!(item_bar.offset, 0);
         assert_eq!(item_bar.len, 3);
-        assert_eq!(blobs_by_sha256[&item_bar.blob_sha256].as_bytes(), b"345");
+        assert_eq!(blobs_by_sha256[&item_blob_sha256(&item_bar)].as_bytes(), b"345");
 
         let item_baz = items_by_path[&tempdir.join("baz")];
         assert_eq!(item_baz.offset, 0);
         assert_eq!(item_baz.len, 3);
-        assert_eq!(blobs_by_sha256[&item_baz.blob_sha256].as_bytes(), b"678");
+        assert_eq!(blobs_by_sha256[&item_blob_sha256(&item_baz)].as_bytes(), b"678");
     }
 
     #[test]
@@ -502,6 +555,7 @@ mod tests {
             ],
             offset: 0,
             len: usize::MAX,
+            mode: Mode::Sink,
         };
 
         let mut session = crate::session::FakeSession::new();
@@ -537,16 +591,59 @@ mod tests {
             .as_ref().unwrap();
         assert_eq!(item_foo.offset, 0);
         assert_eq!(item_foo.len, 3);
-        assert_eq!(blobs_by_sha256[&item_foo.blob_sha256].as_bytes(), b"012");
+        assert_eq!(blobs_by_sha256[&item_blob_sha256(&item_foo)].as_bytes(), b"012");
 
         let item_bar = items_by_path[&tempdir.join("bar")]
             .as_ref().unwrap();
         assert_eq!(item_bar.offset, 0);
         assert_eq!(item_bar.len, 3);
-        assert_eq!(blobs_by_sha256[&item_bar.blob_sha256].as_bytes(), b"345");
+        assert_eq!(blobs_by_sha256[&item_blob_sha256(&item_bar)].as_bytes(), b"345");
 
         let item_error = items_by_path[&tempdir.join("idonotexist")]
             .as_ref().unwrap_err();
         assert_eq!(item_error.error.kind, FileErrorKind::Open);
+    }
+
+    #[test]
+    fn handle_mode_inline() {
+        let tempdir = tempfile::tempdir()
+            .unwrap();
+
+        std::fs::write(tempdir.path().join("foo"), b"0123456789")
+            .unwrap();
+
+        let args = Args {
+            paths: vec![tempdir.path().join("foo")],
+            offset: 0,
+            len: usize::MAX,
+            mode: Mode::Inline,
+        };
+
+        let mut session = crate::session::FakeSession::new();
+        handle(&mut session, args)
+            .unwrap();
+
+        assert_eq!(session.parcel_count(crate::Sink::Blob), 0);
+        assert_eq!(session.reply_count(), 1);
+
+        let item = session.reply::<Item>(0)
+            .as_ref().unwrap();
+        assert_eq!(item.offset, 0);
+        assert_eq!(item.len, 10);
+        assert_eq!(item_blob_contents(item), b"0123456789");
+    }
+
+    fn item_blob_sha256(item: &OkItem) -> [u8; 32] {
+        match item.blob {
+            Blob::Sha256(blob_sha256) => blob_sha256,
+            Blob::Contents(_) => panic!("non-contents blob: {:?}", item.blob),
+        }
+    }
+
+    fn item_blob_contents(item: &OkItem) -> &[u8] {
+        match &item.blob {
+             Blob::Sha256(_) => panic!("non-SHA-256 blob: {:?}", item.blob),
+             Blob::Contents(blob_contents) => &blob_contents,
+        }
     }
 }
