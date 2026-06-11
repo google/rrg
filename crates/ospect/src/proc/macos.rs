10aa3b8e80cc16c5e63d66ede34e9580824348ea
Łukasz Hanuszczak <hanuszczak@google.com>
2026-06-11 16:35:41 +0200

    Refactor system calls to separate functions.

--- crates/ospect/src/proc/macos.rs
+++ crates/ospect/src/proc/macos.rs
@@ -50,70 +50,7 @@ impl Metadata {
     }
 
     pub fn args(&self) -> std::io::Result<Args> {
-        let mut mib = [
-            libc::CTL_KERN,
-            libc::KERN_PROCARGS2,
-            self.id() as libc::c_int,
-        ];
-
-        let mut buf_size = std::mem::MaybeUninit::uninit();
-
-        // SAFETY: We call the `sysctl` function as described in the FreeBSD
-        // documentation [1] (macOS's kernel derives from FreeBSD). We check for
-        // errors afterwards.
-        //
-        // Note that the `KERN_PROCARGS2` sytem call that we use here is not
-        // documented in the official documentation [2] but we can see how it
-        // works in the kernel source [3].
-        //
-        // Despite the call being undocumented this code should not cause any
-        // undefined behaviour: we pass input and output buffers along with its
-        // size. In the worst case we will get back garbage data but we will
-        // only access memory that we explicitly allocated and own.
-        //
-        // This is the first call where we don't pass any buffer and we just
-        // want to estimate the size of the buffer to hold the data. It should
-        // be returned thought the fourth (`oldlenp`) argument.
-        //
-        // [1]: https://man.freebsd.org/cgi/man.cgi?sysctl(3)
-        // [2]: https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man3/sysctl.3.html
-        // [3]: https://github.com/apple-oss-distributions/xnu/blob/f6217f891ac0bb64f3d375211650a4c1ff8ca1ea/bsd/kern/kern_sysctl.c#L1300-L1326
-        let code = unsafe {
-            libc::sysctl(
-                mib.as_mut_ptr(), mib.len() as libc::c_uint,
-                std::ptr::null_mut(), buf_size.as_mut_ptr(),
-                std::ptr::null_mut(), 0,
-            )
-        };
-        if code != 0 {
-            return Err(std::io::Error::last_os_error());
-        }
-
-        // SAFETY: If the call to `sysctl` succeeded, we can assume that the
-        // `buf_size` is now filled with the expected size of the buffer.
-        let mut buf_size = unsafe {
-            buf_size.assume_init()
-        };
-        // Note that we do not need to worry about any alignment issues (the
-        // kernel writes an `i32` with the argument count to the beginning of
-        // the buffer) as it is not used directly but instead the kernel makes
-        // its copy in the kernel space and uses that instead.
-        let mut buf = vec![0u8; buf_size as usize];
-
-        // SAFETY: We create a buffer of the size specified by the previous call
-        // to `sysctl`. We verify whether the call succeeded below.
-        //
-        // The rest is as with the `sysctl` call above.
-        let code = unsafe {
-            libc::sysctl(
-                mib.as_mut_ptr(), mib.len() as libc::c_uint,
-                buf.as_mut_ptr().cast::<libc::c_void>(), &mut buf_size,
-                std::ptr::null_mut(), 0,
-            )
-        };
-        if code != 0 {
-            return Err(std::io::Error::last_os_error());
-        }
+        let buf = sysctl_kern_procargs2(self.raw.kp_proc.p_pid)?;
 
         let argc_bytes = <[u8; 4]>::try_from(&buf[0..4])
             .map_err(|error| std::io::Error::new(
@@ -148,83 +85,7 @@ impl Metadata {
 
 /// Returns an iterator yielding metadata for all processes on the system.
 pub fn all() -> std::io::Result<impl Iterator<Item = std::io::Result<Metadata>>> {
-        const KINFO_PROC_SIZE: usize = {
-            std::mem::size_of::<crate::libc::kinfo_proc>()
-        };
-
-        let mut mib = [libc::CTL_KERN, libc::KERN_PROC, libc::KERN_PROC_ALL];
-
-        let mut buf_size = std::mem::MaybeUninit::uninit();
-
-        // SAFETY: We call the `sysctl` function as described in the FreeBSD
-        // documentation [1] (macOS's kernel derives from FreeBSD). We check for
-        // errors afterwards.
-        //
-        // This is the first call where we don't pass any buffer and we just
-        // want to estimate the size of the buffer to hold the data. It should
-        // be returned thought the fourth (`oldlenp`) argument.
-        //
-        // Note that the `namelen` parameter (second argument) is the _length_
-        // of the array passed as the `name` argument (not size in bytes), while
-        // the remaining two expect size in bytes.
-        //
-        // [1]: https://man.freebsd.org/cgi/man.cgi?sysctl(3)
-        let code = unsafe {
-            libc::sysctl(
-                mib.as_mut_ptr(), mib.len() as libc::c_uint,
-                std::ptr::null_mut(), buf_size.as_mut_ptr(),
-                std::ptr::null_mut(), 0,
-            )
-        };
-        if code != 0 {
-            return Err(std::io::Error::last_os_error());
-        }
-
-        // SAFETY: If the call to `sysctl` succeeded, we can assume that the
-        // `buf_size` no is filled with the expected size of the buffer.
-        let mut buf_size = unsafe {
-            buf_size.assume_init()
-        } as usize;
-
-        let mut buf_len = buf_size / KINFO_PROC_SIZE;
-        if buf_size % KINFO_PROC_SIZE != 0 {
-            buf_len += 1;
-        }
-
-        let mut buf = Vec::<crate::libc::kinfo_proc>::with_capacity(buf_len);
-
-        // SAFETY: We create a buffer of the size specified by the previous call
-        // to `sysctl`. Note that between the two calls the required buffer size
-        // might have changed in which case `ENOMEM` ought to be returned. The
-        // operating system should round up the required buffer size to handle
-        // such cases. We verify whether the call succeeded below.
-        //
-        // The rest is as with the `sysctl` call above (not the comment about
-        // the length parameters).
-        let code = unsafe {
-            libc::sysctl(
-                mib.as_mut_ptr(), mib.len() as libc::c_uint,
-                buf.as_mut_ptr().cast::<libc::c_void>(), &mut buf_size,
-                std::ptr::null_mut(), 0,
-            )
-        };
-        if code != 0 {
-            return Err(std::io::Error::last_os_error());
-        }
-
-        if buf_size % KINFO_PROC_SIZE != 0 {
-            return Err(std::io::ErrorKind::InvalidData.into());
-        }
-
-        let buf_len = buf_size / KINFO_PROC_SIZE;
-
-        // SAFETY: The `syctl` call succeeded and we calculated the length of
-        // the buffer above.
-        unsafe {
-            buf.set_len(buf_len);
-        }
-
-        Ok(buf.into_iter().map(|raw| Ok(Metadata { raw })))
+    Ok(sysctl_kern_proc()?.into_iter().map(|raw| Ok(Metadata { raw })))
 }
 
 /// Iterator over the arguments of the process.
@@ -275,6 +136,159 @@ impl Iterator for Args {
 }
 
 impl ExactSizeIterator for Args {
+}
+
+fn sysctl_kern_proc() -> std::io::Result<Vec<crate::libc::kinfo_proc>> {
+    const KINFO_PROC_SIZE: usize = {
+        std::mem::size_of::<crate::libc::kinfo_proc>()
+    };
+
+    let mut mib = [
+        libc::CTL_KERN,
+        libc::KERN_PROC,
+        libc::KERN_PROC_ALL,
+    ];
+
+    let mut buf_size = std::mem::MaybeUninit::uninit();
+
+    // SAFETY: We call the `sysctl` function as described in the FreeBSD
+    // documentation [1] (macOS's kernel derives from FreeBSD). We check for
+    // errors afterwards.
+    //
+    // This is the first call where we don't pass any buffer and we just want
+    // to estimate the size of the buffer to hold the data. It should be
+    // returned thought the fourth (`oldlenp`) argument.
+    //
+    // Note that the `namelen` parameter (second argument) is the _length_ of
+    // the array passed as the `name` argument (not size in bytes), while the
+    // remaining two expect size in bytes.
+    //
+    // [1]: https://man.freebsd.org/cgi/man.cgi?sysctl(3)
+    let code = unsafe {
+        libc::sysctl(
+            mib.as_mut_ptr(), mib.len() as libc::c_uint,
+            std::ptr::null_mut(), buf_size.as_mut_ptr(),
+            std::ptr::null_mut(), 0,
+        )
+    };
+    if code != 0 {
+        return Err(std::io::Error::last_os_error());
+    }
+
+    // SAFETY: If the call to `sysctl` succeeded, we can assume that the
+    // `buf_size` no is filled with the expected size of the buffer.
+    let mut buf_size = unsafe {
+        buf_size.assume_init()
+    } as usize;
+
+    let mut buf_len = buf_size / KINFO_PROC_SIZE;
+    if buf_size % KINFO_PROC_SIZE != 0 {
+        buf_len += 1;
+    }
+
+    let mut buf = Vec::<crate::libc::kinfo_proc>::with_capacity(buf_len);
+
+    // SAFETY: We create a buffer of the size specified by the previous call to
+    // `sysctl`. Note that between the two calls the required buffer size might
+    // have changed in which case `ENOMEM` ought to be returned. The operating
+    // system should round up the required buffer size to handle such cases. We
+    // verify whether the call succeeded below.
+    //
+    // The rest is as with the `sysctl` call above (note the comment about the
+    // length parameters).
+    let code = unsafe {
+        libc::sysctl(
+            mib.as_mut_ptr(), mib.len() as libc::c_uint,
+            buf.as_mut_ptr().cast::<libc::c_void>(), &mut buf_size,
+            std::ptr::null_mut(), 0,
+        )
+    };
+    if code != 0 {
+        return Err(std::io::Error::last_os_error());
+    }
+
+    if buf_size % KINFO_PROC_SIZE != 0 {
+        return Err(std::io::ErrorKind::InvalidData.into());
+    }
+
+    let buf_len = buf_size / KINFO_PROC_SIZE;
+
+    // SAFETY: The `syctl` call succeeded and we calculated the length of
+    // the buffer above.
+    unsafe {
+        buf.set_len(buf_len);
+    }
+
+    Ok(buf)
+}
+
+fn sysctl_kern_procargs2(pid: libc::pid_t) -> std::io::Result<Vec<u8>> {
+    let mut mib = [
+        libc::CTL_KERN,
+        libc::KERN_PROCARGS2,
+        pid as libc::c_int,
+    ];
+
+    let mut buf_size = std::mem::MaybeUninit::uninit();
+
+    // SAFETY: We call the `sysctl` function as described in the FreeBSD
+    // documentation [1] (macOS's kernel derives from FreeBSD). We check for
+    // errors afterwards.
+    //
+    // Note that the `KERN_PROCARGS2` sytem call that we use here is not
+    // documented in the official documentation [2] but we can see how it
+    // works in the kernel source [3].
+    //
+    // Despite the call being undocumented this code should not cause any
+    // undefined behaviour: we pass input and output buffers along with its
+    // size. In the worst case we will get back garbage data but we will
+    // only access memory that we explicitly allocated and own.
+    //
+    // This is the first call where we don't pass any buffer and we just
+    // want to estimate the size of the buffer to hold the data. It should
+    // be returned thought the fourth (`oldlenp`) argument.
+    //
+    // [1]: https://man.freebsd.org/cgi/man.cgi?sysctl(3)
+    // [2]: https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man3/sysctl.3.html
+    // [3]: https://github.com/apple-oss-distributions/xnu/blob/f6217f891ac0bb64f3d375211650a4c1ff8ca1ea/bsd/kern/kern_sysctl.c#L1300-L1326
+    let code = unsafe {
+        libc::sysctl(
+            mib.as_mut_ptr(), mib.len() as libc::c_uint,
+            std::ptr::null_mut(), buf_size.as_mut_ptr(),
+            std::ptr::null_mut(), 0,
+        )
+    };
+    if code != 0 {
+        return Err(std::io::Error::last_os_error());
+    }
+
+    // SAFETY: If the call to `sysctl` succeeded, we can assume that the
+    // `buf_size` is now filled with the expected size of the buffer.
+    let mut buf_size = unsafe {
+        buf_size.assume_init()
+    };
+    // Note that we do not need to worry about any alignment issues (the
+    // kernel writes an `i32` with the argument count to the beginning of
+    // the buffer) as it is not used directly but instead the kernel makes
+    // its copy in the kernel space and uses that instead.
+    let mut buf = vec![0u8; buf_size as usize];
+
+    // SAFETY: We create a buffer of the size specified by the previous call
+    // to `sysctl`. We verify whether the call succeeded below.
+    //
+    // The rest is as with the `sysctl` call above.
+    let code = unsafe {
+        libc::sysctl(
+            mib.as_mut_ptr(), mib.len() as libc::c_uint,
+            buf.as_mut_ptr().cast::<libc::c_void>(), &mut buf_size,
+            std::ptr::null_mut(), 0,
+        )
+    };
+    if code != 0 {
+        return Err(std::io::Error::last_os_error());
+    }
+
+    Ok(buf)
 }
 
 #[cfg(test)]
